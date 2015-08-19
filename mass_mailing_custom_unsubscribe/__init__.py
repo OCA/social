7b5e992fb86100b2bbc9253cefba70d7388bceb6
Antonio Espinosa <antespi@gmail.com>
2015-08-19 12:15:24 +0200

    [ADD] mass_mailing_custom_unsubscribe addon

---
+++ mass_mailing_custom_unsubscribe/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from . import models
