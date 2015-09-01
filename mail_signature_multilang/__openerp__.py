9230de47b3f7f00db987f4634e6a3447bbfd5fea
Javier Iniesta <javieria@antiun.com>
2015-09-01 16:11:05 +0200

    [FIX] License information

--- mail_signature_multilang/__openerp__.py
+++ mail_signature_multilang/__openerp__.py
@@ -1,50 +1,15 @@
 # -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
-##############################################################################
-#
-#    OpenERP, Odoo Source Management Solution
-#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# See README.rst file on addon root folder for license details
 
 {
-    # Addon information
     'name': "Mail Signature Multilang",
-    'category': 'Personalization',
-    'version': '1.0',
+    'category': 'Marketing',
+    'version': '8.0.1.0.0',
     'depends': [
         'base',
-    ],
-    'external_dependencies': {},
-    # Views templates, pages, menus, options and snippets
-    'data': [
-    ],
-    # Qweb templates
-    'qweb': [
-    ],
-    'js': [
     ],
     'author': 'Antiun Ingeniería S.L.',
     'website': 'http://www.antiun.com',
     'license': 'AGPL-3',
-    # Technical options
-    'demo': [],
-    'test': [],
     'installable': True,
-    # 'auto_install':False,
-    # 'active':True,
 }
