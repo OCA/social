ffdd938d931b3cdee5561ce98f21186fe6114975
Jairo Llopis <yajo.sk8@gmail.com>
2015-09-29 16:34:14 +0200

    [ADD] New module mail_sent.

---
+++ mail_sent/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Sent mails",
+    "summary": "Provide a view of sent mails",
+    "version": "8.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://odoo-community.org/",
+    "author": "Antiun Ingeniería, S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        "views/mail.xml",
+    ],
+}
