beae88b0cba6ef59d3db724d187d2e14e3125270
Jairo Llopis <j.llopis@grupoesoc.es>
2016-01-25 10:53:15 +0100

    [ADD] mass_mailing_unique: Avoid duplicates.
    This new module avoids duplicates in mass mailing lists.
    
    Now you cannot have 2 lists with the same name, and you cannot have the same
    email repeated in a list.
    
    This will avoid sending the same mail to a contact repeated times, which will
    disturb him and most probably cause you to be blocked as spam.

---
+++ mass_mailing_unique/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
+from .hooks import pre_init_hook
