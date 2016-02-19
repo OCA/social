ad72af590ab9ee6699cba73e8c0cb41f59528766
Jairo Llopis <yajo.sk8@gmail.com>
2016-02-19 16:27:09 +0100

    Prepare module website_mail_snippet_fixed.

---
+++ website_mail_snippet_fixed/__openerp__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Responsive Layout Snippets for Writing Emails",
+    "summary": "Well... pseudo-responsive (see description)",
+    "version": "8.0.1.0.0",
+    "category": "Marketing",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "images": [
+        "images/snippets.png",
+    ],
+    "depends": [
+        "website_mail",
+    ],
+    "data": [
+        "views/assets.xml",
+        "views/templates.xml",
+        "views/snippet_1_col.xml",
+        "views/snippet_2_cols.xml",
+        "views/snippet_3_cols.xml",
+        "views/snippet_hr.xml",
+        "views/snippet_img_text.xml",
+        "views/snippet_text_img.xml",
+    ],
+}
