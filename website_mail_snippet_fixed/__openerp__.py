ca36a5873d96c8532b1fe6a1a7f6204dfb68601d
Daniel Gómez-Zurita <danielgz@antiun.com>
2016-02-19 17:16:56 +0100

    Fixed blocks width: 560px

--- website_mail_snippet_fixed/__openerp__.py
+++ website_mail_snippet_fixed/__openerp__.py
@@ -2,8 +2,8 @@
 # © 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Responsive Layout Snippets for Writing Emails",
-    "summary": "Well... pseudo-responsive (see description)",
+    "name": "Extra Layout Snippets for Writing Emails",
+    "summary": "560px width extra building blocks",
     "version": "8.0.1.0.0",
     "category": "Marketing",
     "website": "http://www.antiun.com",
