ed2e9a29cc1d45fc29815fde924d010eae358622
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-01 18:12:03 +0100

    [8.0][website_mail_snippet_table_edit] Edit tables in your mails.
    With this module, you can now duplicate or delete <tr> elements in your table-based templates or snippets for emails.

---
+++ website_mail_snippet_table_edit/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Edit Table Rows in Website Mail Designer",
+    "summary": "Allows you to edit any <tr> element in any snippet/template",
+    "version": "8.0.1.0.0",
+    "category": "Social Network",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_mail",
+    ],
+    "data": [
+        "views/snippets.xml",
+    ],
+}
