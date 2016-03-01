c928832ae2cd8813630a19cb7a70ccc18734c82d
Jairo Llopis <yajo.sk8@gmail.com>
2016-03-01 17:23:15 +0100

    [8.0][mail_debrand] Remove branding from mail footers.
    Those mail footers odoo sends that say "Sent by YourCompany using Odoo" now say nothing of Odoo.

---
+++ mail_debrand/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail Debrand",
+    "summary": "Remove Odoo branding from email footers",
+    "version": "8.0.1.0.0",
+    "category": "Social Network",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+}
