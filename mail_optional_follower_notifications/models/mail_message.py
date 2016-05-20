2d35fe861079e505de8b8ba149d9b14c7aa8cc1e
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-05-20 11:55:10 +0200

    [IMP][mail_optional_follower_notifications] Code review.

--- mail_optional_follower_notifications/models/mail_message.py
+++ mail_optional_follower_notifications/models/mail_message.py
@@ -12,10 +12,8 @@ class MailMessage(models.Model):
     def create(self, values):
         ctx = self.env.context.copy()
         if not ctx.get('notify_followers') and values.get('partner_ids'):
-            force_partners_to_notify = []
-            for partner_id in values.get('partner_ids'):
-                if isinstance(partner_id, tuple) and\
-                        len(partner_id) == 2 and partner_id[0] == 4:
-                    force_partners_to_notify.append(partner_id[1])
+            partner_list = self.resolve_2many_commands(
+                'partner_ids', values.get('partner_ids'), fields=['id'])
+            force_partners_to_notify = [d['id'] for d in partner_list]
             ctx['force_partners_to_notify'] = force_partners_to_notify
         return super(MailMessage, self.with_context(ctx)).create(values)
