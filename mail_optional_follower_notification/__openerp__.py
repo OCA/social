962bdca9a28dd1dce924e443fca148fc63b92fea
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-05-25 16:03:38 +0200

    Add mail_optional_follower_notification module (#60)
    * [ADD] Add mail_optional_follower_notifications module
    
    * [CHG][mail_optional_follower_notifications] Readme.
    
    * [IMP][mail_optional_follower_notifications] Code review.
    
    * [CHG] Rename mail_optional_follower_notifications to mail_optional_follower_notification
    
    * [IMP][mail_optional_follower_notification] Review

---
+++ mail_optional_follower_notification/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Mail optional follower notification",
+
+    'summary': """
+        Choose if you want to automatically notify followers
+        on mail.compose.message""",
+    'author': 'ACSONE SA/NV,'
+              'Odoo Community Association (OCA)',
+    'website': "http://acsone.eu",
+    'category': 'Social Network',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'mail',
+    ],
+    'data': [
+        'wizard/mail_compose_message_view.xml',
+    ],
+}
