f5ed3ee950a1edf3eeebfdd25e48d630eaa07cff
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-05-25 14:12:00 +0200

    Upgrade version from 8 -> 9

--- mail_optional_follower_notification/__openerp__.py
+++ mail_optional_follower_notification/__openerp__.py
@@ -11,7 +11,7 @@
               'Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Social Network',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'mail',
