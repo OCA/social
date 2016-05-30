c37d601745d0c17f6dfa1328317124d5120f16ca
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-05-30 13:11:09 +0200

    [FIX] Call to generate_email can be done with a list of ids or only one id (#65)
    fixes #64

--- email_template_qweb/tests/test_mail_template_qweb.py
+++ email_template_qweb/tests/test_mail_template_qweb.py
@@ -15,3 +15,12 @@ class TestMailTemplateQweb(TransactionCase):
                 mail_values[self.env.user.id]['body_html']
             )
         )
+        # the same method is also called in a non multi mode
+        mail_values = template.generate_email(self.env.user.id)
+        self.assertTrue(
+            # this comes from the called template if everything worked
+            '<footer>' in mail_values['body_html'],
+            'Did not rcv rendered template in response. Got: \n%s\n' % (
+                mail_values['body_html']
+            )
+        )
