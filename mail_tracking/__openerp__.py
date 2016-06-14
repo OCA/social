0a5c9adcd720574d2b338e70bdd7ae52b4ce8424
Antonio Espinosa <antespi@gmail.com>
2016-06-14 17:22:17 +0200

    [8.0][ADD] mail_tracking addon (#67)
    * [ADD] mail_tracking addon
    
    * Add description icon
    
    * Fixes remarked
    
    * Fix Travis error
    
    * Remarks fixed

---
+++ mail_tracking/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Email tracking",
+    "summary": "Email tracking system for all mails sent",
+    "version": "8.0.2.0.0",
+    "category": "Social Network",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "decimal_precision",
+        "mail",
+    ],
+    "data": [
+        "data/tracking_data.xml",
+        "security/ir.model.access.csv",
+        "views/assets.xml",
+        "views/mail_tracking_email_view.xml",
+        "views/mail_tracking_event_view.xml",
+        "views/res_partner_view.xml",
+    ],
+    "qweb": [
+        "static/src/xml/mail_tracking.xml",
+    ],
+    "post_init_hook": "post_init_hook",
+}
