a5c621319413abd6a16a3ea4715466d721ad7a8f
Antonio Espinosa <antespi@gmail.com>
2016-07-15 13:41:49 +0200

    [ADD] mail_tracking_mailgun

---
+++ mail_tracking_mailgun/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail tracking for Mailgun",
+    "summary": "Mail tracking and Mailgun webhooks integration",
+    "version": "8.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://odoo-community.org/",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail_tracking",
+    ],
+}
