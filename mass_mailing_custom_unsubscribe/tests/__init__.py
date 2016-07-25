f1aa9907dd64b820d623a2a75f4c256c3824dc14
Yajo <Yajo@users.noreply.github.com>
2016-07-25 12:50:32 +0200

    [8.0][IMP][mass_mailing_custom_unsubscribe] Get reasons for unsubscription (#58)
    * [8.0][IMP][mass_mailing_custom_unsubscribe] Get reasons for unsubscription.

---
+++ mass_mailing_custom_unsubscribe/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_unsubscription
+from . import test_mail_mail
+from . import test_controller
