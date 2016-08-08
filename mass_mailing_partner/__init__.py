6e50c7cb37ec72b95b3a98fbf74390467d768e74
Antonio Espinosa <antespi@gmail.com>
2016-08-08 13:14:37 +0200

    Link mail statistics to partner (#104)
    [IMP] mass_mailing_partner: Link mail statistics to partner

--- mass_mailing_partner/__init__.py
+++ mass_mailing_partner/__init__.py
@@ -1,20 +1,10 @@
 # -*- coding: utf-8 -*-
-# See README.rst file on addon root folder for license details
+# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015 Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Javier Iniesta <javieria@antiun.com>
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
 from . import wizard
-from openerp import api, SUPERUSER_ID
-
-
-def _match_existing_contacts(cr, registry):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        contact_model = env['mail.mass_mailing.contact']
-        partner_model = env['res.partner']
-        contacts = contact_model.search([('email', '!=', False)])
-        for contact in contacts:
-            if contact.email:
-                partners = partner_model.search([('email', '=ilike',
-                                                  contact.email)])
-                if partners:
-                    contact.write({'partner_id': partners[0].id})
+from .hooks import post_init_hook
--- mass_mailing_partner/__openerp__.py
+++ mass_mailing_partner/__openerp__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-# See README.rst file on addon root folder for license details
+# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2015 Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Javier Iniesta <javieria@antiun.com>
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Link partners with mass-mailing",
-    "version": "8.0.1.0.0",
-    "author": "Antiun Ingeniería S.L., "
+    "version": "8.0.2.0.0",
+    "author": "Tecnativa, "
+              "Antiun Ingeniería S.L., "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
@@ -12,8 +17,9 @@
     "depends": [
         'mass_mailing',
     ],
-    "post_init_hook": "_match_existing_contacts",
+    "post_init_hook": "post_init_hook",
     'data': [
+        'views/mail_mail_statistics_view.xml',
         'views/mail_mass_mailing_contact_view.xml',
         'views/mail_mass_mailing_view.xml',
         'views/res_partner_view.xml',
