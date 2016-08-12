a0a884ed6447de5956b2edf8ab287eb6b559abe6
Antonio Espinosa <antespi@gmail.com>
2016-08-12 13:15:07 +0200

    Fix travis

--- mail_tracking_mass_mailing/models/mail_mail_statistics.py
+++ mail_tracking_mass_mailing/models/mail_mail_statistics.py
@@ -2,7 +2,7 @@
 # © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class MailMailStatistics(models.Model):
