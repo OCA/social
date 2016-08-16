79d18553c172a4997c54c43b9a5b988fce833574
Antonio Espinosa <antespi@gmail.com>
2016-08-16 09:07:35 +0200

    mail_tracking_mass_mailing (#78)
    [ADD] mail_tracking_mass_mailing
    
    ==============================
    Mail tracking for mass mailing
    ==============================
    
    Links mail statistics objects with mail tracking objects.
    
    Installation
    ============
    
    This addon will be automatically installed when 'mail_tracking' and
    'mass_mailing' are both installed
    
    Usage
    =====
    
    From mail statistic object, you can see:
    - Email tracking state
    - Email related tracking object
    - Email related tracking events
    
    From mass mailing contact, you can see:
    - Email score, in order to clean up your lists from bad score emails
    
    As a bonus feature, you have a new checkbox 'Avoid resend' in mass mailing,
    in order to not send twice the same email to the same recipient. This is very
    useful when you want to resend the mass mailing after changing selection
    recipients. Notice that recipient selection could be a domain over a model, so
    result ids could change over the time. With this flag you can send
    the same email several times but only once to each recipient.
    
    OCA Transbot updated translations from Transifex

---
+++ mail_tracking_mass_mailing/models/mail_mass_mailing_contact.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api, fields
+
+
+class MailMassMailingContact(models.Model):
+    _inherit = 'mail.mass_mailing.contact'
+
+    email_bounced = fields.Boolean(string="Email bounced")
+    email_score = fields.Float(
+        string="Email score", readonly=True, store=False,
+        compute='_compute_email_score')
+
+    @api.multi
+    @api.depends('email')
+    def _compute_email_score(self):
+        for contact in self.filtered('email'):
+            contact.email_score = self.env['mail.tracking.email'].\
+                email_score_from_email(contact.email)
+
+    @api.multi
+    def email_bounced_set(self, tracking_emails, reason, event=None):
+        contacts = self.filtered(lambda r: not r.email_bounced)
+        return contacts.write({'email_bounced': True})
+
+    @api.multi
+    def write(self, vals):
+        email = vals.get('email')
+        if email is not None:
+            vals['email_bounced'] = (
+                bool(email) and
+                self.env['mail.tracking.email'].email_is_bounced(email))
+        return super(MailMassMailingContact, self).write(vals)
