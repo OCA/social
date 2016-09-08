ef1d3af5cff9f6b47a6398e40e8e5e7468683af8
Antonio Espinosa <antespi@gmail.com>
2016-09-08 17:49:08 +0200

    Protect import

--- mail_tracking_mass_mailing/hooks.py
+++ mail_tracking_mass_mailing/hooks.py
@@ -3,14 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-from openerp.addons.mail_tracking.hooks import column_add_with_value
+try:
+    from openerp.addons.mail_tracking.hooks import column_add_with_value
+except ImportError:
+    column_add_with_value = False
 
 _logger = logging.getLogger(__name__)
 
 
 def pre_init_hook(cr):
-    _logger.info("Creating mail.mass_mailing.contact.email_score column "
-                 "with value 50.0")
-    column_add_with_value(
-        cr, 'mail_mass_mailing_contact', 'email_score', 'double precision',
-        50.0)
+    if column_add_with_value:
+        _logger.info("Creating mail.mass_mailing.contact.email_score column "
+                     "with value 50.0")
+        column_add_with_value(
+            cr, 'mail_mass_mailing_contact', 'email_score', 'double precision',
+            50.0)
