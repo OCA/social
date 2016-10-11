f9d4b0ea76398b7c69fc63fe0491f9e2158afa3c
Damien Bouvy <dbo@odoo.com>
2016-10-11 23:56:12 +0200

    [MIG] mail_full_expand: Migrated to 10.0

--- mail_full_expand/static/src/js/mail_full_expand.js
+++ mail_full_expand/static/src/js/mail_full_expand.js
@@ -1,26 +1,29 @@
 /* © 2014-2015 Grupo ESOC <http://www.grupoesoc.es>
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
+ odoo.define('mail_full_expand.expand', function (require) {
+"use strict";
 
-openerp.mail_full_expand = function (instance) {
-    instance.mail.ThreadMessage.include({
-        bind_events: function () {
-            this._super.apply(this, arguments);
-            this.$('.oe_full_expand').on('click', this.on_message_full_expand);
-        },
+var Thread = require('mail.ChatThread');
 
-        on_message_full_expand: function() {
-            // Get the action data and execute it to open the full view
-            var do_action = this.do_action,
-                msg_id = this.id;
+Thread.include({    
+    events: _.defaults({
+        "click .o_full_expand": "on_message_full_expand",
+    }, Thread.prototype.events),
+        
+    on_message_full_expand: function(event) {
+        // Get the action data and execute it to open the full view
+        var do_action = this.do_action,
+            msg_id = $(event.currentTarget).data('message-id');
 
-            this.rpc("/web/action/load", {
-                "action_id": "mail_full_expand.act_window",
-            })
-            .done(function(action) {
-                action.res_id = msg_id;
-                do_action(action);
-            });
-        }
-    });
-};
+        this.rpc("/web/action/load", {
+            "action_id": "mail_full_expand.mail_message_action",
+        })
+        .done(function(action) {
+            action.res_id = msg_id;
+            do_action(action);
+        });
+    }
+});
+
+});
