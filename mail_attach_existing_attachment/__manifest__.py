a1f30a1f693a1141fc8c698012cac3296676e4f9
Damien Bouvy <dbo@odoo.com>
2016-10-11 20:22:36 +0200

    [MIG] mail_attach_existing_attachment: Migrated to 10.0

--- mail_attach_existing_attachment/__manifest__.py
+++ mail_attach_existing_attachment/__manifest__.py
@@ -31,7 +31,7 @@
               "Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'Social Network',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'mail',
