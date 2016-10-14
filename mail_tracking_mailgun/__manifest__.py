1fcc22e769c1149cdf23891e507a4d244b38ab13
Damien Bouvy <dbo@odoo.com>
2016-10-14 22:57:30 +0200

    [MIG] mail_tracking_mailgun: Migrated to 10.0

--- mail_tracking_mailgun/__manifest__.py
+++ mail_tracking_mailgun/__manifest__.py
@@ -4,14 +4,14 @@
 {
     "name": "Mail tracking for Mailgun",
     "summary": "Mail tracking and Mailgun webhooks integration",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Social Network",
     "website": "https://odoo-community.org/",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'installable': False,
+    'installable': True,
     "depends": [
         "mail_tracking",
     ],
