f1dee4b517355e7b3cd4c4b93f41d896af8815d4
Antonio Espinosa <antespi@gmail.com>
2016-10-14 15:42:29 +0200

    [IMP] mail_tracking performance and bounce process (#103)

--- mail_tracking/models/mail_tracking_event.py
+++ mail_tracking/models/mail_tracking_event.py
@@ -23,7 +23,7 @@ class MailTrackingEvent(models.Model):
     date = fields.Date(
         string="Date", readonly=True, compute="_compute_date", store=True)
     tracking_email_id = fields.Many2one(
-        string='Message', readonly=True,
+        string='Message', readonly=True, required=True, ondelete='cascade',
         comodel_name='mail.tracking.email')
     event_type = fields.Selection(string='Event type', selection=[
         ('sent', 'Sent'),
