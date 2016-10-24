9b49b8d5ef5c11fc5061966e5f7c1d77f0bbd204
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-24 11:46:07 +0200

    Migration of mail_optional_follower_notification to 10.0

--- mail_optional_follower_notification/models/res_partner.py
+++ mail_optional_follower_notification/models/res_partner.py
@@ -2,17 +2,19 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.multi
-    def _notify(self, message, force_send=False, user_signature=True):
+    def _notify(self, message, force_send=False, send_after_commit=True,
+                user_signature=True):
         if self.env.context.get('force_partners_to_notify'):
             partners_to_notify =\
                 self.env.context.get('force_partners_to_notify')
             self = self.filtered(lambda p: p.id in partners_to_notify)
         super(ResPartner, self)._notify(
-            message, force_send=False, user_signature=True)
+            message, force_send=force_send,
+            send_after_commit=send_after_commit, user_signature=user_signature)
--- mail_optional_follower_notification/tests/test_mail_optional_follower_notifications.py
+++ mail_optional_follower_notification/tests/test_mail_optional_follower_notifications.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestMailOptionalFollowernotifications(common.TransactionCase):
@@ -11,8 +11,8 @@ class TestMailOptionalFollowernotifications(common.TransactionCase):
         super(TestMailOptionalFollowernotifications, self).setUp()
         self.partner_obj = self.env['res.partner']
         self.partner_01 = self.env.ref('base.res_partner_2')
-        self.partner_02 = self.env.ref('base.res_partner_3')
-        self.partner_03 = self.env.ref('base.res_partner_4')
+        self.demo_user = self.env.ref('base.user_demo')
+        self.partner_03 = self.demo_user.copy().partner_id
 
     def test_send_email_optional_follower_notifications(self):
         ctx = self.env.context.copy()
@@ -22,10 +22,11 @@ class TestMailOptionalFollowernotifications(common.TransactionCase):
             'default_composition_mode': 'comment',
         })
         mail_compose = self.env['mail.compose.message']
+        self.partner_01.message_subscribe_users(user_ids=[self.demo_user.id])
         values = mail_compose.with_context(ctx)\
             .onchange_template_id(False, 'comment', 'res.partner',
                                   self.partner_01.id)['value']
-        values['partner_ids'] = [(4, self.partner_02.id),
+        values['partner_ids'] = [(4, self.demo_user.partner_id.id),
                                  (4, self.partner_03.id)]
         compose_id = mail_compose.with_context(ctx).create(values)
         compose_id.with_context(ctx).send_mail()
@@ -35,7 +36,7 @@ class TestMailOptionalFollowernotifications(common.TransactionCase):
         self.assertEqual(len(res.ids), 1)
         message = self.env['mail.message']
         for record in res:
-            if record.notified_partner_ids.ids == [self.partner_03.id] and\
+            if record.notification_ids.mapped('res_partner_id').ids == [self.partner_03.id] and\
                     record.partner_ids.ids == [self.partner_03.id]:
                 message += record
         self.assertEqual(len(message.ids), 0)
@@ -48,7 +49,7 @@ class TestMailOptionalFollowernotifications(common.TransactionCase):
              ('res_id', '=', self.partner_01.id)])
         message = self.env['mail.message']
         for record in res:
-            if record.notified_partner_ids.ids == [self.partner_03.id] and\
+            if record.notification_ids.mapped('res_partner_id').ids == [self.partner_03.id] and\
                     record.partner_ids.ids == [self.partner_03.id]:
                 message += record
         self.assertEqual(len(message.ids), 1)
