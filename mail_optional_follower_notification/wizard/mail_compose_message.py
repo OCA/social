6be1a3e5fd4a1eec150a372557c8309ba983df63
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-24 11:46:07 +0200

    Migration of mail_optional_follower_notification to 10.0

--- mail_optional_follower_notification/wizard/mail_compose_message.py
+++ mail_optional_follower_notification/wizard/mail_compose_message.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class MailComposeMessage(models.TransientModel):
