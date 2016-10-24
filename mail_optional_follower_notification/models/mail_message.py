1b0a761388998c685a922ea2c0c0753d987ca7f4
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-24 11:46:07 +0200

    Migration of mail_optional_follower_notification to 10.0

--- mail_optional_follower_notification/models/mail_message.py
+++ mail_optional_follower_notification/models/mail_message.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class MailMessage(models.Model):
@@ -17,3 +17,15 @@ class MailMessage(models.Model):
             force_partners_to_notify = [d['id'] for d in partner_list]
             ctx['force_partners_to_notify'] = force_partners_to_notify
         return super(MailMessage, self.with_context(ctx)).create(values)
+
+    @api.multi
+    def _notify(self, force_send=False, send_after_commit=True,
+                user_signature=True):
+        res = super(MailMessage, self)._notify(
+            force_send=force_send, send_after_commit=send_after_commit,
+            user_signature=user_signature)
+        if not self.env.context.get('notify_followers'):
+            # Needaction only for recipients
+            self.needaction_partner_ids =  [(6, 0, self.partner_ids.ids)]
+        return res
+
