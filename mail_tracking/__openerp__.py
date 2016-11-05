e695e539f348483d9660a4f4cb6bd10cde267d41
Antonio Espinosa <antespi@gmail.com>
2016-11-05 11:38:05 +0100

    Migration script to 9.0.3.0.0

--- mail_tracking/__openerp__.py
+++ mail_tracking/__openerp__.py
@@ -5,7 +5,7 @@
 {
     "name": "Email tracking",
     "summary": "Email tracking system for all mails sent",
-    "version": "9.0.2.1.0",
+    "version": "9.0.3.0.0",
     "category": "Social Network",
     "website": "http://www.tecnativa.com",
     "author": "Tecnativa, "
