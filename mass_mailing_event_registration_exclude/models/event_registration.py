e50fb60660f7369f040fb73a03b8d89d1a572a25
Antonio Espinosa <antespi@gmail.com>
2016-11-09 19:23:30 +0100

    mass_mailing_event (#127)
    OCA Transbot updated translations from Transifex

---
+++ mass_mailing_event_registration_exclude/models/event_registration.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+from .mail_mass_mailing import event_filtered_ids
+
+
+class EventRegistration(models.Model):
+    _inherit = 'event.registration'
+
+    @api.model
+    def search_count(self, domain):
+        res = super(EventRegistration, self).search_count(domain)
+        mass_mailing_id = self.env.context.get('exclude_mass_mailing', False)
+        if mass_mailing_id:
+            res_ids = event_filtered_ids(
+                self, mass_mailing_id, domain, field='email')
+            return len(res_ids) if res_ids else 0
+        return res
