708780cab9202656b2ad64a07e71f69080c9a8b4
Jairo Llopis <yajo.sk8@gmail.com>
2016-11-11 14:41:20 +0100

    [9.0][MIG][mass_mailing_custom_unsubscribe] Migrate.
    - Imported last updates from v8.
    - Adapted to v9.
    - Added a saner default to `mass_mailing.salt` configuration parameter by
      reusing `database.secret` if available, hoping that some day
      https://github.com/odoo/odoo/pull/12040 gets merged.
    - Updated README.
    - Increase security, drop backwards compatibility.
      Security got improved upstream, which would again break compatibility among current addon and future master upstream.
      I choose to break it now and keep it secured future-wise, so I drop the backwards compatibility features.
    - Includes tour tests.
    - Removes outdated tests.
    - Extends the mailing list management form when unsubscriber is a contact.
    - Adds a reason form even if he is not.
    - Avoids all methods that were not model-agnostic.
    
    [FIX][mass_mailing_custom_unsubscribe] Reasons noupdate
    
    After this fix, when you update the addon, you will not lose your customized reasons.
    
    [FIX] Compatibilize with mass_mailing_partner
    
    Current test code was based on the assumption that the `@api.model` decorator on `create()` ensured an empty recordset when running the method, but that's not true. This was causing an incompatibility betwee these tests and the `mass_mailing_partner` addon, which works assuming 0-1 recordsets.
    
    Now records are created from an empty recordset, and thus tests work everywhere.
    
    Update instructions
    
    If the user does not add the unsubscribe snippet, nothing will happen, so it's added to README to avoid confusion when testing/using the addon.
    
    [FIX] Use the right operator to preserve recordsets order
    
    Using `|=` sorts records at will each time (treating them as Python's `set`).
    Using `+=` always appends a record to the end of the set.
    Since we are using the record position in the set, this caused the test to work sometimes and fail other times. Now it works always.

--- mass_mailing_custom_unsubscribe/static/src/js/require_details.js
+++ mass_mailing_custom_unsubscribe/static/src/js/require_details.js
@@ -1,13 +1,25 @@
-/* © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+/* Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl). */
+odoo.define("mass_mailing_custom_unsubscribe.require_details",
+            function (require) {
+    "use strict";
+    var animation = require("web_editor.snippets.animation");
 
-"use strict";
-(function ($) {
-    $("#reason_form :radio").change(function(event) {
-        $("textarea[name=details]").attr(
-            "required",
-            $(event.target).is("[data-details-required]")
-        );
+    return animation.registry.mass_mailing_custom_unsubscribe_require_details =
+    animation.Class.extend({
+        selector: ".js_unsubscription_reason",
+
+        start: function () {
+            this.$radio = this.$(":radio");
+            this.$details = this.$("[name=details]");
+            this.$radio.on("change click", $.proxy(this.toggle, this));
+            this.$radio.filter(":checked").trigger("change");
+        },
+
+        toggle: function (event) {
+            this.$details.prop(
+                "required",
+                $(event.target).is("[data-details-required]"));
+        },
     });
-    $("#reason_form :radio:checked").change();
-})(jQuery);
+});
