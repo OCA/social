f303cfb2d58e53a7d067b1a867b1e61ed6036075
Jairo Llopis <yajo.sk8@gmail.com>
2016-11-11 14:41:20 +0100

    [9.0][MIG][mass_mailing_custom_unsubscribe] Migrate.
    - Imported last updates from v8.
    - Adapted to v9.
    - Added a saner default to `mass_mailing.salt` configuration parameter by
      reusing `database.secret` if available, hoping that some day
      https://github.com/odoo/odoo/pull/12040 gets merged.
    - Updated README.
    - Increase security, drop backwards compatibility.
      Security got improved upstream, which would again break compatibility among current addon and future master upstream.
      I choose to break it now and keep it secured future-wise, so I drop the backwards compatibility features.
    - Includes tour tests.
    - Removes outdated tests.
    - Extends the mailing list management form when unsubscriber is a contact.
    - Adds a reason form even if he is not.
    - Avoids all methods that were not model-agnostic.
    
    [FIX][mass_mailing_custom_unsubscribe] Reasons noupdate
    
    After this fix, when you update the addon, you will not lose your customized reasons.
    
    [FIX] Compatibilize with mass_mailing_partner
    
    Current test code was based on the assumption that the `@api.model` decorator on `create()` ensured an empty recordset when running the method, but that's not true. This was causing an incompatibility betwee these tests and the `mass_mailing_partner` addon, which works assuming 0-1 recordsets.
    
    Now records are created from an empty recordset, and thus tests work everywhere.
    
    Update instructions
    
    If the user does not add the unsubscribe snippet, nothing will happen, so it's added to README to avoid confusion when testing/using the addon.
    
    [FIX] Use the right operator to preserve recordsets order
    
    Using `|=` sorts records at will each time (treating them as Python's `set`).
    Using `+=` always appends a record to the end of the set.
    Since we are using the record position in the set, this caused the test to work sometimes and fail other times. Now it works always.

--- mass_mailing_custom_unsubscribe/__manifest__.py
+++ mass_mailing_custom_unsubscribe/__manifest__.py
@@ -1,52 +1,34 @@
 # -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
-##############################################################################
-#
-#    OpenERP, Odoo Source Management Solution
-#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Customizable unsubscription process on mass mailing emails",
+    "summary": "Know unsubscription reasons, track them",
     'category': 'Marketing',
-    'version': '8.0.2.0.0',
+    'version': '9.0.2.0.0',
     'depends': [
-        'mass_mailing',
-        'website_crm',
+        'website_mass_mailing',
     ],
     'data': [
         'security/ir.model.access.csv',
-        'data/install_salt.xml',
-        'data/mail.unsubscription.reason.csv',
+        'data/mail_unsubscription_reason.xml',
+        'templates/general_reason_form.xml',
+        'templates/mass_mailing_contact_reason.xml',
         'views/assets.xml',
         'views/mail_unsubscription_reason_view.xml',
         'views/mail_mass_mailing_list_view.xml',
         'views/mail_unsubscription_view.xml',
-        'views/pages.xml',
+    ],
+    "demo": [
+        'demo/assets.xml',
     ],
     'images': [
-        'images/failure.png',
         'images/form.png',
-        'images/success.png',
     ],
     'author': 'Antiun Ingeniería S.L., '
               'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.antiun.com',
+    'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
 }
