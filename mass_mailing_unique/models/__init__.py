ffad1433398091463eee7a84f106eb0d5cf98e4c
cubells <vicent@vcubells.net>
2016-11-30 21:15:57 +0100

    [9.0] [MIG] mass_mailing_unique module

--- mass_mailing_unique/models/__init__.py
+++ mass_mailing_unique/models/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 
 from . import mass_mailing
