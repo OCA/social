b710f79c56f52ee7deb1b526573a8aa959be7cdb
cubells <vicent@vcubells.net>
2016-12-20 08:57:14 +0100

    [9.0] [MIG] website_mail_snippet_vertical_resize_base module

--- website_mail_snippet_vertical_resize_base/__openerp__.py
+++ website_mail_snippet_vertical_resize_base/__openerp__.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2016 Tecnativa - Jairo Llopis
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Base for Vertical Resizing of Snippets",
     "summary": "Allow input of height in pixels with just a class",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Website",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "website": "http://tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
         "website_mail",
+        "mass_mailing",
     ],
     "data": [
         "views/assets.xml",
--- website_mail_snippet_vertical_resize_base/static/src/js/vertical_resize.js
+++ website_mail_snippet_vertical_resize_base/static/src/js/vertical_resize.js
@@ -1,38 +1,44 @@
-/* © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+/* Copyright 2016 Tecnativa - Jairo Llopis
+ * Copyright 2016 Tecnativa - Vicent Cubells
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html). */
 
-"use strict";
-(function ($) {
-    var _t = openerp._t,
-        prompt = openerp.website.prompt,
-        snippet = openerp.website.snippet;
+odoo.define('website_mail_snippet_vertical_resize_base.editor', function (require) {
+    "use strict";
 
-    snippet.options.vertical_resize = snippet.Option.extend({
-        start: function () {
+var ajax = require('web.ajax');
+var core = require('web.core');
+var base = require('web_editor.base');
+var web_editor = require('web_editor.editor');
+var options = require('web_editor.snippets.options');
+
+var _t = core._t;
+
+var editor = options.extend({
+    start: function () {
             var self = this;
             self._super();
             return self.$el.find(".js_vertical_resize").click(function(){
                 return self.ask();
             });
         },
-
-        ask: function() {
-            var self = this;
-            return prompt({
+    ask: function(type, value) {
+        var self = this;
+        if (type !== "click") return;
+        return web_editor.prompt({
                 window_title: _t("Set element height"),
                 input: _t("Element height in pixels"),
-            }).then(function (answer) {
+            })
+            .done(function (answer) {
                 return self.resize(answer);
             });
         },
-
-        resize: function(size) {
-            this.$target.css("height", String(size) + "px");
+    resize: function(size) {
+        this.$target.css("height", String(size) + "px");
 
             // Old-school height attribute changed too if needed
             if (this.$target.attr("height")) {
                 this.$target.attr("height", size);
             }
-        },
-    });
-})(jQuery);
+    }
+});
+});
