763dde0d548d94d4c02a74d974c46286e5539e92
darshan-serpent <darshan.patel.serpentcs@gmail.com>
2017-01-17 12:26:51 +0530

    [IMP] advanced search for negative expression

--- base_search_mail_content/__openerp__.py
+++ base_search_mail_content/__openerp__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Base Mail Search Content",
+    "name": "Base Search Mail Content",
     "version": "9.0.1.0.0",
     "author": "Eficent,"
               "SerpentCS,"
