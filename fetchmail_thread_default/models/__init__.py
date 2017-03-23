7e5fde79de7ab0be0ede7d1277a623700da1f9f2
Jairo Llopis <yajo.sk8@gmail.com>
2017-03-23 15:03:23 +0100

    [ADD][fetchmail_thread_default] Default thread for incoming mails
    This addon lets the sysadmin choose a default mail thread sink for incoming mails.
    
    You can use it to forward all unbound incoming emails to a `mail.channel` where only certain users are subscribed and can triage them.

---
+++ fetchmail_thread_default/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import fetchmail_server
+from . import mail_thread
