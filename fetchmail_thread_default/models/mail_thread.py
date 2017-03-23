01580ea71e365f7e88bd6e2fe755bd46df9faf3c
Jairo Llopis <yajo.sk8@gmail.com>
2017-03-23 15:03:23 +0100

    [ADD][fetchmail_thread_default] Default thread for incoming mails
    This addon lets the sysadmin choose a default mail thread sink for incoming mails.
    
    You can use it to forward all unbound incoming emails to a `mail.channel` where only certain users are subscribed and can triage them.

---
+++ fetchmail_thread_default/models/mail_thread.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models
+
+
+class MailThread(models.AbstractModel):
+    _inherit = "mail.thread"
+
+    @api.model
+    def message_process(self, model, message, custom_values=None,
+                        save_original=False, strip_attachments=False,
+                        thread_id=None):
+        server = self.env["fetchmail.server"].browse(
+            self.env.context.get("fetchmail_server_id"))
+        if server.default_thread_id and not (model or thread_id):
+            model = server.default_thread_id._name
+            thread_id = server.default_thread_id.id
+        return super(
+            MailThread,
+            self.with_context(mail_create_nosubscribe=True)
+        ).message_process(
+            model,
+            message,
+            custom_values,
+            save_original,
+            strip_attachments,
+            thread_id,
+        )
