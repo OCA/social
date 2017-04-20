8e8219357bc754c532852cf9d7b767cc56c9c598
Emanuel Cino <ecino@compassion.ch>
2017-04-20 13:19:59 +0200

    Add Sendgrid module from v9

---
+++ mail_sendgrid/tests/__init__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2017 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Emanuel Cino
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+
+from . import test_mail_sendgrid
