81ccfd78cd80cf4599f278473426ab085637a1d8
Emanuel Cino <ecino@compassion.ch>
2017-04-20 13:19:59 +0200

    Add Sendgrid module from v9

---
+++ mail_sendgrid/models/__init__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Compassion CH (http://www.compassion.ch)
+#    Releasing children from poverty in Jesus' name
+#    @author: Roman Zoller
+#
+#    The licence is in the file __openerp__.py
+#
+##############################################################################
+
+from . import mail_mail
+from . import substitution
+from . import sendgrid_template
+from . import email_template
+from . import email_lang_template
+from . import email_tracking
+from . import mail_tracking_event
