c00876e9b837fff88c3040ce0d264ee64bb0e935
Emanuel Cino <ecino@compassion.ch>
2017-04-20 13:19:59 +0200

    Add Sendgrid modules

---
+++ mail_sendgrid_mass_mailing/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#       ______ Releasing children from poverty      _
+#      / ____/___  ____ ___  ____  ____ ___________(_)___  ____
+#     / /   / __ \/ __ `__ \/ __ \/ __ `/ ___/ ___/ / __ \/ __ \
+#    / /___/ /_/ / / / / / / /_/ / /_/ (__  |__  ) / /_/ / / / /
+#    \____/\____/_/ /_/ /_/ .___/\__,_/____/____/_/\____/_/ /_/
+#                        /_/
+#                            in Jesus' name
+#
+#    Copyright (C) 2016 Compassion CH (http://www.compassion.ch)
+#    @author: Emanuel Cino <ecino@compassion.ch>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Mass Mailing with SendGrid',
+    'version': '9.0.1.0.0',
+    'category': 'Social Network',
+    'author': 'Compassion CH',
+    'website': 'http://www.compassion.ch',
+    'depends': ['mail_sendgrid', 'mail_tracking_mass_mailing'],
+    'data': [
+        'views/mass_mailing_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+    'external_dependencies': {
+        'python': ['sendgrid'],
+    },
+}
