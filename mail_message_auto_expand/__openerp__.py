c09249d62cc823853042707e5158d14bdd49a1ad
kiplangatdan <kiplangatdan@gmail.com>
2017-05-08 18:33:56 +0300

    [WIP][ADD][8.0]mail_message_auto_expand module

---
+++ mail_message_auto_expand/__openerp__.py
@@ -0,0 +1,13 @@
+﻿{
+    'name': 'Mail Message Auto Expand',
+    'version': '8.0',
+    'summary': 'Expands the Read More Automatically',
+    'description': '',
+    'author': 'Sunflower IT',
+    'website': 'http://www.sunflowerweb.nl',
+    'category': 'Social Network',
+    'images': [],
+    'depends': ['mail'],
+    'data': [],
+    'installable': True,
+}
