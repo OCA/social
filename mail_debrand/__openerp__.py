3b89d00189715c9064db9ea993334bcbb3bcb725
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-05-15 16:52:27 +0200

    [MIG] mail_debrand
    There's no commits preservation, as the module doesn't serve as is on v9.

---
+++ mail_debrand/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Tecnativa - Jairo Llopis
+# Copyright 2017 SerpentCS - Darshan Patel
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Mail Debrand",
+    "summary": "Remove Odoo branding in sent emails",
+    "version": "10.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Eficent, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+}
