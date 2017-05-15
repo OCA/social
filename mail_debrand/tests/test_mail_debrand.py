185503a85b74028b7593180a999cc197d5041287
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-05-15 16:52:27 +0200

    [MIG] mail_debrand
    There's no commits preservation, as the module doesn't serve as is on v9.

---
+++ mail_debrand/tests/test_mail_debrand.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import common
+
+
+class TestMailDebrand(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestMailDebrand, cls).setUpClass()
+        cls.template = cls.env.ref(
+            'mail.mail_template_data_notification_email_default'
+        )
+
+    def test_generate_email_simple(self):
+        res = self.template.generate_email(
+            self.env.user.id, fields=['body_html'],
+        )
+        self.assertNotIn('using', res)
+
+    def test_generate_email_multi(self):
+        res = self.template.generate_email(
+            self.env.user.ids, fields=['body_html'],
+        )
+        self.assertNotIn('using', res[res.keys()[0]])
