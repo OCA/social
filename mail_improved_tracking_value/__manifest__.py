db2467134481be3491cf04fab50ced68e98da427
Thierry Ducrest <thierry@ducrest.net>
2017-06-07 15:01:30 +0200

    [MIG] mail_improved_tracking_value: migration to Odoo 11
    Improve the tracking of changed values on database fields
    ("mail.tracking.value" functionality) by adding many2many and one2many support.
    As well as a more user friendly view to consult changes recorded.

--- mail_improved_tracking_value/__manifest__.py
+++ mail_improved_tracking_value/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Camptocamp SA
+# Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Improved tracking value change',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'summary': 'Improves tracking changed values for certain type of fields.'
                'Adds a user-friendly view to consult them.',
     'author': 'Camptocamp, Odoo Community Association (OCA)',
     'license': 'AGPL-3',
     'category': 'Tools',
-    'website': 'http://www.camptocamp.com',
+    'website': 'https://github.com/OCA/social',
     'depends': [
         'base',
         'mail'
@@ -18,6 +17,4 @@
     'data': [
         'views/mail_tracking_value.xml'
     ],
-    'installable': True,
-    'auto_install': False,
 }
