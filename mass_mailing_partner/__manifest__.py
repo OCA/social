7821cbc726df47cbd9f6e35513986c432600ef3d
David Vidal <chienandalu@gmail.com>
2017-06-14 20:47:41 +0200

    [MIG] mass_mailing_partner: Migration to 10.0

--- mass_mailing_partner/__manifest__.py
+++ mass_mailing_partner/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2015 Antonio Espinosa <antonioea@antiun.com>
-# © 2015 Javier Iniesta <javieria@antiun.com>
-# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Javier Iniesta <javieria@antiun.com>
+# Copyright 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Link partners with mass-mailing",
-    "version": "8.0.2.1.0",
+    "version": "10.0.1.0.1",
     "author": "Tecnativa, "
-              "Antiun Ingeniería S.L., "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
     "license": "AGPL-3",
     "category": "Marketing",
     "depends": [
--- mass_mailing_partner/tests/test_mail_mass_mailing_contact.py
+++ mass_mailing_partner/tests/test_mail_mass_mailing_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2015 Antonio Espinosa <antonioea@antiun.com>
-# © 2015 Javier Iniesta <javieria@antiun.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Javier Iniesta <javieria@antiun.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import base
