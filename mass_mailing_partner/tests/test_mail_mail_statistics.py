1657e7740601d3cda7af9eb61750c8511cbd0927
David Vidal <chienandalu@gmail.com>
2017-06-14 20:47:41 +0200

    [MIG] mass_mailing_partner: Migration to 10.0

--- mass_mailing_partner/tests/test_mail_mail_statistics.py
+++ mass_mailing_partner/tests/test_mail_mail_statistics.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Javier Iniesta <javieria@antiun.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import base
--- mass_mailing_partner/tests/test_partner_mail_list_wizard.py
+++ mass_mailing_partner/tests/test_partner_mail_list_wizard.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2015 Antonio Espinosa <antonioea@antiun.com>
-# © 2015 Javier Iniesta <javieria@antiun.com>
+# Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Javier Iniesta <javieria@antiun.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import base
-from openerp.exceptions import Warning as UserError
+from odoo.exceptions import UserError
 
 
 class PartnerMailListWizardCase(base.BaseCase):
