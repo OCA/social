72cfa5ac069087de67fb4fffc7f34dcc8b88cdf9
David Vidal <chienandalu@gmail.com>
2017-06-22 13:19:47 +0200

    [10.0] mail_tracking_mass_mailing: Migration to 10.0

--- mail_tracking_mass_mailing/models/mail_tracking_email.py
+++ mail_tracking_mass_mailing/models/mail_tracking_email.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Vicent Cubells - <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
+from odoo import models, fields, api
 
 
 class MailTrackingEmail(models.Model):
--- mail_tracking_mass_mailing/models/mail_tracking_event.py
+++ mail_tracking_mass_mailing/models/mail_tracking_event.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Vicent Cubells - <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class MailTrackingEvent(models.Model):
