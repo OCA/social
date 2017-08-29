59fa39ae939f8eb603e8ad68a9033353f3763782
Lucas Huber <lh@koina.cc>
2017-08-29 08:20:16 +0200

    [MIG] Change version nr.

--- mail_debrand/__manifest__.py
+++ mail_debrand/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Mail Debrand",
     "summary": "Remove Odoo branding in sent emails",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Social Network",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
