2563896b8d2898a0b3f0ad1f6f2c6f8570fd8ae7
Jairo Llopis <Yajo@users.noreply.github.com>
2017-10-10 10:29:35 +0200

    Upgrade version

--- mail_tracking/__openerp__.py
+++ mail_tracking/__openerp__.py
@@ -5,7 +5,7 @@
 {
     "name": "Email tracking",
     "summary": "Email tracking system for all mails sent",
-    "version": "9.0.3.0.1",
+    "version": "9.0.3.0.2",
     "category": "Social Network",
     "website": "http://www.tecnativa.com",
     "author": "Tecnativa, "
