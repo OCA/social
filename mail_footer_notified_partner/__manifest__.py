ca6bd04c1252b5dcf46541a1c7cdaa113137080b
SimoRubi <32064796+SimoRubi@users.noreply.github.com>
2017-10-10 11:37:56 +0200

    [MIG] [10.0] mail_footer_notified_partner (#200)
    * [NEW][mail_footer_notified_partners]
    This module adds into the footer the partner's name notified by this email
    
    * [CHG] Use common.TransactionCase
    
    * OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    * [FIX][mail_footer_notified_partners] Use @api.model instead of @api.one.
    
    * OCA Transbot updated translations from Transifex
    
    * [FIX] remove en.po that was erroneously created by transbot
    
    * Pass context
    
    * OCA Transbot updated translations from Transifex
    
    OCA Transbot updated translations from Transifex
    
    * [MIG] mail_footer_notified_partner: Migration to 10.0

---
+++ mail_footer_notified_partner/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 ACSONE SA/NV <https://acsone.eu>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Notified partners in mail footer",
+    "summary": """
+        This module adds the list of notified partners in the footer of
+        notification e-mails sent by Odoo.
+        """,
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "installable": True,
+    "website": "http://acsone.eu",
+    "category": "Mail",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+}
