1d5c31ef60b9261b58a74c3dcf7a092e66638f65
David <david.vidal@tecnativa.com>
2017-10-17 20:38:11 +0200

    [10.0][IMP] mail_tracking_mailgun: add partner mail checks

--- mail_tracking_mailgun/models/__init__.py
+++ mail_tracking_mailgun/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import ir_mail_server
 from . import mail_tracking_email
+from . import mail_tracking_event
+from . import res_partner
--- mail_tracking_mailgun/models/ir_mail_server.py
+++ mail_tracking_mailgun/models/ir_mail_server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Tecnativa - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import json
