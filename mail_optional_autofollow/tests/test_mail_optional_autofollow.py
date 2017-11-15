838101b4de27061d638fafc297676e48993d55c9
Damien Carlier <damien.carlier@gmail.com>
2017-11-15 14:34:53 +0100

    [MIG] mail_optional_autofollow: Migration to 11.0

--- mail_optional_autofollow/tests/test_mail_optional_autofollow.py
+++ mail_optional_autofollow/tests/test_mail_optional_autofollow.py
@@ -2,7 +2,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestAttachExistingAttachment(common.TransactionCase):
