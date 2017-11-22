e9a49cc74e73e45108ec3f1a55067134b136df37
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-22 11:49:55 +0100

    fixup! [ADD] mass_mailing_resend

--- mass_mailing_resend/models/mass_mailing.py
+++ mass_mailing_resend/models/mass_mailing.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
-# Copyright 2016 Tecnativa - Vicent Cubells
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
