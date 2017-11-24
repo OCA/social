d7412715ce151290974c04ce1750044ed6d3466b
Simone Orsi <simahawk@gmail.com>
2017-11-24 13:25:49 +0100

    [imp] mail_digest: fallback to default website
    Depending on the request  might fail
    nevertheless the website should be there to be found.
    Fallback to search and use it if possible for digest' site name

--- mail_digest/__openerp__.py
+++ mail_digest/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Mail digest',
     'summary': """Basic digest mail handling.""",
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.0.1',
     'license': 'AGPL-3',
     'author': 'Camptocamp,Odoo Community Association (OCA)',
     'depends': [
