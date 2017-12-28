86243ed254520a0d638c2d9c38de5210601dc6f8
eLBati <lorenzo.battistini@agilebg.com>
2017-12-28 11:03:46 +0100

    ADD mail_notify_bounce

---
+++ mail_notify_bounce/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import fetchmail
+from . import mail_thread
