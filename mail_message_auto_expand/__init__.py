6c06b3a26e984ba23e3a9349a65d6b509d58d5ca
kiplangatdan <kiplangatdan@gmail.com>
2018-01-11 23:33:18 +0300

    [FIX]replace body_short

--- mail_message_auto_expand/__init__.py
+++ mail_message_auto_expand/__init__.py
@@ -1,3 +1,5 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import mail
+from . import models
--- mail_message_auto_expand/__openerp__.py
+++ mail_message_auto_expand/__openerp__.py
@@ -1,13 +1,22 @@
-﻿{
-    'name': 'Mail Message Auto Expand',
-    'version': '8.0',
-    'summary': 'Expands the Read More Automatically',
-    'description': '',
-    'author': 'Sunflower IT',
-    'website': 'http://www.sunflowerweb.nl',
-    'category': 'Social Network',
-    'images': [],
-    'depends': ['mail'],
-    'data': [],
-    'installable': True,
+﻿# -*- coding: utf-8 -*-
+# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Message Auto Expand",
+    "summary": "Expands the Read More Automatically",
+    "version": "8.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://sunflowerweb.nl",
+    "author": "Sunflower IT, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+    "data": [],
+    "qweb": [
+            "static/src/xml/mail_autoexpand.xml",
+        ],
 }
