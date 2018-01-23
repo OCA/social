d6bf75f3e10520e1630a750f6e44e27319b91e40
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-01-23 13:36:17 +0100

    [ADD] mass_mailing_resend (#210)
    * [ADD] mass_mailing_resend
    
    Resend mass mailings
    ====================
    
    A frequent need for users of mass mailings is to resend one mailing that has
    already been sent in the past to new recipients that haven't received yet that
    mail. But the problem is to know which are the applicable ones.
    
    Odoo already includes a method in its mass mailing logic that avoids to resend
    the same mail 2 times for one mass mailing, and for v9, there was a trick that
    allows to modify the state of a mass mailing from kanban view, covering the
    need.
    
    But now on v10 both status bar in form view and dragging between states in
    kanban are not allowed.
    
    This module introduces a button to restart a mass mailing to draft state,
    allowing you to reevaluate the sending domain or list for performing again
    the mailing.
    
    Usage
    =====
    
    * Go to *Mass mailing > Mailings > Mass Mailings*.
    * Click on one record that is done or create a new one and send it.
    * You will see a button called "Resend".
    * If you click on it, mass mailing will be set to Draft again.
    
    Known issues / Roadmap
    ======================
    
    * Add an indicator / filter for knowing resent mailings.
    * Include information on the number of new recipients to be sent on the
      resending (through `get_remaining_recipients` method).

---
+++ mass_mailing_resend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Resend mass mailings",
+    "version": "10.0.1.0.0",
+    "category": "Marketing",
+    "website": "https://github.com/OCA/social",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mass_mailing",
+    ],
+    "data": [
+        "views/mass_mailing_views.xml",
+    ],
+}
