5aa0e87e345799c78ea11fcaa49c0999e3bc0ee2
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-02-01 16:35:36 +0100

    [FIX] mail_tracking_mailgun: Message error on bounce

--- mail_tracking_mailgun/models/res_partner.py
+++ mail_tracking_mailgun/models/res_partner.py
@@ -30,8 +30,7 @@ class ResPartner(models.Model):
             body = _('Email has been bounced: %s\n'
                      'Reason: %s\n'
                      'Event: %s') % (partner.email, reason,
-                                     event and event.get('Message-Id',
-                                                         _('unknown')))
+                                     event and event.id or _('unknown'))
             partner.message_post(body=body)
 
     @api.multi
