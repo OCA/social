29a9cc4a16efd3e4add5782cd8a60cdb60425657
David <david.vidal@tecnativa.com>
2018-02-15 19:46:21 +0100

    [FIX] mail_tracking_mailgun: manual sync gets events from other recipients

--- mail_tracking_mailgun/models/mail_tracking_event.py
+++ mail_tracking_mailgun/models/mail_tracking_event.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class MailTrackingEvent(models.Model):
