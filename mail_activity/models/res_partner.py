78b365e6ff8ce6b3b014345b8869a6f559889895
andreparames <github@andreparames.com>
2018-03-01 10:25:50 +0100

    [9.0] mail_activity: convert mixin to decorator
    The Odoo v9 model mechanism has some issues with loading
    AbstractModels as mixins; specifically, the computed fields of the
    original model no longer work reliably.
    
    This new approach uses a Python decorator to change the class when
    the module is being loaded, so the Odoo model mechanisms only see
    a class that does a simple inheritance, and already "comes" with
    the new fields/methods related to the activities.
    
    It's essentially the same as copy-pasting the fields and methods
    directly into the class.

--- mail_activity/models/res_partner.py
+++ mail_activity/models/res_partner.py
@@ -2,8 +2,9 @@
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import models
+from .mail_activity_mixin import setup_mail_actitivities
 
 
+@setup_mail_actitivities
 class ResPartner(models.Model):
-    _inherit = ['res.partner', 'mail.activity.mixin']
-    _name = 'res.partner'
+    _inherit = 'res.partner'
