ca8fc5125ae241c290aca35300cc48426f2993f6
Holger Brunn <hbrunn@therp.nl>
2018-03-01 16:50:54 +0100

    [ADD] mail_autogenerated_header

---
+++ mail_autogenerated_header/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2018 Therp BV <https://therp.nl>
+# Copyright 2022 Hunki Enterprises BV <https://hunki-enterprises.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import ir_mail_server
+from . import mail_thread
+from . import res_partner
