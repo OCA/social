a14e01a6966d6c79aabb5513893ed577cf3d69a6
andreparames <github@andreparames.com>
2018-03-01 10:25:50 +0100

    [9.0] mail_activity: convert mixin to decorator
    The Odoo v9 model mechanism has some issues with loading
    AbstractModels as mixins; specifically, the computed fields of the
    original model no longer work reliably.
    
    This new approach uses a Python decorator to change the class when
    the module is being loaded, so the Odoo model mechanisms only see
    a class that does a simple inheritance, and already "comes" with
    the new fields/methods related to the activities.
    
    It's essentially the same as copy-pasting the fields and methods
    directly into the class.

--- mail_activity/models/mail_activity_mixin.py
+++ mail_activity/models/mail_activity_mixin.py
@@ -1,34 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from openerp import api, fields, models
+from openerp import api, fields
 
 
-class MailActivityMixin(models.AbstractModel):
-    """ Mail Activity Mixin is a mixin class to use if you want to add
-    activities management on a model. It works like the mail.thread mixin. It
-    defines an activity_ids one2many field toward activities using res_id and
-    res_model_id.
-    Various related / computed fields are also added to have a global status of
-    activities on documents.
-
-    Activities come with a new JS widget for the form view. It is integrated in
-    the Chatter widget although it is a separate widget. It displays activities
-    linked to the current record and allow to schedule, edit and mark done
-    activities.
-    Use widget="mail_activity" on activity_ids field in form view to use it.
-
-    There is also a kanban widget defined. It defines a small widget to
-    integrate in kanban vignettes. It allow to manage activities directly from
-    the kanban view. Use widget="kanban_activity" on activitiy_ids field in
-    kanban view to use it."""
-    _name = 'mail.activity.mixin'
-    _description = 'Activity Mixin'
-
-    activity_ids = fields.One2many(
+def setup_mail_actitivities(cls):
+    """ Mixin using AbstractModels are problematic in Odoo v9, since they
+        prevent the new-style computed fields from working correctly.
+        This decorator edits the original class in-place, so the Odoo model
+        is left unchanged. """
+    cls.activity_ids = fields.One2many(
         'mail.activity', 'res_id', 'Activities',
         auto_join=True,
         domain=lambda self: [('res_model', '=', self._name)])
-    activity_state = fields.Selection([
+    cls.activity_state = fields.Selection([
         ('overdue', 'Overdue'),
         ('today', 'Today'),
         ('planned', 'Planned')], string='State',
@@ -36,66 +20,20 @@ class MailActivityMixin(models.AbstractModel):
         help='Status based on activities\n'
         'Overdue: Due date is already passed\n'
         'Today: Activity date is today\nPlanned: Future activities.')
-    activity_user_id = fields.Many2one(
+    cls.activity_user_id = fields.Many2one(
         'res.users', 'Responsible',
         related='activity_ids.user_id',
         search='_search_activity_user_id')
-    activity_type_id = fields.Many2one(
+    cls.activity_type_id = fields.Many2one(
         'mail.activity.type', 'Next Activity Type',
         related='activity_ids.activity_type_id',
         search='_search_activity_type_id')
-    activity_date_deadline = fields.Date(
+    cls.activity_date_deadline = fields.Date(
         'Next Activity Deadline', related='activity_ids.date_deadline',
         readonly=True, store=True)  # store to enable ordering + search
-    activity_summary = fields.Char(
+    cls.activity_summary = fields.Char(
         'Next Activity Summary', related='activity_ids.summary',
         search='_search_activity_summary')
-
-    @api.model
-    def _setup_complete(self):
-        """ Copied and disabled part of the method, since it doesn't work
-            correctly for AbstractModels. Odoo fixed this for v10 and above
-            in 92144ef """
-        cls = type(self)
-
-        ## set up field triggers
-        #for field in cls._fields.itervalues():
-        #    # dependencies of custom fields may not exist; ignore that case
-        #    exceptions = (Exception,) if field.manual else ()
-        #    with tools.ignore(*exceptions):
-        #        field.setup_triggers(self.env)
-        #
-        ## add invalidation triggers on model dependencies
-        #if cls._depends:
-        #    for model_name, field_names in cls._depends.iteritems():
-        #        model = self.env[model_name]
-        #        for field_name in field_names:
-        #            field = model._fields[field_name]
-        #            for dependent in cls._fields.itervalues():
-        #                model._field_triggers.add(field, (dependent, None))
-
-        # determine old-api structures about inherited fields
-        cls._inherits_reload()
-
-        # register stuff about low-level function fields
-        cls._init_function_fields(cls.pool, self._cr)
-
-        # register constraints and onchange methods
-        cls._init_constraints_onchanges()
-
-        # check defaults
-        for name in cls._defaults:
-            assert name in cls._fields, \
-                "Model %s has a default for nonexiting field %s" % (cls._name, name)
-
-        # validate rec_name
-        if cls._rec_name:
-            assert cls._rec_name in cls._fields, \
-                "Invalid rec_name %s for model %s" % (cls._rec_name, cls._name)
-        elif 'name' in cls._fields:
-            cls._rec_name = 'name'
-        elif 'x_name' in cls._fields:
-            cls._rec_name = 'x_name'
 
     @api.depends('activity_ids.state')
     def _compute_activity_state(self):
@@ -107,26 +45,35 @@ class MailActivityMixin(models.AbstractModel):
                 record.activity_state = 'today'
             elif 'planned' in states:
                 record.activity_state = 'planned'
+    cls._compute_activity_state = _compute_activity_state
 
     @api.model
     def _search_activity_user_id(self, operator, operand):
         return [('activity_ids.user_id', operator, operand)]
+    cls._search_activity_user_id = _search_activity_user_id
 
     @api.model
     def _search_activity_type_id(self, operator, operand):
         return [('activity_ids.activity_type_id', operator, operand)]
+    cls._search_activity_type_id = _search_activity_type_id
 
     @api.model
     def _search_activity_summary(self, operator, operand):
         return [('activity_ids.summary', operator, operand)]
+    cls._search_activity_summary = _search_activity_summary
+
+    original_unlink = cls.unlink
 
     @api.multi
     def unlink(self):
         """ Override unlink to delete records activities through
         (res_model, res_id). """
         record_ids = self.ids
-        result = super(MailActivityMixin, self).unlink()
+        result = original_unlink(self)
         self.env['mail.activity'].sudo().search(
             [('res_model', '=', self._name), ('res_id', 'in', record_ids)]
         ).unlink()
         return result
+    cls.unlink = unlink
+
+    return cls
