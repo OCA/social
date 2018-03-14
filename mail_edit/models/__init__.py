b361f3248e90c4962a0c197a33ef3ac0969ddfb4
kiplangatdan <kiplangatdan@gmail.com>
2018-03-14 12:39:42 +0300

    [8.0][ADD]mail_edit

---
+++ mail_edit/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import compose_message
+from . import res_request_link
