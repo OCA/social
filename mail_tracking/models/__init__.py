4c0a9e46c96fcc384ee4f412295b600bbe587b2a
David <david.vidal@tecnativa.com>
2018-03-19 16:41:26 +0100

    [MIG] mail_tracking: Migration to 11.0

--- mail_tracking/models/__init__.py
+++ mail_tracking/models/__init__.py
@@ -1,7 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# flake8: noqa
 
 from . import ir_mail_server
 from . import mail_mail
