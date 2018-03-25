5b7b097b877a8972ffa280b427d408cee9f2ad53
celm1990 <celm1990@hotmail.com>
2018-03-25 13:20:18 -0500

    [11][MIG] email_template_qweb: Migration to 11.0

--- email_template_qweb/models/mail_template.py
+++ email_template_qweb/models/mail_template.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016 Therp BV <http://therp.nl>
+# Copyright 2016 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, tools, models
 
@@ -12,33 +11,32 @@ class MailTemplate(models.Model):
         default='jinja2', required=True)
     body_view_id = fields.Many2one(
         'ir.ui.view', 'Body view', domain=[('type', '=', 'qweb')])
-    body_view_arch = fields.Text(related=['body_view_id', 'arch'])
+    body_view_arch = fields.Text(related='body_view_id.arch')
 
     @api.multi
     def generate_email(self, res_ids, fields=None):
         multi_mode = True
-        if isinstance(res_ids, (int, long)):
+        if isinstance(res_ids, int):
             res_ids = [res_ids]
             multi_mode = False
         result = super(MailTemplate, self).generate_email(
             res_ids, fields=fields
         )
-        for record_id, this in self.get_email_template(res_ids).iteritems():
-            if this.body_type == 'qweb' and\
+        for res_id, template in self.get_email_template(res_ids).items():
+            if template.body_type == 'qweb' and\
                     (not fields or 'body_html' in fields):
-                for record in self.env[this.model].browse(record_id):
-                    body_html = this.body_view_id.render({
+                for record in self.env[template.model].browse(res_id):
+                    body_html = template.body_view_id.render({
                         'object': record,
-                        'email_template': this,
+                        'email_template': template,
                     })
                     # Some wizards, like when sending a sales order, need this
                     # fix to display accents correctly
-                    if not isinstance(body_html, unicode):
-                        body_html = body_html.decode('utf-8')
-                    result[record_id]['body_html'] = self.render_post_process(
+                    body_html = tools.ustr(body_html)
+                    result[res_id]['body_html'] = self.render_post_process(
                         body_html
                     )
-                    result[record_id]['body'] = tools.html_sanitize(
-                        result[record_id]['body_html']
+                    result[res_id]['body'] = tools.html_sanitize(
+                        result[res_id]['body_html']
                     )
         return multi_mode and result or result[res_ids[0]]
