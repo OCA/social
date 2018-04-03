b1a5bac39465b6c6789ec4d023bf992d926359e8
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-04-03 12:04:11 +0200

    migration of mail_optional_autofollow to 11.0

--- mail_optional_autofollow/__manifest__.py
+++ mail_optional_autofollow/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
@@ -10,7 +9,7 @@
               'Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Social Network',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'mail',
--- mail_optional_autofollow/wizard/__init__.py
+++ mail_optional_autofollow/wizard/__init__.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import mail_compose_message
