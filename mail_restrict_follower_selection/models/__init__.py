5e2a687032a8810ff6fbf29519442a3091c5b08d
Enric Tobella <etobella@creublanca.es>
2018-04-04 10:19:13 +0200

    [IMP] disable the option to create followers that do not meet the conditions

--- mail_restrict_follower_selection/models/__init__.py
+++ mail_restrict_follower_selection/models/__init__.py
@@ -2,4 +2,5 @@
 # Copyright (C) 2017 Komit <http://www.komit-consulting.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+from . import mail_followers
 from . import mail_wizard_invite
