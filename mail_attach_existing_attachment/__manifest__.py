a80c62c9642539f3010055aa06aee854eb18ea07
ernesto <ernesto.tejeda87@gmail.com>
2018-04-16 17:16:07 -0400

    [MIG] mail_attach_existing_attachment: Migration to 11.0

--- mail_attach_existing_attachment/__manifest__.py
+++ mail_attach_existing_attachment/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #     This file is part of mail_attach_existing_attachment,
@@ -31,7 +30,7 @@
               "Odoo Community Association (OCA)",
     'website': "http://acsone.eu",
     'category': 'Social Network',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'mail',
