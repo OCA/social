b02cf7db7e142595220d076bb7181bc0504513de
BALDE <tansadio@gmail.com>
2018-05-04 18:01:41 +0200

    [10.0] mail_template_multi_report: make oca standard instruction happy

--- mail_template_multi_report/__manifest__.py
+++ mail_template_multi_report/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Savoir-faire Linux
+# Copyright © 2016 Savoir-faire Linux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -7,8 +7,8 @@
     'version': '10.0.1.0.0',
     'category': 'Marketing',
     'summary': 'Multiple Reports in Mail Templates',
-    'author': 'Savoir-faire Linux,Odoo Community Association (OCA)',
-    'website': 'http://www.savoirfairelinux.com',
+    'author': 'Savoir-faire Linux, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/social',
     'license': 'AGPL-3',
     'depends': [
         'mail',
