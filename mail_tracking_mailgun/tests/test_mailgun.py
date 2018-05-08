ddb84152b83ab3361c086a659a941a051c2fc005
David <david.vidal@tecnativa.com>
2018-05-08 18:24:36 +0200

    [MIG] mail_tracking_mailgun: Migration to 11.0

--- mail_tracking_mailgun/tests/test_mailgun.py
+++ mail_tracking_mailgun/tests/test_mailgun.py
@@ -7,7 +7,6 @@ from odoo.tools import mute_logger
 from odoo.tests.common import TransactionCase
 from odoo.exceptions import UserError, ValidationError
 import mock
-import json
 
 _packagepath = 'odoo.addons.mail_tracking_mailgun'
 
@@ -29,12 +28,12 @@ class TestMailgun(TransactionCase):
 
     def setUp(self):
         super(TestMailgun, self).setUp()
-        self.recipient = u'to@example.com'
+        self.recipient = 'to@example.com'
         self.mail, self.tracking_email = self.mail_send()
-        self.api_key = u'key-12345678901234567890123456789012'
-        self.domain = u'example.com'
-        self.token = u'f1349299097a51b9a7d886fcb5c2735b426ba200ada6e9e149'
-        self.timestamp = u'1471021089'
+        self.api_key = 'key-12345678901234567890123456789012'
+        self.domain = 'example.com'
+        self.token = 'f1349299097a51b9a7d886fcb5c2735b426ba200ada6e9e149'
+        self.timestamp = '1471021089'
         self.signature = ('4fb6d4dbbe10ce5d620265dcd7a3c0b8'
                           'ca0dede1433103891bc1ae4086e9d5b2')
         self.env['ir.config_parameter'].set_param(
@@ -46,17 +45,17 @@ class TestMailgun(TransactionCase):
         self.env['ir.config_parameter'].set_param(
             'mailgun.auto_check_partner_email', '')
         self.event = {
-            'Message-Id': u'<xxx.xxx.xxx-openerp-xxx-res.partner@test_db>',
-            'X-Mailgun-Sid': u'WyIwNjgxZSIsICJ0b0BleGFtcGxlLmNvbSIsICI3MG'
+            'Message-Id': '<xxx.xxx.xxx-openerp-xxx-res.partner@test_db>',
+            'X-Mailgun-Sid': 'WyIwNjgxZSIsICJ0b0BleGFtcGxlLmNvbSIsICI3MG'
                              'I0MWYiXQ==',
             'token': self.token,
             'timestamp': self.timestamp,
             'signature': self.signature,
-            'domain': u'example.com',
-            'message-headers': u'[]',
+            'domain': 'example.com',
+            'message-headers': '[]',
             'recipient': self.recipient,
             'odoo_db': self.env.cr.dbname,
-            'tracking_email_id': u'%s' % self.tracking_email.id
+            'tracking_email_id': '%s' % self.tracking_email.id
         }
         self.metadata = {
             'ip': '127.0.0.1',
@@ -110,8 +109,8 @@ class TestMailgun(TransactionCase):
                  '.mail_tracking_email')
     def test_bad_signature(self):
         self.event.update({
-            'event': u'delivered',
-            'signature': u'bad_signature',
+            'event': 'delivered',
+            'signature': 'bad_signature',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
@@ -121,7 +120,7 @@ class TestMailgun(TransactionCase):
                  '.mail_tracking_email')
     def test_bad_event_type(self):
         self.event.update({
-            'event': u'bad_event',
+            'event': 'bad_event',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
@@ -131,19 +130,19 @@ class TestMailgun(TransactionCase):
                  '.mail_tracking_email')
     def test_bad_db(self):
         self.event.update({
-            'event': u'delivered',
-            'odoo_db': u'bad_db',
+            'event': 'delivered',
+            'odoo_db': 'bad_db',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
         self.assertEqual('ERROR: Invalid DB', response)
 
     def test_bad_ts(self):
-        timestamp = u'7a'  # Now time will be used instead
+        timestamp = '7a'  # Now time will be used instead
         signature = ('06cc05680f6e8110e59b41152b2d1c0f'
                      '1045d755ef2880ff922344325c89a6d4')
         self.event.update({
-            'event': u'delivered',
+            'event': 'delivered',
             'timestamp': timestamp,
             'signature': signature,
         })
@@ -155,8 +154,8 @@ class TestMailgun(TransactionCase):
                  '.mail_tracking_email')
     def test_tracking_not_found(self):
         self.event.update({
-            'event': u'delivered',
-            'tracking_email_id': u'bad_id',
+            'event': 'delivered',
+            'tracking_email_id': 'bad_id',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
@@ -165,7 +164,7 @@ class TestMailgun(TransactionCase):
     # https://documentation.mailgun.com/user_manual.html#tracking-deliveries
     def test_event_delivered(self):
         self.event.update({
-            'event': u'delivered',
+            'event': 'delivered',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
@@ -176,20 +175,20 @@ class TestMailgun(TransactionCase):
 
     # https://documentation.mailgun.com/user_manual.html#tracking-opens
     def test_event_opened(self):
-        ip = u'127.0.0.1'
-        user_agent = u'Odoo Test/8.0 Gecko Firefox/11.0'
-        os_family = u'Linux'
-        ua_family = u'Firefox'
-        ua_type = u'browser'
+        ip = '127.0.0.1'
+        user_agent = 'Odoo Test/8.0 Gecko Firefox/11.0'
+        os_family = 'Linux'
+        ua_family = 'Firefox'
+        ua_type = 'browser'
         self.event.update({
-            'event': u'opened',
-            'city': u'Mountain View',
-            'country': u'US',
-            'region': u'CA',
+            'event': 'opened',
+            'city': 'Mountain View',
+            'country': 'US',
+            'region': 'CA',
             'client-name': ua_family,
             'client-os': os_family,
             'client-type': ua_type,
-            'device-type': u'desktop',
+            'device-type': 'desktop',
             'ip': ip,
             'user-agent': user_agent,
         })
@@ -209,21 +208,21 @@ class TestMailgun(TransactionCase):
 
     # https://documentation.mailgun.com/user_manual.html#tracking-clicks
     def test_event_clicked(self):
-        ip = u'127.0.0.1'
-        user_agent = u'Odoo Test/8.0 Gecko Firefox/11.0'
-        os_family = u'Linux'
-        ua_family = u'Firefox'
-        ua_type = u'browser'
-        url = u'https://odoo-community.org'
+        ip = '127.0.0.1'
+        user_agent = 'Odoo Test/8.0 Gecko Firefox/11.0'
+        os_family = 'Linux'
+        ua_family = 'Firefox'
+        ua_type = 'browser'
+        url = 'https://odoo-community.org'
         self.event.update({
-            'event': u'clicked',
-            'city': u'Mountain View',
-            'country': u'US',
-            'region': u'CA',
+            'event': 'clicked',
+            'city': 'Mountain View',
+            'country': 'US',
+            'region': 'CA',
             'client-name': ua_family,
             'client-os': os_family,
             'client-type': ua_type,
-            'device-type': u'tablet',
+            'device-type': 'tablet',
             'ip': ip,
             'user-agent': user_agent,
             'url': url,
@@ -244,20 +243,20 @@ class TestMailgun(TransactionCase):
 
     # https://documentation.mailgun.com/user_manual.html#tracking-unsubscribes
     def test_event_unsubscribed(self):
-        ip = u'127.0.0.1'
-        user_agent = u'Odoo Test/8.0 Gecko Firefox/11.0'
-        os_family = u'Linux'
-        ua_family = u'Firefox'
-        ua_type = u'browser'
+        ip = '127.0.0.1'
+        user_agent = 'Odoo Test/8.0 Gecko Firefox/11.0'
+        os_family = 'Linux'
+        ua_family = 'Firefox'
+        ua_type = 'browser'
         self.event.update({
-            'event': u'unsubscribed',
-            'city': u'Mountain View',
-            'country': u'US',
-            'region': u'CA',
+            'event': 'unsubscribed',
+            'city': 'Mountain View',
+            'country': 'US',
+            'region': 'CA',
             'client-name': ua_family,
             'client-os': os_family,
             'client-type': ua_type,
-            'device-type': u'mobile',
+            'device-type': 'mobile',
             'ip': ip,
             'user-agent': user_agent,
         })
@@ -278,7 +277,7 @@ class TestMailgun(TransactionCase):
     #   user_manual.html#tracking-spam-complaints
     def test_event_complained(self):
         self.event.update({
-            'event': u'complained',
+            'event': 'complained',
         })
         response = self.env['mail.tracking.email'].event_process(
             None, self.event, self.metadata)
@@ -290,12 +289,12 @@ class TestMailgun(TransactionCase):
 
     # https://documentation.mailgun.com/user_manual.html#tracking-bounces
     def test_event_bounced(self):
-        code = u'550'
-        error = (u"5.1.1 The email account does not exist.\n"
+        code = '550'
+        error = ("5.1.1 The email account does not exist.\n"
                  "5.1.1 double-checking the recipient's email address")
-        notification = u"Please, check recipient's email address"
+        notification = "Please, check recipient's email address"
         self.event.update({
-            'event': u'bounced',
+            'event': 'bounced',
             'code': code,
             'error': error,
             'notification': notification,
@@ -312,11 +311,11 @@ class TestMailgun(TransactionCase):
 
     # https://documentation.mailgun.com/user_manual.html#tracking-failures
     def test_event_dropped(self):
-        reason = u'hardfail'
-        code = u'605'
-        description = u'Not delivering to previously bounced address'
+        reason = 'hardfail'
+        code = '605'
+        description = 'Not delivering to previously bounced address'
         self.event.update({
-            'event': u'dropped',
+            'event': 'dropped',
             'reason': reason,
             'code': code,
             'description': description,
@@ -336,10 +335,10 @@ class TestMailgun(TransactionCase):
         self.partner.email_bounced = False
         mock_request.get.return_value.apparent_encoding = 'ascii'
         mock_request.get.return_value.status_code = 200
-        mock_request.get.return_value.content = json.dumps({
+        mock_request.get.return_value.json.return_value = {
             'is_valid': True,
             'mailbox_verification': 'true',
-        }, ensure_ascii=True)
+        }
         # Trigger email auto validation in partner
         self.env['ir.config_parameter'].set_param(
             'mailgun.auto_check_partner_email', 'True')
@@ -347,24 +346,24 @@ class TestMailgun(TransactionCase):
         self.assertFalse(self.partner.email_bounced)
         self.partner.email = 'xoxoxoxo@tecnativa.com'
         # Not a valid mailbox
-        mock_request.get.return_value.content = json.dumps({
+        mock_request.get.return_value.json.return_value = {
             'is_valid': True,
             'mailbox_verification': 'false',
-        }, ensure_ascii=True)
+        }
         with self.assertRaises(UserError):
             self.partner.check_email_validity()
         # Not a valid mail address
-        mock_request.get.return_value.content = json.dumps({
+        mock_request.get.return_value.json.return_value = {
             'is_valid': False,
             'mailbox_verification': 'false',
-        }, ensure_ascii=True)
+        }
         with self.assertRaises(UserError):
             self.partner.check_email_validity()
         # Unable to fully validate
-        mock_request.get.return_value.content = json.dumps({
+        mock_request.get.return_value.json.return_value = {
             'is_valid': True,
             'mailbox_verification': 'unknown',
-        }, ensure_ascii=True)
+        }
         with self.assertRaises(UserError):
             self.partner.check_email_validity()
         self.assertTrue(self.partner.email_bounced)
@@ -402,9 +401,7 @@ class TestMailgun(TransactionCase):
 
     @mock.patch(_packagepath + '.models.mail_tracking_email.requests')
     def test_manual_check(self, mock_request):
-        mock_request.get.return_value.content = json.dumps(self.response,
-                                                           ensure_ascii=True)
-        mock_request.get.return_value.apparent_encoding = 'ascii'
+        mock_request.get.return_value.json.return_value = self.response
         mock_request.get.return_value.status_code = 200
         self.tracking_email.action_manual_check_mailgun()
         event = self.env['mail.tracking.event'].search(
@@ -417,8 +414,6 @@ class TestMailgun(TransactionCase):
         with self.assertRaises(ValidationError):
             self.tracking_email.action_manual_check_mailgun()
         mock_request.get.return_value.status_code = 200
-        mock_request.get.return_value.content = json.dumps('{}',
-                                                           ensure_ascii=True)
-        mock_request.get.return_value.apparent_encoding = 'ascii'
+        mock_request.get.return_value.json.return_value = {}
         with self.assertRaises(ValidationError):
             self.tracking_email.action_manual_check_mailgun()
