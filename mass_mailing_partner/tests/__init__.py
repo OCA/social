4e550a3f416352d82f86a02ba4854d712256c479
ernesto <ernesto.tejeda87@gmail.com>
2018-05-17 16:00:43 -0400

    [MIG] mass_mailing_partner: Migration to 11.0

--- mass_mailing_partner/tests/__init__.py
+++ mass_mailing_partner/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Javier Iniesta <javieria@antiun.com>
