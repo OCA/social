c3993506cd54d07b51ae8920679094c86010b0f7
ernesto <ernesto.tejeda87@gmail.com>
2018-05-17 16:00:43 -0400

    [MIG] mass_mailing_partner: Migration to 11.0

--- mass_mailing_partner/tests/base.py
+++ mass_mailing_partner/tests/base.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Javier Iniesta <javieria@antiun.com>
@@ -10,17 +9,23 @@ from odoo.tests.common import TransactionCase
 class BaseCase(TransactionCase):
     def setUp(self):
         super(BaseCase, self).setUp()
-        m_partner_category = self.env['res.partner.category']
-        m_mailing_list = self.env['mail.mass_mailing.list']
-        self.company_id = self.env.ref('base.main_company')
-        self.company = self.env['res.company'].browse(self.company_id)
-        self.partner = self.create_partner({'name': 'Partner test',
-                                            'email': 'partner@test.com'})
-        partner_category = m_partner_category.create({'name': 'Category Test'})
-        self.mailing_list = m_mailing_list.create({'name': 'List test'})
-        self.mailing_list2 = m_mailing_list.create(
+
+        self.main_company = self.env.ref('base.main_company')
+        self.country_es = self.env.ref('base.es')
+        self.category_0 = self.env.ref('base.res_partner_category_0')
+        self.category_1 = self.env.ref('base.res_partner_category_1')
+        self.title_mister = self.env.ref('base.res_partner_title_mister')
+        self.partner = self.create_partner(
+            {'name': 'Partner test', 'email': 'partner@test.com',
+             'title': self.title_mister.id, 'company_id': self.main_company.id,
+             'country_id': self.country_es.id,
+             'category_id': [(6, 0, (self.category_0 | self.category_1).ids)]})
+
+        self.category_3 = self.env.ref('base.res_partner_category_3')
+        self.mailing_list = self.create_mailing_list({'name': 'List test'})
+        self.mailing_list2 = self.create_mailing_list(
             {'name': 'List test 2', 'partner_mandatory': True,
-             'partner_category': partner_category.id})
+             'partner_category': self.category_3.id})
 
     def create_partner(self, vals):
         m_partner = self.env['res.partner']
@@ -30,9 +35,22 @@ class BaseCase(TransactionCase):
         m_mailing_contact = self.env['mail.mass_mailing.contact']
         return m_mailing_contact.create(vals)
 
+    def create_mailing_list(self, vals):
+        m_mailing_list = self.env['mail.mass_mailing.list']
+        return m_mailing_list.create(vals)
+
     def check_mailing_contact_partner(self, mailing_contact):
         if mailing_contact.partner_id:
             self.assertEqual(mailing_contact.partner_id.email,
                              mailing_contact.email)
             self.assertEqual(mailing_contact.partner_id.name,
                              mailing_contact.name)
+            self.assertEqual(mailing_contact.partner_id.title,
+                             mailing_contact.title_id)
+            if mailing_contact.partner_id.company_id:
+                self.assertEqual(mailing_contact.partner_id.company_id.name,
+                                 mailing_contact.company_name)
+            self.assertEqual(mailing_contact.partner_id.country_id,
+                             mailing_contact.country_id)
+            self.assertEqual(mailing_contact.partner_id.category_id,
+                             mailing_contact.tag_ids)
