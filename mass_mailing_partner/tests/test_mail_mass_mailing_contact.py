4d623a29e5d437562bd3197f65a5779b3e1a1b2d
ernesto <ernesto.tejeda87@gmail.com>
2018-05-17 16:00:43 -0400

    [MIG] mass_mailing_partner: Migration to 11.0

--- mass_mailing_partner/tests/test_mail_mass_mailing_contact.py
+++ mass_mailing_partner/tests/test_mail_mass_mailing_contact.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Javier Iniesta <javieria@antiun.com>
@@ -6,44 +5,66 @@
 
 from . import base
 from ..hooks import post_init_hook
-from psycopg2 import IntegrityError
+from odoo.exceptions import ValidationError
 
 
 class MailMassMailingContactCase(base.BaseCase):
 
     def test_match_existing_contacts(self):
         contact = self.create_mailing_contact(
-            {'email': 'partner@test.com', 'list_id': self.mailing_list.id})
+            {'email': 'partner@test.com',
+             'list_ids': [(6, 0, self.mailing_list.ids)]})
         post_init_hook(self.cr, self.registry)
         self.assertEqual(contact.partner_id.id, self.partner.id)
+        self.check_mailing_contact_partner(contact)
 
     def test_create_mass_mailing_contact(self):
-        contact = self.create_mailing_contact(
-            {'email': 'partner2@test.com', 'name': 'Partner test 2',
-             'list_id': self.mailing_list2.id})
+        title_doctor = self.env.ref('base.res_partner_title_doctor')
+        country_cu = self.env.ref('base.cu')
+        category_4 = self.env.ref('base.res_partner_category_4')
+        category_5 = self.env.ref('base.res_partner_category_5')
+        contact_vals = {
+            'name': 'Partner test 2', 'email': 'partner2@test.com',
+            'title_id': title_doctor.id, 'company_name': "TestCompany",
+            'country_id': country_cu.id,
+            'tag_ids': [(6, 0, (category_4 | category_5).ids)],
+            'list_ids': [(6, 0, (self.mailing_list | self.mailing_list2).ids)],
+        }
+        contact = self.create_mailing_contact(contact_vals)
         self.check_mailing_contact_partner(contact)
-        with self.assertRaises(IntegrityError):
-            self.create_mailing_contact({'email': 'partner2@test.com',
-                                         'list_id': self.mailing_list2.id})
+        with self.assertRaises(ValidationError):
+            self.create_mailing_contact(
+                {'email': 'partner2@test.com',
+                 'list_ids': [[6, 0, [self.mailing_list2.id]]]})
 
     def test_write_mass_mailing_contact(self):
         contact = self.create_mailing_contact(
-            {'email': 'partner@test.com', 'list_id': self.mailing_list.id})
+            {'email': 'partner@test.com',
+             'list_ids': [(6, 0, self.mailing_list.ids)]})
         contact.write({'partner_id': False})
         self.check_mailing_contact_partner(contact)
         contact2 = self.create_mailing_contact(
             {'email': 'partner2@test.com', 'name': 'Partner test 2',
-             'list_id': self.mailing_list.id})
-        contact.write({'partner_id': False})
+             'list_ids': [(6, 0, self.mailing_list.ids)]})
+        contact2.write({'partner_id': False})
         self.assertFalse(contact2.partner_id)
 
     def test_onchange_partner(self):
         contact = self.create_mailing_contact(
-            {'email': 'partner@test.com', 'list_id': self.mailing_list.id})
-        partner = self.create_partner(
-            {'name': 'Test partner', 'email': 'sample@test.com'})
+            {'email': 'partner@test.com',
+             'list_ids': [[6, 0, [self.mailing_list.id]]]})
+        title_doctor = self.env.ref('base.res_partner_title_doctor')
+        country_cu = self.env.ref('base.cu')
+        category_4 = self.env.ref('base.res_partner_category_4')
+        category_5 = self.env.ref('base.res_partner_category_5')
+        partner_vals = {
+            'name': 'Partner test 2', 'email': 'partner2@test.com',
+            'title': title_doctor.id, 'company_id': self.main_company.id,
+            'country_id': country_cu.id,
+            'category_id': [(6, 0, (category_4 | category_5).ids)],
+        }
+        partner = self.create_partner(partner_vals)
         with self.env.do_in_onchange():
             contact.partner_id = partner
-            contact._onchange_partner()
-            self.assertEqual(contact.name, partner.name)
-            self.assertEqual(contact.email, partner.email)
+            contact._onchange_partner_mass_mailing_partner()
+            self.check_mailing_contact_partner(contact)
