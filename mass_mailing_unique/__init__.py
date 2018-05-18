9c38293b0cffc76c54041a0e1e3f18c367788b24
ernesto <ernesto.tejeda87@gmail.com>
2018-05-18 19:56:18 -0400

    [MIG] mass_mailing_unique: Migration to 11.0

--- mass_mailing_unique/__init__.py
+++ mass_mailing_unique/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
