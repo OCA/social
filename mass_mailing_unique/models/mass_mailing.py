568258030c49633be9cf9abcfc6bf57528e5eb09
ernesto <ernesto.tejeda87@gmail.com>
2018-05-18 19:56:18 -0400

    [MIG] mass_mailing_unique: Migration to 11.0

--- mass_mailing_unique/models/mass_mailing.py
+++ mass_mailing_unique/models/mass_mailing.py
@@ -1,18 +1,25 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import models
+from odoo import models, api, _, tools
+from odoo.exceptions import ValidationError
 
 
 class MailMassMailingContact(models.Model):
     _inherit = "mail.mass_mailing.contact"
-    _sql_constraints = [
-        ("unique_mail_per_list", "UNIQUE(list_id, email)",
-         "Cannot have the same email more than once in the same list.")
-    ]
+
+    @api.constrains('email', 'list_ids')
+    def _check_email_list_ids(self):
+        for contact in self:
+            other_contact = self.search([
+                ('email', '=ilike', tools.escape_psql(contact.email)),
+                ('id', '!=', contact.id)
+            ])
+            if contact.list_ids & other_contact.mapped('list_ids'):
+                raise ValidationError(_("Cannot have the same email more "
+                                        "than once in the same list"))
 
 
 class MailMassMailingList(models.Model):
