9082b1bf6b65ae431257c60e52c0b05b0d4b06e4
David <david.vidal@tecnativa.com>
2018-05-25 18:49:55 +0200

    [MIG] mass_mailing_custom_unsubscribe: Migration to 11.0

--- mass_mailing_custom_unsubscribe/__init__.py
+++ mass_mailing_custom_unsubscribe/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import controllers, models
+from .hooks import post_init_hook
--- mass_mailing_custom_unsubscribe/exceptions.py
+++ mass_mailing_custom_unsubscribe/exceptions.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import exceptions
+from odoo import exceptions
 
 
 class DetailsRequiredError(exceptions.ValidationError):
