28446b54ab0e9a13392a1b1660a724a4f1fb4eaf
cubells <vicent@vcubells.net>
2018-06-11 18:12:03 +0200

    [MIG] base_search_mail_content: Migration to 11.0

--- base_search_mail_content/__manifest__.py
+++ base_search_mail_content/__manifest__.py
@@ -1,19 +1,21 @@
-# -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
-# © 2016 Serpent Consulting Services Pvt. Ltd. (<http://www.serpentcs.com>)
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Base Search Mail Content",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent,"
               "SerpentCS,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "category": "Social",
-    "data": ["data/trgm_index_data.xml",
-             "views/trgm_index_view.xml"],
+    "data": [
+        "data/trgm_index_data.xml",
+    ],
     "depends": ["mail", "base_search_fuzzy"],
     "license": "AGPL-3",
     'installable': True,
