add3c079ff4e786fb3aad280a1b867302f655a56
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2018-07-06 14:14:31 +0200

    [MIG] Backport mail_improved_tracking_value from 10.0 to 9.0

--- mail_improved_tracking_value/tests/test_improved_tracking_value.py
+++ mail_improved_tracking_value/tests/test_improved_tracking_value.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import json
-from odoo.tests.common import TransactionCase
+from openerp.tests.common import TransactionCase
 
 
 class TestImproveTrackingValue(TransactionCase):
