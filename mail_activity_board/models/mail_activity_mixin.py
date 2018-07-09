82dd15e8a415c5ee25060e265c021ebcaa2b6656
David Juaneda Ayensa <djuaneda@sdi.es>
2018-07-09 12:46:38 +0200

    [FIX] Eliminated unnecessary imports.

--- mail_activity_board/models/mail_activity_mixin.py
+++ mail_activity_board/models/mail_activity_mixin.py
@@ -1,6 +1,6 @@
 # Copyright 2016 David Juaneda - <djuaneda@sdi.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api, _
+from odoo import models
 
 
 class MailActivityMixin(models.AbstractModel):
