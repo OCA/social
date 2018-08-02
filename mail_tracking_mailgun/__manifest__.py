e7b7be9f27c7473ba682b58994d5a905d0a10fd6
David <david.vidal@tecnativa.com>
2018-08-02 10:53:59 +0200

    [FIX] mail_tracking_mailgun: alternative domain
    - In case the sending domain is different from the one configured in the
    mail.domain.catchall setting.

--- mail_tracking_mailgun/__manifest__.py
+++ mail_tracking_mailgun/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Mail tracking for Mailgun",
     "summary": "Mail tracking and Mailgun webhooks integration",
-    "version": "11.0.1.0.3",
+    "version": "11.0.1.1.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, "
