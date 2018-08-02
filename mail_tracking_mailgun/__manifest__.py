1a2c593c7529c42f341e6abd43427baac6fd9104
David <david.vidal@tecnativa.com>
2018-08-02 10:53:59 +0200

    [FIX] mail_tracking_mailgun: alternative domain
    - In case the sending domain is different from the one configured in the
    mail.domain.catchall setting.

--- mail_tracking_mailgun/__manifest__.py
+++ mail_tracking_mailgun/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Mail tracking for Mailgun",
     "summary": "Mail tracking and Mailgun webhooks integration",
-    "version": "10.0.1.1.3",
+    "version": "10.0.1.2.0",
     "category": "Social Network",
     "website": "https://odoo-community.org/",
     "author": "Tecnativa, "
