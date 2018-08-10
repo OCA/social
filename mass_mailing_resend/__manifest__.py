5bf43e239bccff98162bd8e76384e2bc8c25ff88
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-10 07:56:00 +0200

    [MIG] mass_mailing_resend: Migration to 11.0
    * Standard procedure
    * Adopt README by fragments
    * Adapt tests

--- mass_mailing_resend/__manifest__.py
+++ mass_mailing_resend/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Resend mass mailings",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Marketing",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, "
