991736f7936fcd446aeffbb6d2ecfcef7e5b4250
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-08-10 07:56:00 +0200

    [MIG] mass_mailing_resend: Migration to 11.0
    * Standard procedure
    * Adopt README by fragments
    * Adapt tests

--- mass_mailing_resend/tests/test_mass_mailing_resend.py
+++ mass_mailing_resend/tests/test_mass_mailing_resend.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
@@ -20,7 +19,9 @@ class TestMassMailingResend(common.SavepointCase):
         cls.mass_mailing = cls.env['mail.mass_mailing'].create({
             'name': 'Test mass mailing',
             'email_from': 'test@example.org',
-            'mailing_model': 'mail.mass_mailing.contact',
+            'mailing_model_id': cls.env.ref(
+                'mass_mailing.model_mail_mass_mailing_contact'
+            ).id,
             'contact_list_ids': [(6, 0, cls.list.ids)],
             'reply_to_mode': 'thread',
         })
