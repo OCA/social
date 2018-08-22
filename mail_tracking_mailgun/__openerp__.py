50409aba507cc3e626fa5f26ed114617b5e4649e
David <david.vidal@tecnativa.com>
2018-08-22 14:35:55 +0200

    [FIX] mail_tracking_mailgun: Validation API url
    - Besides Mailgun API url changes if the domain zone is in UE, it
    remains the same for Validation API.

--- mail_tracking_mailgun/__openerp__.py
+++ mail_tracking_mailgun/__openerp__.py
@@ -7,7 +7,7 @@
 {
     "name": "Mail tracking for Mailgun",
     "summary": "Mail tracking and Mailgun webhooks integration",
-    "version": "9.0.1.3.0",
+    "version": "9.0.1.3.1",
     "category": "Social Network",
     "website": "https://odoo-community.org/",
     "author": "Tecnativa, "
