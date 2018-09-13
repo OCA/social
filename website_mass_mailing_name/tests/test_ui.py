b77e4c4c9c845b55b0333a76ca703702484e89bd
Jairo Llopis <yajo.sk8@gmail.com>
2018-09-13 10:05:20 +0100

    [IMP] website_mass_mailing_name: Make tour faster
    Under some integration environment, the public part of the tour fails to load, I don't know exactly why (or better, I don't know why it was working before, because it was deleting the phantomjs session entirely).
    
    In any case, with this fix, it works there and is faster to test.

--- website_mass_mailing_name/tests/test_ui.py
+++ website_mass_mailing_name/tests/test_ui.py
@@ -2,24 +2,14 @@
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo.http import root
 from odoo.tests.common import HttpCase
 
 
 class UICase(HttpCase):
     def test_ui(self):
         """Test snippet behavior."""
-        tour = "odoo.__DEBUG__.services['web_tour.tour'].%s"
-        # Admin edits home page and adds subscription snippet
-        self.phantom_js(
-            "/",
-            tour % "run('mass_mailing_name_editor')",
-            tour % "tours.mass_mailing_name_editor.ready",
-            login="admin")
-        # Forced log out
-        root.session_store.delete(self.session)
-        # Public user uses subscription snippet
-        self.phantom_js(
-            "/",
-            tour % "run('mass_mailing_name_public')",
-            tour % "tours.mass_mailing_name_public.ready")
+        base = "odoo.__DEBUG__.services['web_tour.tour'].%s"
+        run = base % "run('%s')"
+        ready = base % "tours.%s.ready"
+        tour = "mass_mailing_name_editor_and_public"
+        self.phantom_js("/", run % tour, ready % tour, login="admin")
