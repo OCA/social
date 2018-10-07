7b72cb128d05f0c36cb1b2e4de3e2708645fff52
notze <georg.notter@agent-erp.de>
2018-10-07 08:05:00 +0200

    cleaned pylint issues

--- mail_store_outgoing/tests/test_ir_mail.py
+++ mail_store_outgoing/tests/test_ir_mail.py
@@ -10,9 +10,9 @@ class TestIrMail(TransactionCase):
         super(TestIrMail, self).setUp()
 
     def test_parse_list_response(self):
-        imap_mailbox = '(\HasNoChildren \UnMarked) "." "INBOX.Deleted Messages"'
+        imap_mailbox = '(\\HasNoChildren \\UnMarked) "." "INBOX.Deleted Messages"'
         flags, delimiter, mailbox_name = self.env['ir.mail_server'].parse_list_response(imap_mailbox)
-        self.assertEqual(flags,'\HasNoChildren \UnMarked')
+        self.assertEqual(flags,'\\HasNoChildren \\UnMarked')
         self.assertEqual(delimiter,'.')
         self.assertEqual(mailbox_name, 'INBOX.Deleted Messages')
 
