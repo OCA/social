a653ec8eb0fe9ffeda39c86c92dceec549962daf
Hugo Adan <hugo@vauxoo.com>
2018-10-30 16:17:06 +0000

    [Add] 11.0 mail_track_diff_only
    [FIX] includes unknown fields: sequence
    
    [REF] Unitests

---
+++ mail_track_diff_only/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Mail track diff only",
+    "version": "11.0.1.0.0",
+    "author": "Vauxoo, Odoo Community Association (OCA)",
+    "category": "Discuss",
+    "website": "https://github.com/OCA/social/tree/11.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "demo": [],
+    "data": [],
+    "installable": True,
+}
---
+++ mail_track_diff_only/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module shows only the values changed of an object on email, instead of all
+tracked values, even if it has track_visibility set as always.
