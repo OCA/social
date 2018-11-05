8ab506bed7a7059e6917285bd70d984d4df1eecb
ernesto <ernesto.tejeda87@gmail.com>
2018-11-05 11:26:01 -0500

    [MIG] mail_tracking: Migration to 12.0

--- mail_tracking/models/__init__.py
+++ mail_tracking/models/__init__.py
@@ -1,6 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import ir_mail_server
+from . import mail_bounced_mixin
 from . import mail_mail
 from . import mail_message
 from . import mail_tracking_email
