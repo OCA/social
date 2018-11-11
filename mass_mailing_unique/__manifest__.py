3f09804ef6f717a90dc612bedc8351ad90fcb0fa
ernesto <ernesto.tejeda87@gmail.com>
2018-11-11 09:18:45 -0500

    [MIG] mass_mailing_unique: Migration to 12.0

--- mass_mailing_unique/__manifest__.py
+++ mass_mailing_unique/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
+# Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Unique records for mass mailing",
     "summary": "Avoids duplicate mailing lists and contacts",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Marketing",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, "
