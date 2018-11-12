1ee380424be4e21aee14d5f16706d3e755b1c68d
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 00:46:56 -0500

    mass_mailing_resend: Migration to 12.0

--- mass_mailing_resend/__manifest__.py
+++ mass_mailing_resend/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Resend mass mailings",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Marketing",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, "
