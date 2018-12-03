add4bd178c3d565e61ff085e907ffdc122374b18
David <david.vidal@tecnativa.com>
2018-12-03 11:53:45 +0100

    [MIG] fetch_thread_default: Migration to 11.0

--- fetchmail_thread_default/__manifest__.py
+++ fetchmail_thread_default/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 Tecnativa - Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Default Thread For Unbounded Emails",
     "summary": "Post unkonwn messages to an existing thread",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Discuss",
-    "website": "https://www.tecnativa.com/",
+    "website": "https://www.github.com/social",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
