c706391c2e1f6fc4224a79526d0adc789a43a623
Graeme Gellatly <gdgellatly@gmail.com>
2018-12-27 21:26:36 +1300

    [FIX] Parse generated html to remove to remove Odoo branding messages

--- mail_debrand/__manifest__.py
+++ mail_debrand/__manifest__.py
@@ -3,16 +3,13 @@
 {
     "name": "Mail Debrand",
     "summary": "Remove Odoo branding in sent emails",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "Social Network",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/OCA/social/",
     "author": "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
         "mail",
     ],
-    "data": [
-        'views/mail_notification_view.xml'
-    ]
 }
