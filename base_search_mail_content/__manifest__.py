bfd6554a1128ce5b021e09d03e8f9557b7e32010
ernesto <ernesto.tejeda87@gmail.com>
2019-01-10 10:59:45 -0500

    [MIG] base_search_mail_content: Migration to 12.0

--- base_search_mail_content/__manifest__.py
+++ base_search_mail_content/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Base Search Mail Content",
-    "version": "11.0.1.0.1",
+    "version": "12.0.1.0.0",
     "author": "Eficent,"
               "SerpentCS,"
               "Tecnativa,"
