d6e43308dafc331d0b04d854940e9b17452885b7
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-01-16 17:08:14 +0100

    [IMP] mail_inline_css: Improve and add test cov

--- mail_inline_css/__manifest__.py
+++ mail_inline_css/__manifest__.py
@@ -1,17 +1,26 @@
 # Copyright 2017 David BEAL @ Akretion
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2019 Camptocamp SA
 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Mail Inline CSS',
     "summary": "Convert style tags in inline style in your mails",
     'version': '11.0.1.0.0',
-    'author': 'Akretion, Odoo Community Association (OCA)',
+    'author': 'Akretion, camptocamp, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/social',
     'license': 'AGPL-3',
     'category': 'Tools',
-    'depends': ['mail'],
     'installable': True,
     "external_dependencies": {
         "python": ['premailer'],
     },
+    "depends": [
+        "email_template_qweb",
+    ],
+    "data": [
+    ],
+    "demo": [
+        "demo/demo_template.xml",
+        "demo/demo_mail_template.xml",
+    ],
 }
