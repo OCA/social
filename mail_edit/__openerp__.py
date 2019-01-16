9ad59d81b408a1c3f225c1957de5b1df82cf3206
Giovanni Francesco Capalbo <giovanni@therp.nl>
2019-01-16 11:18:32 +0100

    [UPD] mail_edit non installable

--- mail_edit/__openerp__.py
+++ mail_edit/__openerp__.py
@@ -11,7 +11,7 @@
     "author": "Sunflower IT, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": True,
+    "installable": False,
     "depends": [
         "mail",
         "web",
