3bc2ed67a018fc4b6917496c6d01fde662d70aa7
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-02-11 18:48:31 +0100

    [message_auto_subscribe_notify_own]

---
+++ message_auto_subscribe_notify_own/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Aleph Objects, Inc.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Message Auto Subscribe Notify Own",
+    "summary": "Receive notifications of your own subscriptions",
+    "version": "11.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+}
