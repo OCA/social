2ff470bec01deb6f8e2093b7a09a994290286dc1
Victor Martin <victor.martin@tecnativa.com>
2019-05-21 19:47:45 +0200

    [MIG] website_mass_mailing_name: Migration to 12.0

--- website_mass_mailing_name/__manifest__.py
+++ website_mass_mailing_name/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2016-2019 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Mass Mailing Subscription Snippet With Name",
     "summary": "Ask for name when subscribing, and create and/or link partner",
-    "version": "10.0.1.0.1",
+    "version": "12.0.1.0.0",
     "category": "Website",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, Odoo Community Association (OCA)",
---
+++ website_mass_mailing_name/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module extends the functionality of mass mailings to support asking for
+the contact name directly in the subscription snippet.
+
+If you want to get partners created automatically and linked to the contacts,
+you can additionally install the `mass_mailing_partner`
