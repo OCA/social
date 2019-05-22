c86d92b226f19c2a8d3ed07fdad65999655f1e34
ernesto <ernesto.tejeda87@gmail.com>
2019-05-22 11:44:35 -0400

    [MIG] mail_full_expand: Migration to 12.0

---
+++ mail_full_expand/readme/DESCRIPTION.rst
@@ -0,0 +1,12 @@
+This module was written to extend the functionality of messaging to support
+expanding messages in a big window and allow you to read its full content.
+
+Odoo automatically tries to remove blockquotes and signatures from received
+mails. That is useful because it removes lots of distraction, but sometimes it
+removes important information.
+
+Also, messages are narrow to fit in the conversations views, but sometimes you
+receive a mail with predefined width and cannot read it.
+
+This module adds a button to all messages to read them in a floating window
+with their full contents.
--- mail_full_expand/static/src/js/mail_full_expand.js
+++ mail_full_expand/static/src/js/mail_full_expand.js
@@ -1,23 +1,26 @@
-/* © 2014-2015 Grupo ESOC <http://www.grupoesoc.es>
+/* Copyright 2014-2015 Grupo ESOC <http://www.grupoesoc.es>
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
  odoo.define('mail_full_expand.expand', function (require) {
 "use strict";
 
-var Thread = require('mail.ChatThread');
+var ThreadWidget = require('mail.widget.Thread');
 
-Thread.include({    
+ThreadWidget.include({
     events: _.defaults({
-        "click .o_full_expand": "on_message_full_expand",
-    }, Thread.prototype.events),
-        
-    on_message_full_expand: function(event) {
+        "click .o_full_expand": "_onClickMessageFullExpand",
+    }, ThreadWidget.prototype.events),
+
+    _onClickMessageFullExpand: function(event) {
         // Get the action data and execute it to open the full view
         var do_action = this.do_action,
             msg_id = $(event.currentTarget).data('message-id');
 
-        this.rpc("/web/action/load", {
-            "action_id": "mail_full_expand.mail_message_action",
+        this._rpc({
+            route: "/web/action/load",
+            params: {
+                action_id: "mail_full_expand.mail_message_action",
+            },
         })
         .done(function(action) {
             action.res_id = msg_id;
