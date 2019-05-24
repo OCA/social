6e01193bf755c06b69fbb92c3bf95f7fc59d0c57
eLBati <elbaddy@gmail.com>
2019-05-24 14:40:45 +0200

    Port mail_outbound_static to 12.0 Fixing and extending tests Add setup

--- mail_outbound_static/__manifest__.py
+++ mail_outbound_static/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Mail Outbound Static',
     'summary': 'Allows you to configure the from header for a mail server.',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.0',
     'category': 'Discuss',
     'website': 'https://laslabs.com/',
     'author': 'LasLabs, Odoo Community Association (OCA)',
