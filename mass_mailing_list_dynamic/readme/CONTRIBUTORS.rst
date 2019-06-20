1776230992b3eedcfaadf610eac07d4dab0de647
Victor Martin <victor.martin@tecnativa.com>
2019-06-20 19:00:52 +0200

    [MIG] mass_mailing_list_dynamic: Migration to 12.0

---
+++ mass_mailing_list_dynamic/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* `Tecnativa <https://www.tecnativa.com>`_:
+
+  * Jairo Llopis <jairo.llopis@tecnativa.com>
+  * Pedro M. Baeza <pedro.baeza@tecnativa.com>
+  * David Vidal <david.vidal@tecnativa.com>
+  * Victor M.M. Torres <victor.martin@tecnativa.com>
--- mass_mailing_list_dynamic/wizards/mail_mass_mailing_load_filter.py
+++ mass_mailing_list_dynamic/wizards/mail_mass_mailing_load_filter.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
@@ -7,6 +6,7 @@ from odoo import fields, models
 
 class MassMailingLoadFilter(models.TransientModel):
     _name = "mail.mass_mailing.load.filter"
+    _description = "Mail Mass Mailing Load Filter"
 
     filter_id = fields.Many2one(
         comodel_name='ir.filters',
