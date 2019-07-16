6ff275d41a1ceecf637d1fbaf4a995609b1f3857
Lois Rilo <lois.rilo@eficent.com>
2019-07-16 15:49:41 +0200

    [12.0][FIX] mail_debrand: debrand regular notifications.
    without this fix, any regular notification (added as follower,
    thread notifications...) were not parsed at all and as a
    consequence also not debranded.

---
+++ mail_debrand/models/mail_thread.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+#   Lois Rilo <lois.rilo@eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class MailThread(models.AbstractModel):
+    _inherit = "mail.thread"
+
+    def _replace_local_links(self, html, base_url=None):
+        html = super()._replace_local_links(html, base_url=base_url)
+        html_debranded = self.env["mail.template"]._debrand_body(html)
+        return html_debranded
