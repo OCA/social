6614166903878c5ef81a42ee26e178c73e78eaf2
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-07-22 17:07:50 +0200

    [FIX] mail_tracking: Repeated suggested cc emails

--- mail_tracking/models/mail_thread.py
+++ mail_tracking/models/mail_thread.py
@@ -24,19 +24,21 @@ class MailThread(models.AbstractModel):
     def message_get_suggested_recipients(self):
         res = super().message_get_suggested_recipients()
         ResPartnerObj = self.env['res.partner']
+        email_cc_formated_list = []
         for record in self:
-            messages = record.message_ids.filtered('email_cc')
-            for msg in messages:
-                email_cc_list = email_split_and_format(msg.email_cc)
-                for cc in email_cc_list:
-                    email_parts = getaddresses([cc])[0]
-                    partner_id = record.message_partner_info_from_emails(
-                        [email_parts[1]])[0].get('partner_id')
-                    if not partner_id:
-                        res[record.id].append((False, cc, _('Cc')))
-                    else:
-                        partner = ResPartnerObj.browse(partner_id,
-                                                       self._prefetch)
-                        record._message_add_suggested_recipient(
-                            res, partner=partner, reason=_('Cc'))
+            emails_cc = record.message_ids.mapped('email_cc')
+            for email in emails_cc:
+                email_cc_formated_list.extend(email_split_and_format(email))
+        email_cc_formated_list = set(email_cc_formated_list)
+        for cc in email_cc_formated_list:
+            email_parts = getaddresses([cc])[0]
+            partner_id = record.message_partner_info_from_emails(
+                [email_parts[1]])[0].get('partner_id')
+            if not partner_id:
+                record._message_add_suggested_recipient(
+                    res, email=cc, reason=_('Cc'))
+            else:
+                partner = ResPartnerObj.browse(partner_id, self._prefetch)
+                record._message_add_suggested_recipient(
+                    res, partner=partner, reason=_('Cc'))
         return res
