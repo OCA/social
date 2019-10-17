54df60fb40da69870e8f2f2ec0981018f89209b6
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-10-17 03:28:42 +0200

    [IMP] mail_tracking: Failed Messages to 12.0

--- mail_tracking/controllers/main.py
+++ mail_tracking/controllers/main.py
@@ -2,10 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import werkzeug
-from psycopg2 import OperationalError
-from odoo import api, http, registry, SUPERUSER_ID, _
+import odoo
+from contextlib import contextmanager
+from odoo import api, http, SUPERUSER_ID
+
 from odoo.addons.mail.controllers.main import MailController
-from odoo.http import request
 import logging
 import base64
 _logger = logging.getLogger(__name__)
@@ -13,35 +14,23 @@ _logger = logging.getLogger(__name__)
 BLANK = 'R0lGODlhAQABAIAAANvf7wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
 
 
-def _env_get(db, callback, tracking_id, event_type, **kw):
-    res = 'NOT FOUND'
-    reg = False
-    current = http.request.db and db == http.request.db
-    env = current and http.request.env
-    if not env:
-        with api.Environment.manage():
-            try:
-                reg = registry(db)
-            except OperationalError:
-                _logger.warning("Selected BD '%s' not found", db)
-            except Exception:  # pragma: no cover
-                _logger.warning("Selected BD '%s' connection error", db)
-            if reg:
-                _logger.info("New environment for database '%s'", db)
-                with reg.cursor() as new_cr:
-                    new_env = api.Environment(new_cr, SUPERUSER_ID, {})
-                    res = callback(new_env, tracking_id, event_type, **kw)
-                    new_env.cr.commit()
-    else:
-        # make sudo when reusing environment
-        env = env(user=SUPERUSER_ID)
-        res = callback(env, tracking_id, event_type, **kw)
-    return res
+@contextmanager
+def db_env(dbname):
+    if not http.db_filter([dbname]):
+        raise werkzeug.exceptions.BadRequest()
+    cr = None
+    if dbname == http.request.db:
+        cr = http.request.cr
+    if not cr:
+        cr = odoo.sql_db.db_connect(dbname).cursor()
+    with api.Environment.manage():
+        yield api.Environment(cr, SUPERUSER_ID, {})
 
 
 class MailTrackingController(MailController):
 
     def _request_metadata(self):
+        """Prepare remote info metadata"""
         request = http.request.httprequest
         return {
             'ip': request.remote_addr or False,
@@ -50,37 +39,45 @@ class MailTrackingController(MailController):
             'ua_family': request.user_agent.browser or False,
         }
 
-    def _tracking_open(self, env, tracking_id, event_type, **kw):
-        tracking_email = env['mail.tracking.email'].search([
-            ('id', '=', tracking_id),
-        ])
-        if tracking_email:
-            metadata = self._request_metadata()
-            tracking_email.event_create('open', metadata)
-        else:
-            _logger.warning(
-                "MailTracking email '%s' not found", tracking_id)
-
-    def _tracking_event(self, env, tracking_id, event_type, **kw):
+    @http.route(['/mail/tracking/all/<string:db>',
+                 '/mail/tracking/event/<string:db>/<string:event_type>'],
+                type='http', auth='none', csrf=False)
+    def mail_tracking_event(self, db, event_type=None, **kw):
+        """Route used by external mail service"""
         metadata = self._request_metadata()
-        return env['mail.tracking.email'].event_process(
-            http.request, kw, metadata, event_type=event_type)
+        res = None
+        with db_env(db) as env:
+            try:
+                res = env['mail.tracking.email'].event_process(
+                    http.request, kw, metadata, event_type=event_type)
+            except Exception:
+                pass
+        if not res or res == 'NOT FOUND':
+            return werkzeug.exceptions.NotAcceptable()
+        return res
 
-    @http.route('/mail/tracking/all/<string:db>',
-                type='http', auth='none', csrf=False)
-    def mail_tracking_all(self, db, **kw):
-        return _env_get(db, self._tracking_event, None, None, **kw)
-
-    @http.route('/mail/tracking/event/<string:db>/<string:event_type>',
-                type='http', auth='none', csrf=False)
-    def mail_tracking_event(self, db, event_type, **kw):
-        return _env_get(db, self._tracking_event, None, event_type, **kw)
-
-    @http.route('/mail/tracking/open/<string:db>'
-                '/<int:tracking_email_id>/blank.gif',
-                type='http', auth='none')
-    def mail_tracking_open(self, db, tracking_email_id, **kw):
-        _env_get(db, self._tracking_open, tracking_email_id, None, **kw)
+    @http.route(['/mail/tracking/open/<string:db>'
+                 '/<int:tracking_email_id>/blank.gif',
+                 '/mail/tracking/open/<string:db>'
+                 '/<int:tracking_email_id>/<string:token>/blank.gif'],
+                type='http', auth='none', methods=['GET'])
+    def mail_tracking_open(self, db, tracking_email_id, token=False, **kw):
+        """Route used to track mail openned (With & Without Token)"""
+        metadata = self._request_metadata()
+        with db_env(db) as env:
+            try:
+                tracking_email = env['mail.tracking.email'].search([
+                    ('id', '=', tracking_email_id),
+                    ('state', 'in', ['sent', 'delivered']),
+                    ('token', '=', token),
+                ])
+                if tracking_email:
+                    tracking_email.event_create('open', metadata)
+                else:
+                    _logger.warning(
+                        "MailTracking email '%s' not found", tracking_email_id)
+            except Exception:
+                pass
 
         # Always return GIF blank image
         response = werkzeug.wrappers.Response()
@@ -89,20 +86,11 @@ class MailTrackingController(MailController):
         return response
 
     @http.route()
-    def mail_client_action(self):
-        values = super().mail_client_action()
-        values['channel_slots']['channel_channel'].append({
-            'id': 'channel_failed',
-            'name': _("Failed"),
-            'uuid': None,
-            'state': 'open',
-            'is_minimized': False,
-            'channel_type': 'static',
-            'public': False,
-            'mass_mailing': None,
-            'group_based_subscription': None,
-        })
+    def mail_init_messaging(self):
+        """Route used to initial values of Discuss app"""
+        values = super().mail_init_messaging()
         values.update({
-            'failed_counter': request.env['mail.message'].get_failed_count(),
+            'failed_counter':
+                http.request.env['mail.message'].get_failed_count(),
         })
         return values
