a4a15857e9e30cca0ef3177da19ad7cd920e8a27
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-10-22 15:48:44 +0200

    [MIG] mail_footer_notified_martner: add to use _notify() v12 method

--- mail_footer_notified_partner/tests/test_mail_notification.py
+++ mail_footer_notified_partner/tests/test_mail_notification.py
@@ -8,20 +8,24 @@ class TestMailNotification(common.TransactionCase):
     def setUp(self):
         super().setUp()
 
-        self.partner_obj = self.env['res.partner']
+        self.partner_obj = self.env["res.partner"]
 
     def test_get_signature_footer(self):
-        vals = {
-            'name': 'p1@example.com',
-        }
+        vals = {"name": "p1@example.com"}
         partner1 = self.partner_obj.create(vals)
 
-        body = 'this is the body'
-        subject = 'this is the subject'
+        body = "this is the body"
+        subject = "this is the subject"
         recipients = partner1
-        emails, recipients_nbr = \
-            self.partner_obj._notify_send(body, subject, recipients)
+        emails, recipients_nbr = self.partner_obj._notify_send(
+            body, subject, recipients
+        )
 
         self.assertTrue(
             partner1.name in emails.body_html,
-            'Partner name is not in the body of the mail')
+            "Partner name is not in the body of the mail",
+        )
+
+        self.assertTrue(
+            body in emails.body_html, "The inital body of the mail was lack"
+        )
