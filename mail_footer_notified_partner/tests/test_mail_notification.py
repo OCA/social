ae47908d1800ee26d0e37bde88b95222d6eb22ec
Tonow-c2c <thomas.nowicki@camptocamp.com>
2019-10-22 17:58:29 +0200

    [MIG] mail_footer_notified_partner: Fix test

--- mail_footer_notified_partner/tests/test_mail_notification.py
+++ mail_footer_notified_partner/tests/test_mail_notification.py
@@ -7,25 +7,53 @@ import odoo.tests.common as common
 class TestMailNotification(common.TransactionCase):
     def setUp(self):
         super().setUp()
+        self.sender = self.env['res.partner'].create({
+            'name': 'Test sender',
+            'email': 'sender@example.com',
+        })
+        self.recipient = self.env['res.partner'].create({
+            'name': 'Test recipient',
+            'email': 'recipient@example.com',
+        })
+        self.message = self.env['mail.message'].create({
+            'subject': 'Message test',
+            'author_id': self.sender.id,
+            'email_from': self.sender.email,
+            'message_type': 'comment',
+            'model': 'res.partner',
+            'res_id': self.recipient.id,
+            'partner_ids': [(4, self.recipient.id)],
+            'body': '<p>This is a test message</p>',
+        })
 
-        self.partner_obj = self.env["res.partner"]
+        self.record = self.recipient
+
+        self.rdata = [{
+            'type': 'customer',
+            'id': self.recipient.id,
+            'groups': [None],
+            'active': True,
+            'share': True,
+            'notif': 'email'
+        }]
+        self.force_send = True
+        self.send_after_commit = True
+        self.model_description = False
+        self.mail_auto_delete = True
 
     def test_get_signature_footer(self):
-        vals = {"name": "p1@example.com"}
-        partner1 = self.partner_obj.create(vals)
-
-        body = "this is the body"
-        subject = "this is the subject"
-        recipients = partner1
-        emails, recipients_nbr = self.partner_obj._notify_send(
-            body, subject, recipients
+        rep = self.recipient._notify(
+            self.message,
+            self.rdata,
+            self.record,
+            self.force_send,
+            self.send_after_commit,
+            self.model_description,
+            self.mail_auto_delete,
         )
 
+        self.assertTrue(rep, "message not send")
         self.assertTrue(
-            partner1.name in emails.body_html,
+            self.recipient.name in self.message.body,
             "Partner name is not in the body of the mail",
-        )
-
-        self.assertTrue(
-            body in emails.body_html, "The inital body of the mail was lack"
         )
