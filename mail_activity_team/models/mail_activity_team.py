108a4e722ff62216758a00a8804461d5ea015f45
Pedro Gonzalez <pedro.gonzalez@pesol.es>
2019-10-24 13:47:32 +0200

    [IMP] mail_activity_team: black, isort

--- mail_activity_team/models/mail_activity_team.py
+++ mail_activity_team/models/mail_activity_team.py
@@ -5,60 +5,56 @@ from odoo import api, fields, models
 
 class MailActivityTeam(models.Model):
     _name = "mail.activity.team"
-    _description = 'Mail Activity Team'
+    _description = "Mail Activity Team"
 
-    @api.depends('res_model_ids', 'member_ids')
+    @api.depends("res_model_ids", "member_ids")
     def _compute_missing_activities(self):
-        activity_model = self.env['mail.activity']
+        activity_model = self.env["mail.activity"]
         for team in self:
-            domain = [('team_id', '=', False)]
+            domain = [("team_id", "=", False)]
             if team.member_ids:
-                domain.append(('user_id', 'in', team.member_ids.ids))
+                domain.append(("user_id", "in", team.member_ids.ids))
             if team.res_model_ids:
-                domain.append(('res_model_id', 'in', team.res_model_ids.ids))
-            team.count_missing_activities = activity_model.search(
-                domain, count=True)
+                domain.append(("res_model_id", "in", team.res_model_ids.ids))
+            team.count_missing_activities = activity_model.search(domain, count=True)
 
-    name = fields.Char(
-        string='Name',
-        required=True,
-        translate=True,
-    )
-    active = fields.Boolean(
-        string='Active',
-        default=True,
-    )
+    name = fields.Char(string="Name", required=True, translate=True)
+    active = fields.Boolean(string="Active", default=True)
     res_model_ids = fields.Many2many(
-        comodel_name='ir.model',
-        string='Used models',
+        comodel_name="ir.model",
+        string="Used models",
         domain=lambda self: [
-            ('model', 'in',
-             [k for k in self.env.registry if issubclass(
-                 type(self.env[k]), type(self.env['mail.activity.mixin']))
-              and self.env[k]._auto])
+            (
+                "model",
+                "in",
+                [
+                    k
+                    for k in self.env.registry
+                    if issubclass(
+                        type(self.env[k]), type(self.env["mail.activity.mixin"])
+                    )
+                    and self.env[k]._auto
+                ],
+            )
         ],
     )
     member_ids = fields.Many2many(
-        comodel_name='res.users',
-        relation='mail_activity_team_users_rel',
+        comodel_name="res.users",
+        relation="mail_activity_team_users_rel",
         string="Team Members",
     )
-    user_id = fields.Many2one(
-        comodel_name='res.users',
-        string='Team Leader',
-    )
+    user_id = fields.Many2one(comodel_name="res.users", string="Team Leader")
     count_missing_activities = fields.Integer(
-        string="Missing Activities",
-        compute='_compute_missing_activities',
-        default=0,
+        string="Missing Activities", compute="_compute_missing_activities", default=0
     )
 
-    @api.onchange('member_ids')
+    @api.onchange("member_ids")
     def _onchange_member_ids(self):
+        """Remove team leader in case is not a member anymore"""
         if self.user_id and self.user_id not in self.member_ids:
             self.user_id = False
 
-    @api.onchange('user_id')
+    @api.onchange("user_id")
     def _onchange_user_id(self):
         if self.user_id and self.user_id not in self.member_ids:
             members_ids = self.member_ids.ids
@@ -66,13 +62,13 @@ class MailActivityTeam(models.Model):
             self.member_ids = [(4, member) for member in members_ids]
 
     def assign_team_to_unassigned_activities(self):
-        activity_model = self.env['mail.activity']
+        activity_model = self.env["mail.activity"]
         for team in self:
-            domain = [('team_id', '=', False)]
+            domain = [("team_id", "=", False)]
             if team.member_ids:
-                domain.append(('user_id', 'in', team.member_ids.ids))
+                domain.append(("user_id", "in", team.member_ids.ids))
             if team.res_model_ids:
-                domain.append(('res_model_id', 'in', team.res_model_ids.ids))
+                domain.append(("res_model_id", "in", team.res_model_ids.ids))
             missing_activities = activity_model.search(domain)
             for missing_activity in missing_activities:
-                missing_activity.write({'team_id': team.id})
+                missing_activity.write({"team_id": team.id})
