4506c6c7de82591d76e33e85369f8e9317b94726
pegonzalezspesol <pedro.gonzalez@pesol.es>
2019-10-26 14:14:35 +0200

    Update mail_activity_board/__manifest__.py
    Co-Authored-By: Miquel Raïch <miquel.raich@eficent.com>

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2018 David Juaneda - <djuaneda@sdi.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Activities board",
+    "name": "Mail Activity Board",
     "summary": "Add Activity Boards",
     "version": "13.0.1.0.0",
     "development_status": "Beta",
