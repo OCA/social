3a81de8acc80d0d6ea65bb76c684c67c9d29a4e2
laurent.corron <laurent.corron@acsone.eu>
2019-11-06 11:37:46 +0100

    [REF] mail_optional_autofollow: Black python code

--- mail_optional_autofollow/__manifest__.py
+++ mail_optional_autofollow/__manifest__.py
@@ -1,21 +1,16 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "Mail optional autofollow",
-    'summary': """
+    "name": "Mail optional autofollow",
+    "summary": """
         Choose if you want to automatically add new recipients as followers
         on mail.compose.message""",
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': "http://acsone.eu",
-    'category': 'Social Network',
-    'version': '11.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'mail',
-    ],
-    'data': [
-        'wizard/mail_compose_message_view.xml',
-    ],
-    'installable': True,
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "http://acsone.eu",
+    "category": "Social Network",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mail",],
+    "data": ["wizard/mail_compose_message_view.xml",],
+    "installable": True,
 }
