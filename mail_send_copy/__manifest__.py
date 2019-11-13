95ca83161cac96632ab6b68a11902fda5a2e9830
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-11-13 15:26:07 +0100

    [ADD] mail_send_bcc

---
+++ mail_send_copy/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mail - Send Email Copy",
+    "summary": "Send to you a copy of each mail sent by Odoo",
+    "version": "12.0.1.0.0",
+    "category": "Social Network",
+    "author": "GRAP,"
+              "Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/server-tools",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "installable": True,
+}
