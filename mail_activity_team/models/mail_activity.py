770167d9f78c8854de3ed43e4a2f2b0601b292cc
Pedro Gonzalez <pedro.gonzalez@pesol.es>
2019-11-20 15:39:49 +0100

    [MIG] 13.0 mail_activity_team

--- mail_activity_team/models/mail_activity.py
+++ mail_activity_team/models/mail_activity.py
@@ -57,19 +57,16 @@ class MailActivity(models.Model):
 
     @api.constrains("team_id", "user_id")
     def _check_team_and_user(self):
-        for activity in self:
-            # SUPERUSER is used to put mail.activity on some objects
-            # like sale.order coming from stock.picking
-            # (for example with exception type activity, with no backorder).
-            # SUPERUSER is inactive and then even if you add it
-            # to member_ids it's not taken account
-            # To not be blocked we must add it to constraint condition
-            if (
-                activity.user_id.id != SUPERUSER_ID
-                and activity.team_id
-                and activity.user_id
-                and activity.user_id not in self.team_id.member_ids
-            ):
-                raise ValidationError(
-                    _("The assigned user is not member of " "the team.")
-                )
+        # SUPERUSER is used to put mail.activity on some objects
+        # like sale.order coming from stock.picking
+        # (for example with exception type activity, with no backorder).
+        # SUPERUSER is inactive and then even if you add it
+        # to member_ids it's not taken account
+        # To not be blocked we must add it to constraint condition
+        for _activity in self.filtered(
+            lambda a: a.user_id.id != SUPERUSER_ID
+            and a.team_id
+            and a.user_id
+            and a.user_id not in a.team_id.member_ids
+        ):
+            raise ValidationError(_("The assigned user is not member of the team."))
