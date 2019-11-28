923debe5d954efeff08c0182a7545cf691609479
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-11-28 22:58:09 +0100

    [FIX] mail_tracking: Changed injection method for 'failed_message_ids' field
    Before this commit, some methods with views crashes trying to
    process 'failed_message_ids' field.
    After this commit the 'failed_message_ids' field description
    is properly generated.

--- mail_tracking/__manifest__.py
+++ mail_tracking/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Email tracking",
     "summary": "Email tracking system for all mails sent",
-    "version": "11.0.3.2.0",
+    "version": "11.0.3.2.1",
     "category": "Social Network",
     "website": "http://github.com/OCA/social",
     "author": "Tecnativa, "
