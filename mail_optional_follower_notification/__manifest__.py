f3c8e7d177bc179f7a472a4864e40ec174d33035
Andrea <a.stirpe@onestein.nl>
2019-11-29 16:01:16 +0100

    [12.0][MIG] mail_optional_follower_notification

--- mail_optional_follower_notification/__manifest__.py
+++ mail_optional_follower_notification/__manifest__.py
@@ -1,22 +1,18 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': "Mail optional follower notification",
-
-    'summary': """
-        Choose if you want to automatically notify followers
-        on mail.compose.message""",
-    'author': 'ACSONE SA/NV,'
-              'Odoo Community Association (OCA)',
-    'website': "http://acsone.eu",
-    'category': 'Social Network',
-    'version': '10.0.1.0.1',
-    'license': 'AGPL-3',
-    'depends': [
-        'mail',
+    "name": "Mail optional follower notification",
+    "summary": "Choose to notify followers on mail.compose.message",
+    "author": "ACSONE SA/NV,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "category": "Social Network",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
     ],
-    'data': [
-        'wizard/mail_compose_message_view.xml',
+    "data": [
+        "wizard/mail_compose_message_view.xml",
     ],
 }
