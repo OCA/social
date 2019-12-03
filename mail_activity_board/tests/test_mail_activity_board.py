03633cdb0e478ee94834d40d073195a85f368213
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-12-03 14:29:08 +0100

    [IMP] mail_activity_board: Direct access to related model instance Add a direct access from activity calendar and form views to the linked model instance.
    Bedore this change, it was not possible to access to the model instance linked to an activity from the calendar view nor from the view opened by clicling on the activity into the calendar
    
    [IMP] mail_activiy_board: Restrict related_model_instance selection to models supporting mail acitvities
    
    Co-Authored-By: Miquel Raïch <miquel.raich@eficent.com>

--- mail_activity_board/tests/test_mail_activity_board.py
+++ mail_activity_board/tests/test_mail_activity_board.py
@@ -202,3 +202,11 @@ class TestMailActivityBoardMethods(TransactionCase):
 
         for act in acts:
             self.assertIn(act, self.partner_client.activity_ids.ids)
+
+    def test_related_model_instance(self):
+        """This test case checks the direct access from the activity to the
+        linked model instance
+        """
+        self.assertEqual(self.act3.related_model_instance, self.partner_client)
+        self.act3.write({"res_id": False, "res_model": False})
+        self.assertFalse(self.act3.related_model_instance)
