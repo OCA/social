4c5828af29084d0b050477e6f2da2a4f2e753f21
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-12-06 11:37:20 +0100

    [IMP] mail_optional_follower_notification: black, isort

--- mail_optional_follower_notification/models/mail_message.py
+++ mail_optional_follower_notification/models/mail_message.py
@@ -1,32 +1,43 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, api
+from odoo import api, models
 
 
 class MailMessage(models.Model):
-    _inherit = 'mail.message'
+    _inherit = "mail.message"
 
     @api.model
     def create(self, values):
         ctx = self.env.context.copy()
-        if not ctx.get('notify_followers') and values.get('partner_ids'):
+        if not ctx.get("notify_followers") and values.get("partner_ids"):
             partner_list = self.resolve_2many_commands(
-                'partner_ids', values.get('partner_ids'), fields=['id'])
-            ctx['force_partners_to_notify'] = [d['id'] for d in partner_list]
+                "partner_ids", values.get("partner_ids"), fields=["id"]
+            )
+            ctx["force_partners_to_notify"] = [d["id"] for d in partner_list]
         return super(MailMessage, self.with_context(ctx)).create(values)
 
     @api.multi
-    def _notify(self, record, msg_vals, force_send=False,
-                send_after_commit=True, model_description=False,
-                mail_auto_delete=True):
+    def _notify(
+        self,
+        record,
+        msg_vals,
+        force_send=False,
+        send_after_commit=True,
+        model_description=False,
+        mail_auto_delete=True,
+    ):
         res = super()._notify(
-            record, msg_vals, force_send=force_send,
+            record,
+            msg_vals,
+            force_send=force_send,
             send_after_commit=send_after_commit,
             model_description=model_description,
-            mail_auto_delete=mail_auto_delete)
-        if self.env.context.get('force_partners_to_notify'):
+            mail_auto_delete=mail_auto_delete,
+        )
+        if self.env.context.get("force_partners_to_notify"):
             # Needaction only for recipients
             self.needaction_partner_ids = [
-                (6, 0, self.env.context.get('force_partners_to_notify'))]
+                (6, 0, self.env.context.get("force_partners_to_notify"))
+            ]
         return res
--- mail_optional_follower_notification/wizard/mail_compose_message.py
+++ mail_optional_follower_notification/wizard/mail_compose_message.py
@@ -1,11 +1,11 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class MailComposeMessage(models.TransientModel):
-    _inherit = 'mail.compose.message'
+    _inherit = "mail.compose.message"
 
     notify_followers = fields.Boolean(default=True)
 
@@ -13,8 +13,7 @@ class MailComposeMessage(models.TransientModel):
     def send_mail(self, auto_commit=False):
         ctx = self.env.context.copy()
         for wizard in self:
-            ctx['notify_followers'] = wizard.notify_followers
+            ctx["notify_followers"] = wizard.notify_followers
             wizard = wizard.with_context(ctx)
-            super(MailComposeMessage, wizard).send_mail(
-                auto_commit=auto_commit)
-        return {'type': 'ir.actions.act_window_close'}
+            super(MailComposeMessage, wizard).send_mail(auto_commit=auto_commit)
+        return {"type": "ir.actions.act_window_close"}
