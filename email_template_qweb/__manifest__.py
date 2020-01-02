b28df137e81db6971e3211c6dfc09efa158bf27a
tfo <fossoul.t@gmail.com>
2020-01-02 11:23:57 +0100

    [IMP] email_template_qweb: black, isort

--- email_template_qweb/__manifest__.py
+++ email_template_qweb/__manifest__.py
@@ -3,21 +3,13 @@
 {
     "name": "QWeb for email templates",
     "version": "12.0.1.0.0",
-    "author": "Therp BV, "
-              "Odoo Community Association (OCA)",
+    "author": "Therp BV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Marketing",
     "summary": "Use the QWeb templating mechanism for emails",
-    'website': 'https://github.com/OCA/social',
-    "depends": [
-        'mail',
-    ],
-    "demo": [
-        "demo/ir_ui_view.xml",
-        "demo/mail_template.xml",
-    ],
-    "data": [
-        "views/mail_template.xml",
-    ],
-    'installable': True,
+    "website": "https://github.com/OCA/social",
+    "depends": ["mail"],
+    "demo": ["demo/ir_ui_view.xml", "demo/mail_template.xml"],
+    "data": ["views/mail_template.xml"],
+    "installable": True,
 }
