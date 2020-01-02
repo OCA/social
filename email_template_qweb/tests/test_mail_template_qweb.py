d1f3815091dd2e6ef497989a55d41865a5634901
tfo <fossoul.t@gmail.com>
2020-01-02 11:23:57 +0100

    [IMP] email_template_qweb: black, isort

--- email_template_qweb/tests/test_mail_template_qweb.py
+++ email_template_qweb/tests/test_mail_template_qweb.py
@@ -5,21 +5,19 @@ from odoo.tests.common import TransactionCase
 
 class TestMailTemplateQweb(TransactionCase):
     def test_email_template_qweb(self):
-        template = self.env.ref('email_template_qweb.email_template_demo1')
+        template = self.env.ref("email_template_qweb.email_template_demo1")
         mail_values = template.generate_email([self.env.user.id])
         self.assertTrue(
             # this comes from the called template if everything worked
-            '<footer>' in mail_values[self.env.user.id]['body_html'],
-            'Did not receive rendered template in response. Got: \n%s\n' % (
-                mail_values[self.env.user.id]['body_html']
-            )
+            "<footer>" in mail_values[self.env.user.id]["body_html"],
+            "Did not receive rendered template in response. Got: \n%s\n"
+            % (mail_values[self.env.user.id]["body_html"]),
         )
         # the same method is also called in a non multi mode
         mail_values = template.generate_email(self.env.user.id)
         self.assertTrue(
             # this comes from the called template if everything worked
-            '<footer>' in mail_values['body_html'],
-            'Did not receive rendered template in response. Got: \n%s\n' % (
-                mail_values['body_html']
-            )
+            "<footer>" in mail_values["body_html"],
+            "Did not receive rendered template in response. Got: \n%s\n"
+            % (mail_values["body_html"]),
         )
