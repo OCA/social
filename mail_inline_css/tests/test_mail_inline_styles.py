16a39de143f90efe6dfb8779b7ae225a18b06528
Simone Orsi <simahawk@gmail.com>
2020-01-17 09:49:13 +0100

    [REF] mail_inline_css: apply pre-commit

--- mail_inline_css/tests/test_mail_inline_styles.py
+++ mail_inline_css/tests/test_mail_inline_styles.py
@@ -1,17 +1,16 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from lxml import html
+
 from odoo.tests import SavepointCase
 
 
 class TestMailInlineStyles(SavepointCase):
-
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.mail_template = cls.env.ref(
-            'mail_inline_css.email_template_demo')
-        cls.demo_user = cls.env.ref('base.user_demo')
+        cls.mail_template = cls.env.ref("mail_inline_css.email_template_demo")
+        cls.demo_user = cls.env.ref("base.user_demo")
 
     def to_xml_node(self, html_):
         return html.fragments_fromstring(html_)
@@ -19,8 +18,8 @@ class TestMailInlineStyles(SavepointCase):
     def parse_node_style(self, node):
         """ Convert node CSS string to Python dict"""
         res = {}
-        for style in node.attrib.get('style', '').split(';'):
-            l = style.split(':')
+        for style in node.attrib.get("style", "").split(";"):
+            l = style.split(":")
             res[l[0].strip()] = l[1].strip()
         return res
 
@@ -28,33 +27,31 @@ class TestMailInlineStyles(SavepointCase):
         return node.xpath('//*[@id="{}"]'.format(html_id))
 
     def assertNodeStyle(self, node, expected):
-        self.assertIn('style', node.attrib)
+        self.assertIn("style", node.attrib)
         self.assertEqual(self.parse_node_style(node), expected)
 
     def test_generate_mail(self):
         res = self.mail_template.generate_email(
-            [self.demo_user.id], fields=['body_html']
+            [self.demo_user.id], fields=["body_html"]
         )
-        body_html_string = res[self.demo_user.id].get('body_html')
+        body_html_string = res[self.demo_user.id].get("body_html")
         html_node = self.to_xml_node(body_html_string)[0]
 
         expected = {
-            'main_logo': {
-                'max-width': '300px'
+            "main_logo": {"max-width": "300px"},
+            "main_wrapper": {
+                "max-width": "620px",
+                "margin": "0 auto",
+                "border": "1px solid #ccc",
+                "font-size": "18px",
+                "font-family": "verdana",
+                "color": "#6B6E71",
             },
-            'main_wrapper': {
-                'max-width': '620px',
-                'margin': '0 auto',
-                'border': '1px solid #ccc',
-                'font-size': '18px',
-                'font-family': 'verdana',
-                'color': '#6B6E71'
+            "main_footer": {
+                "padding-top": "0",
+                "font-size": "120%",
+                "padding": "30px 40px",
             },
-            'main_footer': {
-                'padding-top': '0',
-                'font-size': '120%',
-                'padding': '30px 40px'
-            }
         }
 
         for html_id, expected_style in expected.items():
