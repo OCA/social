d7ccc735c66c89ca1545d538adebd2b9e9332053
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-10 11:35:23 +0100

    [IMP] mass_mailing_partner: black, isort

--- mass_mailing_partner/tests/test_mail_mail_statistics.py
+++ mass_mailing_partner/tests/test_mail_mail_statistics.py
@@ -7,26 +7,23 @@ from . import base
 
 
 class MailMailStatisticsCase(base.BaseCase):
-
     def test_link_partner(self):
-        partner = self.create_partner(
-            {'name': 'Test partner'})
-        stat = self.env['mail.mail.statistics'].create({
-            'model': 'res.partner',
-            'res_id': partner.id,
-        })
+        partner = self.create_partner({"name": "Test partner"})
+        stat = self.env["mail.mail.statistics"].create(
+            {"model": "res.partner", "res_id": partner.id}
+        )
         self.assertEqual(partner.id, stat.partner_id.id)
 
     def test_link_mail_contact(self):
         partner = self.create_partner(
-            {'name': 'Test partner', 'email': 'test@domain.com'})
+            {"name": "Test partner", "email": "test@domain.com"}
+        )
         contact_vals = {
-            'partner_id': partner.id,
-            'list_ids': [[6, 0, [self.mailing_list.id]]]
+            "partner_id": partner.id,
+            "list_ids": [[6, 0, [self.mailing_list.id]]],
         }
         contact = self.create_mailing_contact(contact_vals)
-        stat = self.env['mail.mail.statistics'].create({
-            'model': 'mail.mass_mailing.contact',
-            'res_id': contact.id,
-        })
+        stat = self.env["mail.mail.statistics"].create(
+            {"model": "mail.mass_mailing.contact", "res_id": contact.id}
+        )
         self.assertEqual(partner.id, stat.partner_id.id)
