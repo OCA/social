c53cf9af70117edbf92b27145ba2267f1a92f2c9
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-10 11:35:23 +0100

    [IMP] mass_mailing_partner: black, isort

--- mass_mailing_partner/__manifest__.py
+++ mass_mailing_partner/__manifest__.py
@@ -6,21 +6,18 @@
 {
     "name": "Link partners with mass-mailing",
     "version": "12.0.1.0.2",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "license": "AGPL-3",
     "category": "Marketing",
-    "depends": [
-        'mass_mailing',
-    ],
+    "depends": ["mass_mailing"],
     "post_init_hook": "post_init_hook",
-    'data': [
-        'views/mail_mail_statistics_view.xml',
-        'views/mail_mass_mailing_contact_view.xml',
-        'views/mail_mass_mailing_view.xml',
-        'views/res_partner_view.xml',
-        'wizard/partner_mail_list_wizard.xml'
+    "data": [
+        "views/mail_mail_statistics_view.xml",
+        "views/mail_mass_mailing_contact_view.xml",
+        "views/mail_mass_mailing_view.xml",
+        "views/res_partner_view.xml",
+        "wizard/partner_mail_list_wizard.xml",
     ],
     "installable": True,
 }
--- mass_mailing_partner/wizard/partner_mail_list_wizard.py
+++ mass_mailing_partner/wizard/partner_mail_list_wizard.py
@@ -11,18 +11,21 @@ class PartnerMailListWizard(models.TransientModel):
     _name = "partner.mail.list.wizard"
     _description = "Create contact mailing list"
 
-    mail_list_id = fields.Many2one(comodel_name="mail.mass_mailing.list",
-                                   string="Mailing List")
+    mail_list_id = fields.Many2one(
+        comodel_name="mail.mass_mailing.list", string="Mailing List"
+    )
     partner_ids = fields.Many2many(
-        comodel_name="res.partner", relation="mail_list_wizard_partner",
-        default=lambda self: self.env.context.get("active_ids"))
+        comodel_name="res.partner",
+        relation="mail_list_wizard_partner",
+        default=lambda self: self.env.context.get("active_ids"),
+    )
 
     @api.multi
     def add_to_mail_list(self):
-        contact_obj = self.env['mail.mass_mailing.contact']
+        contact_obj = self.env["mail.mass_mailing.contact"]
         partners = self.partner_ids
 
-        add_list = partners.filtered('mass_mailing_contact_ids')
+        add_list = partners.filtered("mass_mailing_contact_ids")
         for partner in add_list:
             partner.mass_mailing_contact_ids[0].list_ids |= self.mail_list_id
 
@@ -31,11 +34,11 @@ class PartnerMailListWizard(models.TransientModel):
             if not partner.email:
                 raise UserError(_("Partner '%s' has no email.") % partner.name)
             contact_vals = {
-                'partner_id': partner.id,
-                'list_ids': [[6, 0, [self.mail_list_id.id]]],
-                'title_id': partner.title or False,
-                'company_name': partner.company_id.name or False,
-                'country_id': partner.country_id or False,
-                'tag_ids': partner.category_id or False,
+                "partner_id": partner.id,
+                "list_ids": [[6, 0, [self.mail_list_id.id]]],
+                "title_id": partner.title or False,
+                "company_name": partner.company_id.name or False,
+                "country_id": partner.country_id or False,
+                "tag_ids": partner.category_id or False,
             }
             contact_obj.create(contact_vals)
