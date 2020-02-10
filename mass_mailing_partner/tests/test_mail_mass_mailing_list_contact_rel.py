3f9016505aa88802edcb742bf32448f679e7b72f
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-10 11:40:11 +0100

    [MIG] mass_mailing_partner: Migration to 13.0

--- mass_mailing_partner/tests/test_mail_mass_mailing_list_contact_rel.py
+++ mass_mailing_partner/tests/test_mail_mass_mailing_list_contact_rel.py
@@ -1,4 +1,5 @@
 # Copyright 2018 Tecnativa - Ernesto tejeda
+# Copyright 2020 Tecnativa - Manuel Calero
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import ValidationError
@@ -14,13 +15,8 @@ class MailMassMailingListContactRelCase(base.BaseCase):
         contact_test_2 = self.create_mailing_contact(
             {"name": "Contact test 2", "partner_id": self.partner.id}
         )
-        list_3 = self.create_mailing_list({"name": "List test 3"})
-
-        self.env["mail.mass_mailing.list_contact_rel"].create(
-            {"list_id": list_3.id, "contact_id": contact_test_1.id}
+        list_3 = self.create_mailing_list(
+            {"name": "List test 3", "contact_ids": [(4, contact_test_1.id)]}
         )
-
         with self.assertRaises(ValidationError):
-            self.env["mail.mass_mailing.list_contact_rel"].create(
-                {"list_id": list_3.id, "contact_id": contact_test_2.id}
-            )
+            list_3.contact_ids = [(4, contact_test_2.id)]
