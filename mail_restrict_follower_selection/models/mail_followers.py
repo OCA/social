1c713ad24a2559a80fa67d3d7261ee743317540c
Enric Tobella <etobella@creublanca.es>
2020-03-04 16:27:05 +0100

    [12.0][MIG] mail_restrict_follower_selection

--- mail_restrict_follower_selection/models/mail_followers.py
+++ mail_restrict_follower_selection/models/mail_followers.py
@@ -1,24 +1,31 @@
 # Copyright (C) 2018 Creu Blanca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 from odoo.tools.safe_eval import safe_eval
 
 
 class MailFollowers(models.Model):
     _inherit = 'mail.followers'
 
-    @api.model
-    def _add_follower_command(self, res_model, res_ids, partner_data,
-                              channel_data, force=True):
+    def _add_followers(self, res_model, res_ids, partner_ids, partner_subtypes,
+                       channel_ids, channel_subtypes,
+                       check_existing=False, existing_policy='skip'):
         domain = self.env[
             'mail.wizard.invite'
         ]._mail_restrict_follower_selection_get_domain()
         partners = self.env['res.partner'].search(
-            [('id', 'in', list(partner_data))] +
+            [('id', 'in', partner_ids)] +
             safe_eval(domain)
         )
-        return super()._add_follower_command(
-            res_model, res_ids,
-            {p.id: partner_data[p.id] for p in partners},
-            channel_data, force=force)
+        _res_ids = res_ids.copy() or [0]
+        new, update = super()._add_followers(
+            res_model, res_ids, partners.ids, partner_subtypes, channel_ids,
+            channel_subtypes, check_existing=check_existing,
+            existing_policy=existing_policy
+        )
+
+        for res_id in _res_ids:
+            if res_id not in new:
+                new.setdefault(res_id, list())
+        return new, update
