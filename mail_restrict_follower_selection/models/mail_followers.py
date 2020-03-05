03b5b686cb936dc002e8c0d2970dc5546d8e1633
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-05 13:38:11 +0100

    [MIG] mail_restrict_follower_selection: Migration to 12.0

--- mail_restrict_follower_selection/models/mail_followers.py
+++ mail_restrict_follower_selection/models/mail_followers.py
@@ -9,16 +9,16 @@ class MailFollowers(models.Model):
     _inherit = 'mail.followers'
 
     @api.model
-    def _add_follower_command(self, res_model, res_ids, partner_data,
-                              channel_data, force=True):
-        domain = self.env[
-            'mail.wizard.invite'
-        ]._mail_restrict_follower_selection_get_domain()
-        partners = self.env['res.partner'].search(
-            [('id', 'in', list(partner_data))] +
-            safe_eval(domain)
-        )
-        return super()._add_follower_command(
-            res_model, res_ids,
-            {p.id: partner_data[p.id] for p in partners},
-            channel_data, force=force)
+    def _add_default_followers(self, res_model, res_ids, partner_ids,
+                               channel_ids=None, customer_ids=None):
+        if partner_ids and res_ids:
+            domain = self.env[
+                'mail.wizard.invite'
+            ]._mail_restrict_follower_selection_get_domain()
+            partners = self.env['res.partner'].search(
+                [('id', 'in', partner_ids)] +
+                safe_eval(domain))
+            partner_ids = partners.ids
+        return super()._add_default_followers(
+            res_model, res_ids, partner_ids, channel_ids=channel_ids,
+            customer_ids=customer_ids)
---
+++ mail_restrict_follower_selection/readme/USAGE.rst
@@ -0,0 +1,7 @@
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/205/11.0
+
+For further information, please visit:
+
+* https://www.odoo.com/forum/help-1
