8ac33df4c52fc8bdffec5e31a9a1bbbf8db396f2
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-09 15:10:18 +0100

    [IMP] mail_restrict_follower_selection: black, isort

--- mail_restrict_follower_selection/models/mail_thread.py
+++ mail_restrict_follower_selection/models/mail_thread.py
@@ -3,22 +3,24 @@ from odoo.tools.safe_eval import safe_eval
 
 
 class MailThread(models.AbstractModel):
-    _inherit = 'mail.thread'
+    _inherit = "mail.thread"
 
     @api.multi
     def _message_add_suggested_recipient(
-            self, result, partner=None, email=None, reason=''):
+        self, result, partner=None, email=None, reason=""
+    ):
         result = super(MailThread, self)._message_add_suggested_recipient(
-            result, partner=partner, email=email, reason=reason)
+            result, partner=partner, email=email, reason=reason
+        )
         domain = self.env[
-            'mail.wizard.invite'
+            "mail.wizard.invite"
         ]._mail_restrict_follower_selection_get_domain()
         eval_domain = safe_eval(domain)
         for key in result:
             for partner_id, email, reason in result[key]:
                 if partner_id:
-                    partner = self.env['res.partner'].search(
-                        [('id', '=', partner_id)] + eval_domain
+                    partner = self.env["res.partner"].search(
+                        [("id", "=", partner_id)] + eval_domain
                     )
                     if not partner:
                         result[key].remove((partner_id, email, reason))
