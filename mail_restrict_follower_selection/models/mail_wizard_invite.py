a80fee0d7b000df63a8c9a984b4d550d8da203db
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-09 15:10:18 +0100

    [IMP] mail_restrict_follower_selection: black, isort

--- mail_restrict_follower_selection/models/mail_wizard_invite.py
+++ mail_restrict_follower_selection/models/mail_wizard_invite.py
@@ -3,31 +3,38 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from lxml import etree
+
 from odoo import api, models
 
 
 class MailWizardInvite(models.TransientModel):
-    _inherit = 'mail.wizard.invite'
+    _inherit = "mail.wizard.invite"
 
     @api.model
     def _mail_restrict_follower_selection_get_domain(self):
-        parameter_name = 'mail_restrict_follower_selection.domain'
-        return self.env['ir.config_parameter'].sudo().get_param(
-            "{0}.{1}".format(parameter_name,
-                             self.env.context.get('default_res_model')),
-            self.env['ir.config_parameter'].sudo().get_param(
-                parameter_name, default='[]')
+        parameter_name = "mail_restrict_follower_selection.domain"
+        return (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param(
+                "{}.{}".format(
+                    parameter_name, self.env.context.get("default_res_model")
+                ),
+                self.env["ir.config_parameter"]
+                .sudo()
+                .get_param(parameter_name, default="[]"),
+            )
         )
 
     @api.model
-    def fields_view_get(self, view_id=None, view_type='form', toolbar=False,
-                        submenu=False):
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
         result = super(MailWizardInvite, self).fields_view_get(
-            view_id=view_id, view_type=view_type, toolbar=toolbar,
-            submenu=submenu)
-        arch = etree.fromstring(result['arch'])
+            view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu
+        )
+        arch = etree.fromstring(result["arch"])
         for field in arch.xpath('//field[@name="partner_ids"]'):
-            field.attrib['domain'] = self\
-                ._mail_restrict_follower_selection_get_domain()
-        result['arch'] = etree.tostring(arch)
+            field.attrib["domain"] = self._mail_restrict_follower_selection_get_domain()
+        result["arch"] = etree.tostring(arch)
         return result
