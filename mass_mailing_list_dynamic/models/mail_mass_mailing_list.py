fa2fc83522bf51d0897a66a28b2736369055b4c8
Jared Kipe <jared@hibou.io>
2020-03-10 08:40:19 -0700

    [IMP] mass_mailing_list_dynamic: black, isort

--- mass_mailing_list_dynamic/models/mail_mass_mailing_list.py
+++ mass_mailing_list_dynamic/models/mail_mass_mailing_list.py
@@ -10,17 +10,14 @@ class MassMailingList(models.Model):
 
     dynamic = fields.Boolean(
         help="Set this list as dynamic, to make it autosynchronized with "
-             "partners from a given criteria.",
+        "partners from a given criteria."
     )
     sync_method = fields.Selection(
-        [
-            ("add", "Only add new records"),
-            ("full", "Add and remove records as needed"),
-        ],
+        [("add", "Only add new records"), ("full", "Add and remove records as needed")],
         default="add",
         required=True,
         help="Choose the syncronization method for this list if you want to "
-             "make it dynamic",
+        "make it dynamic",
     )
     sync_domain = fields.Char(
         string="Synchronization critera",
@@ -29,15 +26,12 @@ class MassMailingList(models.Model):
         help="Filter partners to sync in this list",
     )
     is_synced = fields.Boolean(
-        help="Helper field to make the user aware of unsynced changes",
-        default=True,
+        help="Helper field to make the user aware of unsynced changes", default=True
     )
 
     def action_sync(self):
         """Sync contacts in dynamic lists."""
-        Contact = self.env["mail.mass_mailing.contact"].with_context(
-            syncing=True,
-        )
+        Contact = self.env["mail.mass_mailing.contact"].with_context(syncing=True)
         Partner = self.env["res.partner"]
         # Skip non-dynamic lists
         dynamic = self.filtered("dynamic").with_context(syncing=True)
@@ -47,11 +41,12 @@ class MassMailingList(models.Model):
             # Detach or remove undesired contacts when synchronization is full
             if one.sync_method == "full":
                 contact_to_detach = one.contact_ids.filtered(
-                    lambda r: r.partner_id not in desired_partners)
+                    lambda r: r.partner_id not in desired_partners
+                )
                 one.contact_ids -= contact_to_detach
                 contact_to_detach.filtered(lambda r: not r.list_ids).unlink()
             # Add new contacts
-            current_partners = one.contact_ids.mapped('partner_id')
+            current_partners = one.contact_ids.mapped("partner_id")
             contact_to_list = self.env["mail.mass_mailing.contact"]
             vals_list = []
             for partner in desired_partners - current_partners:
@@ -59,10 +54,9 @@ class MassMailingList(models.Model):
                 if contacts_in_partner:
                     contact_to_list |= contacts_in_partner[0]
                 else:
-                    vals_list.append({
-                        "list_ids": [(4, one.id)],
-                        "partner_id": partner.id,
-                    })
+                    vals_list.append(
+                        {"list_ids": [(4, one.id)], "partner_id": partner.id}
+                    )
             one.contact_ids |= contact_to_list
             Contact.create(vals_list)
             one.is_synced = True
