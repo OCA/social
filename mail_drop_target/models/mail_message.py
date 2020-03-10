014823bcd9f7fcaf0f091ab5c6f3e50b8b924b1a
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-10 17:18:35 +0100

    [IMP] mail_drop_target: black, isort

--- mail_drop_target/models/mail_message.py
+++ mail_drop_target/models/mail_message.py
@@ -2,17 +2,25 @@ from odoo import api, models
 
 
 class MailMessage(models.Model):
-    _inherit = 'mail.message'
+    _inherit = "mail.message"
 
     @api.multi
     def _notify(
-        self, record, msg_vals, force_send=False, send_after_commit=True,
-        model_description=False, mail_auto_delete=True
+        self,
+        record,
+        msg_vals,
+        force_send=False,
+        send_after_commit=True,
+        model_description=False,
+        mail_auto_delete=True,
     ):
-        if self.env.context.get('message_create_from_mail_mail', False):
+        if self.env.context.get("message_create_from_mail_mail", False):
             return
         return super()._notify(
-            record, msg_vals, force_send=force_send,
+            record,
+            msg_vals,
+            force_send=force_send,
             send_after_commit=send_after_commit,
             model_description=model_description,
-            mail_auto_delete=mail_auto_delete)
+            mail_auto_delete=mail_auto_delete,
+        )
