a6f9f8b7862c30119143ec1ea609b5bf0022ede6
david <david.vidal@tecnativa.com>
2020-03-13 16:23:05 +0100

    [IMP] mass_mailing_unique: informative constraint

--- mass_mailing_unique/models/mail_mass_mailing_contact.py
+++ mass_mailing_unique/models/mail_mass_mailing_contact.py
@@ -18,5 +18,7 @@ class MailMassMailingContact(models.Model):
             contact_email = contact.email.strip().lower()
             other_emails = [e.strip().lower() for e in others.mapped('email')]
             if contact_email in other_emails:
-                raise ValidationError(_("Cannot have the same email more "
-                                        "than once in the same list"))
+                raise ValidationError(_(
+                    "Cannot have the same email (%s) more"
+                    "than once in the same list." % contact_email
+                ))
