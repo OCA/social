7c3a069b4548f16fcf14d7d14d353f8a74132be2
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-03-20 16:15:04 +0100

    [MIG] mail_debrand: Migration to v13.0

--- mail_debrand/tests/test_mail_debrand.py
+++ mail_debrand/tests/test_mail_debrand.py
@@ -7,19 +7,15 @@ from odoo.tests import common
 class TestMailDebrand(common.TransactionCase):
     def setUp(self):
         super().setUp()
-        self.default_arch = self.env.ref(
-            'mail.message_notification_email'
-        ).arch
-        self.paynow_arch = self.env.ref(
-            'mail.mail_notification_paynow'
-        ).arch
+        self.default_arch = self.env.ref("mail.message_notification_email").arch
+        self.paynow_arch = self.env.ref("mail.mail_notification_paynow").arch
 
     def test_default_debrand(self):
-        self.assertIn('using', self.default_arch)
+        self.assertIn("using", self.default_arch)
         res = self.env["mail.template"]._debrand_body(self.default_arch)
-        self.assertNotIn('using', res)
+        self.assertNotIn("using", res)
 
     def test_paynow_debrand(self):
-        self.assertIn('Powered by', self.paynow_arch)
+        self.assertIn("Powered by", self.paynow_arch)
         res = self.env["mail.template"]._debrand_body(self.paynow_arch)
-        self.assertNotIn('Powered by', res)
+        self.assertNotIn("Powered by", res)
