20a52c937309eab281741a00bb40ccaa0c3295d9
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-03-20 16:15:04 +0100

    [MIG] mail_debrand: Migration to v13.0

--- mail_debrand/__manifest__.py
+++ mail_debrand/__manifest__.py
@@ -7,15 +7,13 @@
 {
     "name": "Mail Debrand",
     "summary": "Remove Odoo branding in sent emails",
-    "version": "12.0.2.0.1",
+    "version": "13.0.0.0.1",
     "category": "Social Network",
     "website": "https://github.com/OCA/social/",
     "author": "Tecnativa, Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "mail",
-    ],
+    "depends": ["mail"],
     "development_status": "Stable",
     "maintainers": ["pedrobaeza"],
 }
