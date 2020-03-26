d620b253f13e30a424c22783cb741acb64ea97cd
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-03-26 20:43:45 +0100

    [IMP] mail_tracking: Test 'to' and named emails

--- mail_tracking/__manifest__.py
+++ mail_tracking/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Email tracking",
     "summary": "Email tracking system for all mails sent",
-    "version": "13.0.1.0.2",
+    "version": "13.0.1.0.3",
     "category": "Social Network",
     "website": "http://github.com/OCA/social",
     "author": ("Tecnativa, " "Odoo Community Association (OCA)"),
