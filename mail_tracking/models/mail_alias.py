1c16e4c15a5c0aae74911084a3988b367ad8e01d
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-04-16 19:36:32 +0200

    [IMP] mail_tracking: Ignore catchall

--- mail_tracking/models/mail_alias.py
+++ mail_tracking/models/mail_alias.py
@@ -10,10 +10,17 @@ class MailAlias(models.Model):
     @api.model
     @tools.ormcache()
     def get_aliases(self):
-        return {
+        aliases = {
             x["display_name"]
             for x in self.search_read([("alias_name", "!=", False)], ["display_name"])
         }
+        IrConfigParamObj = self.env["ir.config_parameter"].sudo()
+        catchall = "{}@{}".format(
+            IrConfigParamObj.get_param("mail.catchall.alias"),
+            IrConfigParamObj.get_param("mail.catchall.domain"),
+        )
+        aliases.add(catchall)
+        return aliases
 
     @api.model_create_multi
     def create(self, vals_list):
