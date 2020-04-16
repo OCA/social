7f64aeb7a53a2986189a84fb90842c532a10f9fe
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-04-16 19:36:32 +0200

    [IMP] mail_tracking: Ignore catchall

---
+++ mail_tracking/models/ir_config_parameter.py
@@ -0,0 +1,24 @@
+# Copyright 2020 Tecnativa - Alexandre Díaz
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class IrConfigParameter(models.Model):
+    _inherit = "ir.config_parameter"
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        self.env["mail.alias"].clear_caches()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        self.env["mail.alias"].clear_caches()
+        return res
+
+    def unlink(self):
+        res = super().unlink()
+        self.env["mail.alias"].clear_caches()
+        return res
