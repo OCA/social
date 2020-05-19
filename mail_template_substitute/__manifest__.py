d76b66db37c6ff12bb9662a474652a2ce9d264d3
juanantonio <ja.alfonso@qubiq.es>
2020-05-19 16:38:07 +0200

    [MIG] mail_template_substitute: Migration to 13.0

--- mail_template_substitute/__manifest__.py
+++ mail_template_substitute/__manifest__.py
@@ -6,10 +6,14 @@
     "summary": """
         This module allows to create substitution rules for mail templates.
         """,
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,"
+              "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
-    "depends": ["base", "mail", "report_substitute"],
-    "data": ["security/mail_template_substitution_rule.xml", "views/mail_template.xml"],
+    "depends": ["base", "mail"],
+    "data": [
+        "security/mail_template_substitution_rule.xml",
+        "views/mail_template.xml",
+    ],
 }
