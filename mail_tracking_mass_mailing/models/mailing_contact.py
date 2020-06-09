6fea297ed7dd17746679be157f499aea93e29045
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-09 10:10:35 -0300

    [MIG] mail_tracking_mass_mailing: Migration to 13.0
    * Change references to mail_mail_statistics model with mail_trace
    * Change references to mail.mass_mailing model with mailing.mailing
    * Change reference to statistics_ids field with mailing_trace_ids
    * Change reference to send_mail method with action_send_mail
    * Replace mailing.mailing name for required subject field
    * fix view reference not_blacklisted with filter_not_email_bl
    * fix not-stored compute, set value for all the records

--- mail_tracking_mass_mailing/models/mailing_contact.py
+++ mail_tracking_mass_mailing/models/mailing_contact.py
@@ -6,17 +6,19 @@ from odoo import api, fields, models
 
 
 class MailMassMailingContact(models.Model):
-    _name = "mail.mass_mailing.contact"
-    _inherit = ["mail.mass_mailing.contact", "mail.bounced.mixin"]
+    _name = "mailing.contact"
+    _inherit = ["mailing.contact", "mail.bounced.mixin"]
 
     email_score = fields.Float(
         string="Email score", readonly=True, store=False, compute="_compute_email_score"
     )
 
-    @api.multi
     @api.depends("email")
     def _compute_email_score(self):
-        for contact in self.filtered("email"):
+        with_email = self.filtered("email")
+        for contact in with_email:
             contact.email_score = self.env[
                 "mail.tracking.email"
             ].email_score_from_email(contact.email)
+        remaining = self - with_email
+        remaining.email_score = 0.0
