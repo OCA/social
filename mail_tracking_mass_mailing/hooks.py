ecdfa394c05b8c4927ec56b37883dc2cc64f734b
Katherine Zaoral <kz@adhoc.com.ar>
2020-06-09 10:10:30 -0300

    [IMP] mail_tracking_mass_mailing: black, isort, prettier

--- mail_tracking_mass_mailing/hooks.py
+++ mail_tracking_mass_mailing/hooks.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
+
 try:
     from odoo.addons.mail_tracking.hooks import column_add_with_value
 except ImportError:
@@ -13,8 +14,7 @@ _logger = logging.getLogger(__name__)
 
 def pre_init_hook(cr):
     if column_add_with_value:
-        _logger.info("Creating mail.mass_mailing.contact.email_score column "
-                     "with value 50.0")
+        _logger.info("Creating mail.mass_mailing.contact.email_score column " "with value 50.0")
         column_add_with_value(
-            cr, 'mail_mass_mailing_contact', 'email_score', 'double precision',
-            50.0)
+            cr, "mail_mass_mailing_contact", "email_score", "double precision", 50.0
+        )
