77a38af50d76780ea44b20a3f2459aa78f0524f0
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-19 12:47:06 +0200

    [ADD] mass_mailing_custom_unsubscribe_event: Allow to unsubscribe discretely from an event
    This addon extends the unsubscription process for allowing to unsubscribe
    only for an event.
    
    Standard process includes the mail in the general blacklist instead, which
    can be very unconvenient.
    
    This includes also the needed changes in the base module `mass_mailing_custom_unsubscribe`.

--- mass_mailing_custom_unsubscribe/models/mail_mass_mailing.py
+++ mass_mailing_custom_unsubscribe/models/mail_mass_mailing.py
@@ -1,7 +1,9 @@
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, tools
+from odoo.tools.safe_eval import safe_eval
 from itertools import groupby
 
 
@@ -31,3 +33,42 @@ class MailMassMailing(models.Model):
                 "action": action,
             })
         return super().update_opt_out(email, list_ids, value)
+
+    def update_opt_out_other(self, email, res_ids, value):
+        """Method for changing unsubscription for models with opt_out field."""
+        model = self.env[self.mailing_model_real].with_context(
+            active_test=False)
+        action = "unsubscription" if value else "subscription"
+        if 'opt_out' in model._fields:
+            email_fname = 'email_from'
+            if 'email' in model._fields:
+                email_fname = 'email'
+            records = model.search([
+                ('id', 'in', res_ids), (email_fname, 'ilike', email)])
+            records.write({'opt_out': value})
+            for res_id in res_ids:
+                self.env["mail.unsubscription"].create({
+                    "email": email,
+                    "mass_mailing_id": self.id,
+                    "unsubscriber_id": "%s,%d" % (
+                        self.mailing_model_real, res_id),
+                    "action": action,
+                })
+
+    def _get_opt_out_list(self):
+        """Handle models with opt_out field for excluding them."""
+        self.ensure_one()
+        model = self.env[self.mailing_model_real].with_context(
+            active_test=False)
+        if (self.mailing_model_real != "mail.mass_mailing.contact" and
+                'opt_out' in model._fields):
+            email_fname = 'email_from'
+            if 'email' in model._fields:
+                email_fname = 'email'
+            domain = safe_eval(self.mailing_domain)
+            domain = [('opt_out', '=', True)] + domain
+            recs = self.env[self.mailing_model_real].search(domain)
+            normalized_email = (
+                tools.email_split(c[email_fname]) for c in recs)
+            return set(e[0].lower() for e in normalized_email if e)
+        return super()._get_opt_out_list()
--- mass_mailing_custom_unsubscribe/readme/DESCRIPTION.rst
+++ mass_mailing_custom_unsubscribe/readme/DESCRIPTION.rst
@@ -6,3 +6,6 @@ This addon extends the unsubscription form to let you:
   mass mailing.
 - Provide proof on why you are sending mass mailings to a given contact, as
   required by the GDPR in Europe.
+- Handle discrete unsubscriptions from other recipients that are not a mailing
+  list. On standard module, unsubscriptions from these recipients directly
+  include that mail on the general blacklist.
