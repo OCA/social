3cc6de9d553c79eb09b20cbaf7682198de7c0a04
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-19 12:47:06 +0200

    [ADD] mass_mailing_custom_unsubscribe_event: Allow to unsubscribe discretely from an event
    This addon extends the unsubscription process for allowing to unsubscribe
    only for an event.
    
    Standard process includes the mail in the general blacklist instead, which
    can be very unconvenient.
    
    This includes also the needed changes in the base module `mass_mailing_custom_unsubscribe`.

---
+++ mass_mailing_custom_unsubscribe_event/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Allow to unsubscribe discretely from an event",
+    "category": "Marketing",
+    "version": "12.0.1.0.1",
+    "depends": [
+        "event",
+        "mass_mailing_custom_unsubscribe",
+    ],
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "data": [
+        "views/event_registration_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+}
---
+++ mass_mailing_custom_unsubscribe_event/readme/USAGE.rst
@@ -0,0 +1,8 @@
+#. Go to *Email Marketing > Mailings > Create*.
+#. Select "Event Registration" in *Recipients* field.
+#. Edit your mass mailing at wish, but remember to add a snippet from
+   *Footers*, so people have an *Unsubscribe* link.
+#. Send it.
+#. If somebody gets unsubscribed, it will get unsubscribed only for that
+   event, but including that mail in other event, the registree will still
+   receive mailings.
