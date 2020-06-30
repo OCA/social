7206175a23b59eda3e12d48182862f22a98211e1
Frédéric Garbely <frederic.garbely@braintec-group.com>
2020-06-30 16:21:11 +0200

    [IMP] mail_outbound_static: black, isort, prettier

--- mail_outbound_static/__manifest__.py
+++ mail_outbound_static/__manifest__.py
@@ -2,19 +2,15 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Mail Outbound Static',
-    'summary': 'Allows you to configure the from header for a mail server.',
-    'version': '13.0.1.0.0',
-    'category': 'Discuss',
-    'website': 'https://github.com/OCA/social',
-    'author': 'brain-tec AG, LasLabs, Odoo Community Association (OCA)',
-    'license': 'LGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'views/ir_mail_server_view.xml',
-    ],
+    "name": "Mail Outbound Static",
+    "summary": "Allows you to configure the from header for a mail server.",
+    "version": "13.0.1.0.0",
+    "category": "Discuss",
+    "website": "https://github.com/OCA/social",
+    "author": "brain-tec AG, LasLabs, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base"],
+    "data": ["views/ir_mail_server_view.xml"],
 }
