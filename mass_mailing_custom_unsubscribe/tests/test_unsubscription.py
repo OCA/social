e946a175912e55b4110f6236ace93f248c6ddf2e
Carlos Roca <carlos.roca@tecnativa.com>
2020-08-24 10:36:19 +0200

    [IMP] mass_mailing_custom_unsubscribe: black, isort, prettier

--- mass_mailing_custom_unsubscribe/tests/test_unsubscription.py
+++ mass_mailing_custom_unsubscribe/tests/test_unsubscription.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import SavepointCase
+
 from .. import exceptions
 
 
@@ -9,22 +10,26 @@ class UnsubscriptionCase(SavepointCase):
     def test_details_required(self):
         """Cannot create unsubscription without details when required."""
         with self.assertRaises(exceptions.DetailsRequiredError):
-            self.env["mail.unsubscription"].create({
-                "email": "axelor@yourcompany.example.com",
-                "mass_mailing_id": self.env.ref("mass_mailing.mass_mail_1").id,
-                "unsubscriber_id":
-                    "res.partner,%d" % self.env.ref("base.res_partner_2").id,
-                "reason_id":
-                    self.env.ref(
-                        "mass_mailing_custom_unsubscribe.reason_other").id,
-            })
+            self.env["mail.unsubscription"].create(
+                {
+                    "email": "axelor@yourcompany.example.com",
+                    "mass_mailing_id": self.env.ref("mass_mailing.mass_mail_1").id,
+                    "unsubscriber_id": "res.partner,%d"
+                    % self.env.ref("base.res_partner_2").id,
+                    "reason_id": self.env.ref(
+                        "mass_mailing_custom_unsubscribe.reason_other"
+                    ).id,
+                }
+            )
 
     def test_reason_required(self):
         """Cannot create unsubscription without reason when required."""
         with self.assertRaises(exceptions.ReasonRequiredError):
-            self.env["mail.unsubscription"].create({
-                "email": "axelor@yourcompany.example.com",
-                "mass_mailing_id": self.env.ref("mass_mailing.mass_mail_1").id,
-                "unsubscriber_id":
-                    "res.partner,%d" % self.env.ref("base.res_partner_2").id,
-            })
+            self.env["mail.unsubscription"].create(
+                {
+                    "email": "axelor@yourcompany.example.com",
+                    "mass_mailing_id": self.env.ref("mass_mailing.mass_mail_1").id,
+                    "unsubscriber_id": "res.partner,%d"
+                    % self.env.ref("base.res_partner_2").id,
+                }
+            )
