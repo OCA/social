6d0f4a6b8ffd06e827e40f2659cc6be3a49fc99e
Carlos Roca <carlos.roca@tecnativa.com>
2020-08-24 10:36:19 +0200

    [IMP] mass_mailing_custom_unsubscribe: black, isort, prettier

--- mass_mailing_custom_unsubscribe/controllers/main.py
+++ mass_mailing_custom_unsubscribe/controllers/main.py
@@ -6,6 +6,7 @@
 import logging
 
 from odoo.http import request, route
+
 from odoo.addons.mass_mailing.controllers.main import MassMailController
 
 _logger = logging.getLogger(__name__)
@@ -35,14 +36,15 @@ class CustomUnsubscribe(MassMailController):
                 "reasons": reasons,
                 "res_id": res_id,
                 "token": token,
-            })
+            },
+        )
 
     @route()
     def mailing(self, mailing_id, email=None, res_id=None, token="", **post):
         """Ask/save unsubscription reason."""
         _logger.debug(
-            "Called `mailing()` with: %r",
-            (mailing_id, email, res_id, token, post))
+            "Called `mailing()` with: %r", (mailing_id, email, res_id, token, post)
+        )
         reasons = request.env["mail.unsubscription.reason"].search([])
         res_id = res_id and int(res_id)
         try:
@@ -55,67 +57,89 @@ class CustomUnsubscribe(MassMailController):
             # Unsubscribe, saving reason and details by context
             details = post.get("details", False)
             self._add_extra_context(mailing_id, res_id, reason_id, details)
-            mailing_obj = request.env['mail.mass_mailing']
+            mailing_obj = request.env["mail.mass_mailing"]
             mass_mailing = mailing_obj.sudo().browse(mailing_id)
             model = mass_mailing.mailing_model_real
-            if ("opt_out" in request.env[model]._fields and
-                    model != "mail.mass_mailing.contact"):
+            if (
+                "opt_out" in request.env[model]._fields
+                and model != "mail.mass_mailing.contact"
+            ):
                 mass_mailing.update_opt_out_other(email, [res_id], True)
-                result = request.render("mass_mailing.page_unsubscribed", {
-                    "email": email,
-                    "mailing_id": mailing_id,
-                    "res_id": res_id,
-                    "show_blacklist_button": request.env[
-                        "ir.config_parameter"
-                    ].sudo().get_param(
-                        "mass_mailing.show_blacklist_buttons"
-                    ),
-                })
+                result = request.render(
+                    "mass_mailing.page_unsubscribed",
+                    {
+                        "email": email,
+                        "mailing_id": mailing_id,
+                        "res_id": res_id,
+                        "show_blacklist_button": request.env["ir.config_parameter"]
+                        .sudo()
+                        .get_param("mass_mailing.show_blacklist_buttons"),
+                    },
+                )
                 result.qcontext.update({"reasons": reasons})
             else:
                 # You could get a DetailsRequiredError here, but only if HTML5
                 # validation fails, which should not happen in modern browsers
-                result = super().mailing(
-                    mailing_id, email, res_id, token=token, **post)
+                result = super().mailing(mailing_id, email, res_id, token=token, **post)
                 if model == "mail.mass_mailing.contact":
                     # update list_ids taking into account
                     # not_cross_unsubscriptable field
-                    result.qcontext.update({
-                        "reasons": reasons,
-                        "list_ids": result.qcontext["list_ids"].filtered(
-                            lambda mailing_list:
-                                not mailing_list.not_cross_unsubscriptable or
-                                mailing_list in mass_mailing.contact_list_ids
-                        )
-                    })
+                    result.qcontext.update(
+                        {
+                            "reasons": reasons,
+                            "list_ids": result.qcontext["list_ids"].filtered(
+                                lambda m_list: not m_list.not_cross_unsubscriptable
+                                or m_list in mass_mailing.contact_list_ids
+                            ),
+                        }
+                    )
             return result
 
     @route()
-    def unsubscribe(self, mailing_id, opt_in_ids, opt_out_ids, email, res_id,
-                    token, reason_id=None, details=None):
+    def unsubscribe(
+        self,
+        mailing_id,
+        opt_in_ids,
+        opt_out_ids,
+        email,
+        res_id,
+        token,
+        reason_id=None,
+        details=None,
+    ):
         """Store unsubscription reasons when unsubscribing from RPC."""
         # Update request context
         self._add_extra_context(mailing_id, res_id, reason_id, details)
         _logger.debug(
             "Called `unsubscribe()` with: %r",
-            (mailing_id, opt_in_ids, opt_out_ids, email, res_id, token,
-             reason_id, details))
+            (
+                mailing_id,
+                opt_in_ids,
+                opt_out_ids,
+                email,
+                res_id,
+                token,
+                reason_id,
+                details,
+            ),
+        )
         return super().unsubscribe(
-            mailing_id, opt_in_ids, opt_out_ids, email, res_id, token)
+            mailing_id, opt_in_ids, opt_out_ids, email, res_id, token
+        )
 
     @route()
-    def blacklist_add(self, mailing_id, res_id, email, token, reason_id=None,
-                      details=None):
+    def blacklist_add(
+        self, mailing_id, res_id, email, token, reason_id=None, details=None
+    ):
         self._add_extra_context(mailing_id, res_id, reason_id, details)
-        return super().blacklist_add(
-            mailing_id, res_id, email, token)
+        return super().blacklist_add(mailing_id, res_id, email, token)
 
     @route()
-    def blacklist_remove(self, mailing_id, res_id, email, token,
-                         reason_id=None, details=None):
+    def blacklist_remove(
+        self, mailing_id, res_id, email, token, reason_id=None, details=None
+    ):
         self._add_extra_context(mailing_id, res_id, reason_id, details)
-        return super().blacklist_remove(
-            mailing_id, res_id, email, token)
+        return super().blacklist_remove(mailing_id, res_id, email, token)
 
     def _add_extra_context(self, mailing_id, res_id, reason_id, details):
         environ = request.httprequest.headers.environ
@@ -123,11 +147,8 @@ class CustomUnsubscribe(MassMailController):
         # redefinition of _add and _remove methods of the mail.blacklist class
         extra_context = {
             "default_metadata": "\n".join(
-                "%s: %s" % (val, environ.get(val)) for val in (
-                    "REMOTE_ADDR",
-                    "HTTP_USER_AGENT",
-                    "HTTP_ACCEPT_LANGUAGE",
-                )
+                "{}: {}".format(val, environ.get(val))
+                for val in ("REMOTE_ADDR", "HTTP_USER_AGENT", "HTTP_ACCEPT_LANGUAGE")
             ),
             "mailing_id": mailing_id,
             "unsubscription_res_id": int(res_id),
