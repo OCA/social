e46a653da244a94d43d3d3004d58c090f74bbb99
Radovan Skolnik <radovan@skolnik.info>
2020-08-24 15:42:33 +0200

    [IMP] mail_attach_existing_attachment: black, isort, prettier

--- mail_attach_existing_attachment/tests/test_mail_attach_existing_attachment.py
+++ mail_attach_existing_attachment/tests/test_mail_attach_existing_attachment.py
@@ -26,28 +26,34 @@ from odoo.tests import common
 
 
 class TestAttachExistingAttachment(common.TransactionCase):
-
     def setUp(self):
         super(TestAttachExistingAttachment, self).setUp()
-        self.partner_obj = self.env['res.partner']
-        self.partner_01 = self.env['res.partner'].create({
-            'name': 'Partner 1',
-            'email': 'partner1@example.org',
-            'is_company': True,
-            'parent_id': False,
-        })
+        self.partner_obj = self.env["res.partner"]
+        self.partner_01 = self.env["res.partner"].create(
+            {
+                "name": "Partner 1",
+                "email": "partner1@example.org",
+                "is_company": True,
+                "parent_id": False,
+            }
+        )
 
     def test_send_email_attachment(self):
-        attach1 = self.env['ir.attachment'].create({
-            'name': 'Attach1', 'datas_fname': 'Attach1',
-            'datas': 'bWlncmF0aW9uIHRlc3Q=',
-            'res_model': 'res.partner', 'res_id': self.partner_01.id})
-        vals = {'model': 'res.partner',
-                'partner_ids': [(6, 0, [self.partner_01.id])],
-                'res_id': self.partner_01.id,
-                'object_attachment_ids': [(6, 0, [attach1.id])]
-                }
-        mail = self.env['mail.compose.message'].create(vals)
+        attach1 = self.env["ir.attachment"].create(
+            {
+                "name": "Attach1",
+                "datas_fname": "Attach1",
+                "datas": "bWlncmF0aW9uIHRlc3Q=",
+                "res_model": "res.partner",
+                "res_id": self.partner_01.id,
+            }
+        )
+        vals = {
+            "model": "res.partner",
+            "partner_ids": [(6, 0, [self.partner_01.id])],
+            "res_id": self.partner_01.id,
+            "object_attachment_ids": [(6, 0, [attach1.id])],
+        }
+        mail = self.env["mail.compose.message"].create(vals)
         values = mail.get_mail_values([self.partner_01.id])
-        self.assertTrue(attach1.id in
-                        values[self.partner_01.id]['attachment_ids'])
+        self.assertTrue(attach1.id in values[self.partner_01.id]["attachment_ids"])
