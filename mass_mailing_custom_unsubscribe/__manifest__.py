dbbe5b3b21291c744a4fb6fd718221073bb878e9
Carlos Roca <carlos.roca@tecnativa.com>
2020-08-24 10:36:19 +0200

    [IMP] mass_mailing_custom_unsubscribe: black, isort, prettier

--- mass_mailing_custom_unsubscribe/__manifest__.py
+++ mass_mailing_custom_unsubscribe/__manifest__.py
@@ -3,32 +3,25 @@
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Customizable unsubscription process on mass mailing emails',
-    'summary': 'Know and track (un)subscription reasons, GDPR compliant',
-    'category': 'Marketing',
-    'version': '12.0.1.0.2',
-    'depends': [
-        'mass_mailing',
+    "name": "Customizable unsubscription process on mass mailing emails",
+    "summary": "Know and track (un)subscription reasons, GDPR compliant",
+    "category": "Marketing",
+    "version": "12.0.1.0.2",
+    "depends": ["mass_mailing"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/mail_unsubscription_reason.xml",
+        "templates/general_reason_form.xml",
+        "templates/mass_mailing_contact_reason.xml",
+        "views/assets.xml",
+        "views/mail_unsubscription_reason_view.xml",
+        "views/mail_mass_mailing_list_view.xml",
+        "views/mail_unsubscription_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'data/mail_unsubscription_reason.xml',
-        'templates/general_reason_form.xml',
-        'templates/mass_mailing_contact_reason.xml',
-        'views/assets.xml',
-        'views/mail_unsubscription_reason_view.xml',
-        'views/mail_mass_mailing_list_view.xml',
-        'views/mail_unsubscription_view.xml',
-    ],
-    'demo': [
-        'demo/assets.xml',
-    ],
-    'images': [
-        'images/form.png',
-    ],
-    'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/social',
-    'license': 'AGPL-3',
-    'installable': True,
+    "demo": ["demo/assets.xml"],
+    "images": ["images/form.png"],
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "installable": True,
 }
