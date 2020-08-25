e94ebe44000109efe9d077cdf9dc4ab4c5c0b889
Katherine Zaoral <kz@adhoc.com.ar>
2020-08-25 13:59:39 -0300

    mail_outbound_static: Domain whitelist based logic.

--- mail_outbound_static/__manifest__.py
+++ mail_outbound_static/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "14.0.1.0.0",
     "category": "Discuss",
     "website": "https://github.com/OCA/social",
-    "author": "brain-tec AG, LasLabs, Odoo Community Association (OCA)",
+    "author": "brain-tec AG, LasLabs, Adhoc SA, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
--- mail_outbound_static/readme/CONTRIBUTORS.rst
+++ mail_outbound_static/readme/CONTRIBUTORS.rst
@@ -2,3 +2,5 @@
 * Dave Lasley <dave@laslabs.com>
 * Lorenzo Battistini <https://github.com/eLBati>
 * Pierre Pizzetta <pierre@devreaction.com>
+* Katherine Zaoral <kz@adhoc.com.ar>
+* Juan José Scarafía <jjs@adhoc.com.ar>
