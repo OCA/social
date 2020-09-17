2a3e341a64c277cc040599e70090a0f45e393ed2
Andrea <a.stirpe@onestein.nl>
2020-09-17 11:54:25 +0200

    [FIX] support all languages, no need of translations

--- mail_debrand/tests/test_mail_debrand.py
+++ mail_debrand/tests/test_mail_debrand.py
@@ -1,7 +1,9 @@
 # Copyright 2017 Tecnativa - Pedro M. Baeza
+# Copyright 2020 Onestein - Andrea Stirpe
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
+from odoo.tools.misc import mute_logger
 
 
 class TestMailDebrand(common.TransactionCase):
@@ -19,3 +21,18 @@ class TestMailDebrand(common.TransactionCase):
         self.assertIn("Powered by", self.paynow_arch)
         res = self.env["mail.template"]._debrand_body(self.paynow_arch)
         self.assertNotIn("Powered by", res)
+
+    def test_lang_paynow_debrand(self):
+        with mute_logger("odoo.addons.base.models.ir_translation"):
+            self.env["base.language.install"].create(
+                {"lang": "nl_NL", "overwrite": True}
+            ).lang_install()
+        with mute_logger("odoo.tools.translate"):
+            self.env["base.update.translations"].create({"lang": "nl_NL"}).act_update()
+
+        ctx = dict(lang="nl_NL")
+        paynow_template = self.env.ref("mail.mail_notification_paynow")
+        paynow_arch = paynow_template.with_context(ctx).arch
+        self.assertIn("Aangeboden door", paynow_arch)
+        res = self.env["mail.template"].with_context(ctx)._debrand_body(paynow_arch)
+        self.assertNotIn("Aangeboden door", res)
