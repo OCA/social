8105798e1a150f8f5cabf1eb11b182abb3084e2a
Carlos Manuel <cmanuelalvarez11>
2020-09-29 00:25:24 -0400

    [MIG] mail_outbound_static: Improvements to version 10 and 12, it eases the sending of emails from a fixed "from" address to comply with DMARC conformity, use it when your emails can't be sent from Odoo due to invalid "from" address.
    The module is very useful as the fix in the Odoo stable version has not being accepted yet.
    
    Compared to v10 and v12, this module adds the feature to allow to setup the "from" address in the server conf file (usually odoo.conf), which is also a valid way for Odoo to setup outgoing email servers.

--- mail_outbound_static/models/ir_mail_server.py
+++ mail_outbound_static/models/ir_mail_server.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
+import odoo
 
 
 class IrMailServer(models.Model):
@@ -19,6 +21,7 @@ class IrMailServer(models.Model):
 
         # Replicate logic from core to get mail server
         mail_server = None
+        email_from = None
         if mail_server_id:
             mail_server = self.sudo().browse(mail_server_id)
         elif not smtp_server:
@@ -31,6 +34,22 @@ class IrMailServer(models.Model):
             else:
                 email_from = mail_server.smtp_from
 
+        elif odoo.tools.config['email_from']:
+            # If we do not have a smtp server defined we
+            # look for the email_from parameter from the
+            # odoo configuration
+            split_from = message['From'].rsplit(' <', 1)
+            if len(split_from) > 1:
+                email_from = '%s <%s>' % \
+                             (split_from[0],
+                              odoo.tools.config['email_from'],)
+            else:
+                email_from = odoo.tools.config['email_from']
+        else:
+            # do nothing
+            pass
+
+        if email_from:
             message.replace_header("From", email_from)
             bounce_alias = (
                 self.env["ir.config_parameter"].sudo().get_param("mail.bounce.alias")
--- mail_outbound_static/tests/test_ir_mail_server.py
+++ mail_outbound_static/tests/test_ir_mail_server.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2017 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
@@ -75,3 +76,61 @@ class TestIrMailServer(TransactionCase):
         self.assertEqual(
             message["Return-Path"], "{} <{}>".format(user, self.email_from)
         )
+
+        def test_send_email_remove_return_path_from_with_canonical(self):
+            """
+            It is responsible for handling the case that the header
+            return-path is not in the message
+            """
+            user = 'Test < User'
+            self.message.replace_header('From', '%s <test@example.com>' % user)
+            for header in self.message._headers:
+                if header[0].lower() == 'return-path':
+                    self.message._headers.remove(header)
+            bounce_parameter = self.parameter_model.search([
+                ('key', '=', 'mail.bounce.alias')])
+            if bounce_parameter:
+                # Remove mail.bounce.alias to test Return-Path
+                bounce_parameter.unlink()
+            # Also check passing mail_server_id
+            mail_server_id = self.Model.sudo().search(
+                [('name', '=', 'mail_server_test')], order='sequence',
+                limit=1)[0].id
+            message = self._send_mail(mail_server_id=mail_server_id)
+            self.assertEqual(
+                message['Return-Path'],
+                '%s <%s>' % (user, self.email_from),
+            )
+
+        def test_send_mail_get_from_conf_no_canonical(self):
+            """
+            It is responsible for managing the variant in which we extract
+            the email_from from .conf file
+            """
+            self.message.replace_header('From', 'test@example.com')
+            # Unlink to all mail servers to force get mail_from
+            # from .conf file
+            mail_server_ids = self.Model.sudo().search([])
+            mail_server_ids.sudo().unlink()
+
+            odoo.tools.config['email_from'] = 'from@example.com'
+            message = self._send_mail(mail_server_id=False)
+            self.assertEqual(message['From'], 'from@example.com')
+
+        def test_send_mail_get_from_conf_canonical(self):
+            """
+            It is responsible for managing the variant in which we extract
+            the email_from from .conf file
+            """
+            user = 'Test < User'
+            self.message.replace_header('From', '%s <test@example.com>' % user)
+            # Unlink to all mail servers to force get mail_from
+            # from .conf file
+            mail_server_ids = self.Model.sudo().search([])
+            mail_server_ids.sudo().unlink()
+
+            odoo.tools.config['email_from'] = 'from@example.com'
+            message = self._send_mail(mail_server_id=False)
+            self.assertEqual(
+                message['From'],
+                '%s <%s>' % (user, 'from@example.com'))
