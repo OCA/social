7065384e6b83a88edce25abdd5d8f5ee7f3c56c1
Pablo Fuentes <pablo@studio73.es>
2020-10-14 10:15:25 +0200

    [IMP] mass_mailing_event_registration_exclude: black

--- mass_mailing_event_registration_exclude/models/mail_mass_mailing_contact.py
+++ mass_mailing_event_registration_exclude/models/mail_mass_mailing_contact.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
+
 from .mail_mass_mailing import event_filtered_ids
 
 
 class MailMassMailingContact(models.Model):
-    _inherit = 'mail.mass_mailing.contact'
+    _inherit = "mail.mass_mailing.contact"
 
     @api.model
     def search_count(self, domain):
         res = super().search_count(domain)
-        mass_mailing_id = self.env.context.get('exclude_mass_mailing', False)
+        mass_mailing_id = self.env.context.get("exclude_mass_mailing", False)
         if mass_mailing_id:
-            res_ids = event_filtered_ids(
-                self, mass_mailing_id, domain, field='email')
+            res_ids = event_filtered_ids(self, mass_mailing_id, domain, field="email")
             res = len(res_ids) if res_ids else 0
         return res
