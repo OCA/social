af19d06ec87b09ddd6f545accd18fdd2f2f7f5a0
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2020-11-02 15:33:44 +0530

    [MIG] Migrated the module in v13.

--- mail_full_expand/__manifest__.py
+++ mail_full_expand/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mail full expand",
     "summary": "Expand mail in a big window",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, Odoo Community Association (OCA)",
