a051f861b00b90c7ef179e8b16c0e5e9491d5144
Sébastien BEAU <sebastien.beau@akretion.com>
2021-01-09 15:47:50 +0100

    [IMP] mail_send_copy: black, isort, prettier

--- mail_send_copy/models/ir_mail_server.py
+++ mail_send_copy/models/ir_mail_server.py
@@ -2,8 +2,9 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
 from email.utils import COMMASPACE
+
+from odoo import api, models
 
 
 class IrMailServer(models.Model):
@@ -14,8 +15,7 @@ class IrMailServer(models.Model):
         do_not_send_copy = self.env.context.get("do_not_send_copy", False)
         if not do_not_send_copy:
             if message["Bcc"]:
-                message["Bcc"] = message["Bcc"].join(
-                    COMMASPACE, message["From"])
+                message["Bcc"] = message["Bcc"].join(COMMASPACE, message["From"])
             else:
                 message["Bcc"] = message["From"]
         return super().send_email(message, *args, **kwargs)
