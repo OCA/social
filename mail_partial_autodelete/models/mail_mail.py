08f1fa11a9bd0ef8956e364fb5346d3844aabef1
Kevin Khao <kevinkhao@gmail.com>
2021-01-15 17:07:25 +0100

    [14.0][ADD] mail_partial_autodelete

---
+++ mail_partial_autodelete/models/mail_mail.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Akretion (http://www.akretion.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models
+
+
+class MailMail(models.Model):
+    _inherit = "mail.mail"
+
+    def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
+        self = self.with_context(autodelete_skip_unlink=True)
+        super()._send(auto_commit, raise_exception, smtp_session)
+
+    def unlink(self):
+        to_unlink = self
+        if self.env.context.get("autodelete_skip_unlink"):
+            to_purge = self.filtered(lambda r: r.auto_delete)
+            to_purge.write({"body_html": "", "body": ""})
+            to_unlink -= to_purge
+        super(MailMail, to_unlink).unlink()
---
+++ mail_partial_autodelete/readme/USAGE.rst
@@ -0,0 +1,1 @@
+Just install this module and if needed put context key force_autodelete for emails you don't care about.
