c3326b92cce66353f8946b3ded0e5dc10c055625
Atchuthan, Sodexis <atchuthan@sodexis.com>
2021-01-22 15:06:00 +0530

    [MIG] mail_activity_board: Migration to 14.0
    [FIX] add title to <i> tag and removed console.log in js
    
    Co-authored-by: Miquel Raïch <miquel.raich@forgeflow.com>
    
    Co-authored-by: Miquel Raïch <miquel.raich@forgeflow.com>

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -1,16 +1,17 @@
 # Copyright 2018 David Juaneda - <djuaneda@sdi.es>
+# Copyright 2021 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Board",
     "summary": "Add Activity Boards",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": "SDi, David Juaneda, Odoo Community Association (OCA)",
+    "author": "SDi, David Juaneda, Sodexis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["calendar", "board"],
     "data": ["views/templates.xml", "views/mail_activity_view.xml"],
-    "qweb": ["static/src/xml/inherit_chatter.xml"],
+    "qweb": ["static/src/components/chatter_topbar/chatter_topbar.xml"],
 }
