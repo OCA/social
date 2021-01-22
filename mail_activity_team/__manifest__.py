3059c491546fae2d18d334420db9d0c879219b0e
Atchuthan, Sodexis <atchuthan@sodexis.com>
2021-01-22 13:01:34 +0530

    [MIG] mail_activity_board,mail_activity_team: Migration to 14.0

--- mail_activity_team/__manifest__.py
+++ mail_activity_team/__manifest__.py
@@ -1,13 +1,14 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2021 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Team",
     "summary": "Add Teams to Activities",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "Eficent, Sodexis, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["mail_activity_board"],
