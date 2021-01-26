a2f5e387cca46a2fc4a751c58428fbf9e2c9562f
Jairo Llopis <jairo.llopis@tecnativa.com>
2021-01-26 14:06:17 +0000

    [IMP] pre-commit run -a

--- mail_activity_team/__manifest__.py
+++ mail_activity_team/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Team",
@@ -7,7 +7,7 @@
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": ["mail_activity_board"],
