c507a6b8fd149ac5bc551e293cdfe581c51a696a
hda <hughes.damry@acsone.eu>
2021-02-15 13:44:12 +0100

    [IMP] mail_as_letter: black, isort, prettier

--- mail_as_letter/__manifest__.py
+++ mail_as_letter/__manifest__.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Mail As Letter',
-    'summary': """
+    "name": "Mail As Letter",
+    "summary": """
         This module allows to download a mail message as a pdf letter.""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu/',
-    'depends': [
-        'mail',
-    ],
-    'data': [
-        'wizards/mail_compose_message.xml',
-        'report/mail_as_letter_qweb.xml',
-    ],
-    'demo': [
-    ],
-    'installable': True,
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "depends": ["mail",],
+    "data": ["wizards/mail_compose_message.xml", "report/mail_as_letter_qweb.xml",],
+    "demo": [],
+    "installable": True,
 }
--- mail_as_letter/wizards/mail_compose_message.py
+++ mail_as_letter/wizards/mail_compose_message.py
@@ -1,45 +1,48 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class MailComposeMessage(models.TransientModel):
 
-    _inherit = 'mail.compose.message'
+    _inherit = "mail.compose.message"
 
     # The number of partner is needed to hide the 'print' button if
     # there is more than one partner
     partner_count = fields.Integer(
-        string='Number of partners',
-        compute='_compute_partner_count')
+        string="Number of partners", compute="_compute_partner_count"
+    )
 
-    @api.depends('partner_ids')
+    @api.depends("partner_ids")
     def _compute_partner_count(self):
         for rec in self:
             rec.partner_count = len(rec.partner_ids)
 
     @api.multi
     def download_pdf(self):
-        '''
+        """
         Download the email in pdf format, based on a QWeb report.
-        '''
+        """
         self.ensure_one()
         if self.partner_count != 1:
             raise UserError(_("There must be only one recipient."))
         ctx = self.env.context.copy()
-        ctx.update({'active_id': self.id,
-                    'active_ids': [self.id],
-                    'active_model': 'mail.compose.message'})
+        ctx.update(
+            {
+                "active_id": self.id,
+                "active_ids": [self.id],
+                "active_model": "mail.compose.message",
+            }
+        )
 
         return {
-            'name': 'mail_as_letter',
-            'model': 'mail.compose.message',
-            'res_id': self.id,
-            'type': 'ir.actions.report.xml',
-            'report_name':
-                'mail_as_letter.report_mail_print',
-            'report_type': 'qweb-pdf',
-            'context': ctx, }
+            "name": "mail_as_letter",
+            "model": "mail.compose.message",
+            "res_id": self.id,
+            "type": "ir.actions.report.xml",
+            "report_name": "mail_as_letter.report_mail_print",
+            "report_type": "qweb-pdf",
+            "context": ctx,
+        }
