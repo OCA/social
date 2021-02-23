acb36f78905bffe1f2523c884a566ae10a67307e
Tran Thanh Phuc <phuc@trobz.com>
2021-02-23 10:24:44 +0700

    [MIG] email_template_qweb: Migration to 14.0

--- email_template_qweb/tests/test_mail_template_qweb.py
+++ email_template_qweb/tests/test_mail_template_qweb.py
@@ -6,7 +6,7 @@ from odoo.tests.common import TransactionCase
 class TestMailTemplateQweb(TransactionCase):
     def test_email_template_qweb(self):
         template = self.env.ref("email_template_qweb.email_template_demo1")
-        mail_values = template.generate_email([self.env.user.id])
+        mail_values = template.generate_email([self.env.user.id], ["body_html"])
         self.assertTrue(
             # this comes from the called template if everything worked
             "<footer>" in mail_values[self.env.user.id]["body_html"],
@@ -14,7 +14,7 @@ class TestMailTemplateQweb(TransactionCase):
             % (mail_values[self.env.user.id]["body_html"]),
         )
         # the same method is also called in a non multi mode
-        mail_values = template.generate_email(self.env.user.id)
+        mail_values = template.generate_email(self.env.user.id, ["body_html"])
         self.assertTrue(
             # this comes from the called template if everything worked
             "<footer>" in mail_values["body_html"],
