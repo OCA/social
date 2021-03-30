94be8655311f2fb9ddeb426cc5f13ffa300f6911
Víctor Martínez <victor.martinez@tecnativa.com>
2021-03-30 17:13:35 +0200

    [MIG] mail_preview_base: Migration to 14.0
    Co-authored-by: Enric Tobella <etobella@creublanca.es>

--- mail_preview_base/__manifest__.py
+++ mail_preview_base/__manifest__.py
@@ -5,11 +5,14 @@
     "name": "Mail Preview",
     "summary": """
         Base to add more previewing options""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "depends": ["mail"],
-    "data": ["template/assets.xml"],
+    "data": [
+        "template/assets.xml",
+        "views/ir_attachment_view.xml",
+    ],
     "qweb": ["static/src/xml/preview.xml"],
 }
