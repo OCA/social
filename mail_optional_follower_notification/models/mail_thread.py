f2a43bdbd826939eba9015a4f47ca7827792203e
Ioan Galan <ioan@studio73.es>
2021-05-07 16:46:45 +0200

    [MIG] mail_optional_follower_notification: Migration to 14.0

--- mail_optional_follower_notification/models/mail_thread.py
+++ mail_optional_follower_notification/models/mail_thread.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import models
+from odoo.tools import config
 
 
 class MailThread(models.AbstractModel):
@@ -10,7 +11,12 @@ class MailThread(models.AbstractModel):
     def _notify_compute_recipients(self, message, msg_vals):
         """Compute recipients to notify based on subtype and followers. This
         method returns data structured as expected for ``_notify_recipients``."""
+        test_condition = config["test_enable"] and not self.env.context.get(
+            "test_optional_follow_notification"
+        )
         recipient_data = super()._notify_compute_recipients(message, msg_vals)
+        if test_condition:
+            return recipient_data
         if "notify_followers" in self.env.context and not self.env.context.get(
             "notify_followers", False
         ):
