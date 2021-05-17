87329aebb72424e4c2996ed74a154283e4cfbe2a
eLBati <lb@takobi.online>
2021-05-17 12:15:44 +0200

    IMP mail_show_follower metadata and style Display CC according to notification_type

--- mail_show_follower/models/mail_mail.py
+++ mail_show_follower/models/mail_mail.py
@@ -1,6 +1,3 @@
-# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from odoo import models, api
 
 
@@ -9,7 +6,12 @@ class MailMail(models.Model):
 
     @api.multi
     def _send(self, auto_commit=False, raise_exception=False, smtp_session=None):
-        plain_text = '<div summary="o_mail_notification" style="padding: 0px; font-size: 10px;"><b>CC</b>: %s<hr style="background-color:rgb(204,204,204);border:medium none;clear:both;display:block;font-size:0px;min-height:1px;line-height:0; margin:4px 0 12px 0;"></div>'
+        plain_text = (
+            '<div summary="o_mail_notification" style="padding: 0px; '
+            'font-size: 10px;"><b>CC</b>: %s<hr style="background-color:'
+            'rgb(204,204,204);border:medium none;clear:both;display:block;'
+            'font-size:0px;min-height:1px;line-height:0; margin:4px 0 12px 0;"></div>'
+        )
         group_portal = self.env.ref('base.group_portal')
         for mail_id in self.ids:
             mail = self.browse(mail_id)
@@ -25,28 +27,44 @@ class MailMail(models.Model):
                     user_partner_ids = self.env['res.users'].search([
                         ('active', 'in', (True, False)),
                         ('show_in_cc', '=', False),
-                    ]).filtered(lambda x: not group_portal in x.groups_id).mapped('partner_id').ids
+                    ]).filtered(
+                        lambda x: group_portal not in x.groups_id
+                    ).mapped('partner_id').ids
                     partners_len = len(partners_obj.filtered(
-                        lambda x: x.id not in user_partner_ids and (not x.user_ids or group_portal in x.user_ids.groups_id)
-                        ))
+                        lambda x: x.id not in user_partner_ids and (
+                            not x.user_ids or group_portal in x.user_ids.groups_id
+                        )))
                     if partners_len > 1:
                         # get partners
-                        partners = None
                         cc_internal = True
                         # else get company in object
                         if hasattr(obj, "company_id") and obj.company_id:
                             cc_internal = obj.company_id.show_internal_users_cc
                         # get company in user
                         elif mail.env and mail.env.user and mail.env.user.company_id:
-                            cc_internal = self.env.user.company_id.show_internal_users_cc
+                            cc_internal = self.env.user.company_id.\
+                                show_internal_users_cc
                         if cc_internal:
                             partners = partners_obj.filtered(
-                                lambda x: x.id not in user_partner_ids and (not x.user_ids or x.user_ids.show_in_cc)
+                                lambda x: x.id not in user_partner_ids and (
+                                    not x.user_ids or x.user_ids.show_in_cc
+                                )
                             )
                         else:
                             partners = partners_obj.filtered(
-                                lambda x: x.id not in user_partner_ids and (not x.user_ids or group_portal in x.user_ids.groups_id)
+                                lambda x: x.id not in user_partner_ids and (
+                                    not x.user_ids or group_portal in
+                                    x.user_ids.groups_id
+                                )
                             )
+                        partners = partners.filtered(
+                            lambda x:
+                            not x.user_ids
+                            or
+                            # otherwise, email is not sent
+                            x.user_ids and "email" in x.user_ids.mapped(
+                                "notification_type")
+                        )
                         # get names and emails
                         final_cc = None
                         mails = ""
@@ -57,4 +75,7 @@ class MailMail(models.Model):
                         # it is saved in the body_html field so that it does
                         # not appear in the odoo log
                         mail.body_html = final_cc + mail.body_html
-        return super(MailMail, self)._send(auto_commit=auto_commit, raise_exception=raise_exception, smtp_session=smtp_session)
+        return super(MailMail, self)._send(
+            auto_commit=auto_commit, raise_exception=raise_exception,
+            smtp_session=smtp_session
+        )
--- mail_show_follower/models/res_config_settings.py
+++ mail_show_follower/models/res_config_settings.py
@@ -1,6 +1,3 @@
-# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from odoo import fields, models
 
 
