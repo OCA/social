9e84e6ff7a13f0069d86a91d2ddbe6ad5c4f22e7
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2021-05-17 11:59:44 +0200

    ADD mail_show_follower copied from https://github.com/sygel-technology/sy-server-backend/tree/12.0/mail_show_follower

---
+++ mail_show_follower/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import mail_mail
+from . import res_company
+from . import res_config_settings
+from . import res_users
