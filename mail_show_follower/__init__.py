f12f1b6ceaed5fd777c6e6023018bf581440abc3
eLBati <lb@takobi.online>
2021-05-17 12:15:44 +0200

    IMP mail_show_follower metadata and style Display CC according to notification_type

--- mail_show_follower/__init__.py
+++ mail_show_follower/__init__.py
@@ -1,4 +1,1 @@
-# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from . import models
--- mail_show_follower/models/__init__.py
+++ mail_show_follower/models/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from . import mail_mail
 from . import res_company
 from . import res_config_settings
