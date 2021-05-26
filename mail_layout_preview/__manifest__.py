82a20060d81972d2037b78ff40dd44a50cc013e6
Tran Thanh Phuc <phuc@trobz.com>
2021-05-26 14:19:09 +0700

    [MIG] mail_layout_preview: Migration to 14.0

--- mail_layout_preview/__manifest__.py
+++ mail_layout_preview/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail Preview",
     "summary": """
         Preview email templates in the browser""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
