69a6b944ec88acaf9531b9f3663f5d3e8748cbea
Chandresh Thakkar OSI <cthakkar@opensourceintegrators.com>
2021-05-29 00:20:48 +0530

    [NEW] Added new module to restrict chatter buttons.

---
+++ mail_restrict_send_button/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Restrict Send Button",
+    "summary": "Security for Send Message Button on Chatter Area",
+    "version": "14.0.1.0.1",
+    "category": "Social Network",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "installable": True,
+    "maintainer": ["dreispt"],
+    "development_status": "Alpha",
+    "data": ["security/res_groups.xml", "views/assets.xml"],
+    "qweb": ["static/src/xml/chatter.xml"],
+}
