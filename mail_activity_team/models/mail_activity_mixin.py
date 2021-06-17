562247b89d58c9a6667c0a63c1c43546757dd42e
david <david.vidal@tecnativa.com>
2021-06-17 09:52:53 +0200

    [FIX] mail_activity_team: archived users
    - If a user is archived but there's an automatic activity, the team
    check wouldn't locate him. We have to ensure the context.
    - When the automatic activity gets a user with no team, we should set
    the team to a void one as well.
    
    TT30470

--- mail_activity_team/models/mail_activity_mixin.py
+++ mail_activity_team/models/mail_activity_mixin.py
@@ -21,8 +21,8 @@ class MailActivityMixin(models.AbstractModel):
                 .with_context(default_res_model=self._name,)
                 ._get_default_team_id(user_id=user_id)
             )
-            if team:
-                act_values.update({"team_id": team.id})
+            # Even if it comes empty, we don't want to mismatch the user's team
+            act_values.update({"team_id": team.id})
         return super().activity_schedule(
             act_type_xmlid=act_type_xmlid,
             date_deadline=date_deadline,
