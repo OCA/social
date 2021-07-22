c01ab8a8b514ad5ba8dd6089a6ba19aafd55d2fe
Carlos Roca <carlos.roca@tecnativa.com>
2021-07-22 11:28:44 +0200

    [MIG] mail_tracking_mass_mailing: Migration to v14.0

--- mail_tracking_mass_mailing/__manifest__.py
+++ mail_tracking_mass_mailing/__manifest__.py
@@ -1,16 +1,16 @@
-# Copyright 2016 Antonio Espinosa - <antonio.espinosa@tecnativa.com>
-# Copyright 2017 Vicent Cubells - <vicent.cubells@tecnativa.com>
-# Copyright 2017 David Vidal - <david.vidal@tecnativa.com>
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2017 Tecnativa - David Vidal
 # Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Mail tracking for mass mailing",
     "summary": "Improve mass mailing email tracking",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
