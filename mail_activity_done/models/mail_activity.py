90e7cfad4ba4b5ad6df4dac1f2e3570947ba7353
manu <manuel.regidor@sygel.es>
2021-08-13 12:50:46 +0200

    [IMP]mail_activity_done: black, isort, prettier

--- mail_activity_done/models/mail_activity.py
+++ mail_activity_done/models/mail_activity.py
@@ -5,26 +5,27 @@ from odoo import api, fields, models
 
 class MailActivity(models.Model):
 
-    _inherit = 'mail.activity'
+    _inherit = "mail.activity"
 
     active = fields.Boolean(default=True)
     done = fields.Boolean(default=False)
-    state = fields.Selection(selection_add=[
-        ('done', 'Done')], compute='_compute_state')
+    state = fields.Selection(selection_add=[("done", "Done")], compute="_compute_state")
     date_done = fields.Date(
-        'Completed Date', index=True, readonly=True,
+        "Completed Date",
+        index=True,
+        readonly=True,
     )
 
-    @api.depends('date_deadline', 'done')
+    @api.depends("date_deadline", "done")
     def _compute_state(self):
         super(MailActivity, self)._compute_state()
         for record in self.filtered(lambda activity: activity.done):
-            record.state = 'done'
+            record.state = "done"
 
 
 class MailActivityMixin(models.AbstractModel):
 
-    _inherit = 'mail.activity.mixin'
+    _inherit = "mail.activity.mixin"
     activity_ids = fields.One2many(
-        domain=lambda self: [('res_model', '=', self._name),
-                             ('active', '=', True)])
+        domain=lambda self: [("res_model", "=", self._name), ("active", "=", True)]
+    )
