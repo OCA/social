375ecf59848f770a67815a3cf9874ae69a974d89
manu <manuel.regidor@sygel.es>
2021-08-13 12:50:46 +0200

    [IMP]mail_activity_done: black, isort, prettier

--- mail_activity_done/hooks.py
+++ mail_activity_done/hooks.py
@@ -1,23 +1,27 @@
 # Copyright 2018 Eficent Business and IT Consulting Services S.L.
 # Copyright 2018 Odoo, S.A.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-from odoo.addons.mail.models.mail_activity import MailActivity
 from odoo import fields
+
+from odoo.addons.mail.models.mail_activity import MailActivity
 
 
 def pre_init_hook(cr):
-    """ The objective of this hook is to default to false all values of field
+    """The objective of this hook is to default to false all values of field
     'done' of mail.activity
     """
-    cr.execute("""SELECT column_name
+    cr.execute(
+        """SELECT column_name
     FROM information_schema.columns
     WHERE table_name='mail_activity' AND
-    column_name='done'""")
+    column_name='done'"""
+    )
     if not cr.fetchone():
         cr.execute(
             """
             ALTER TABLE mail_activity ADD COLUMN done boolean;
-            """)
+            """
+        )
 
     cr.execute(
         """
@@ -28,12 +32,11 @@ def pre_init_hook(cr):
 
 
 def post_load_hook():
-
     def new_action_feedback(self, feedback=False):
 
-        if 'done' not in self._fields:
+        if "done" not in self._fields:
             return self.action_feedback_original(feedback=feedback)
-        message = self.env['mail.message']
+        message = self.env["mail.message"]
         if feedback:
             self.write(dict(feedback=feedback))
         for activity in self:
@@ -42,23 +45,23 @@ def post_load_hook():
             activity.active = False
             activity.date_done = fields.Date.today()
             record.message_post_with_view(
-                'mail.message_activity_done',
-                values={'activity': activity},
-                subtype_id=self.env.ref('mail.mt_activities').id,
+                "mail.message_activity_done",
+                values={"activity": activity},
+                subtype_id=self.env.ref("mail.mt_activities").id,
                 mail_activity_type_id=activity.activity_type_id.id,
             )
             message |= record.message_ids[0]
         return message.ids and message.ids[0] or False
 
-    if not hasattr(MailActivity, 'action_feedback_original'):
+    if not hasattr(MailActivity, "action_feedback_original"):
         MailActivity.action_feedback_original = MailActivity.action_feedback
         MailActivity.action_feedback = new_action_feedback
 
 
 def uninstall_hook(cr, registry):
-    """ The objective of this hook is to remove all activities that are done
-        upon module uninstall
-        """
+    """The objective of this hook is to remove all activities that are done
+    upon module uninstall
+    """
     cr.execute(
         """
         DELETE FROM mail_activity
