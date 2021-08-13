add436681845db4cf46d78871e710dbe2a5680df
manu <manuel.regidor@sygel.es>
2021-08-13 12:53:43 +0200

    [MIG]mail_activity_done: Migration to 14.0

--- mail_activity_done/models/mail_activity.py
+++ mail_activity_done/models/mail_activity.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent <http://www.eficent.com>
+# Copyright 2018-20 ForgeFlow <http://www.forgeflow.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import api, fields, models
 
@@ -9,18 +9,56 @@ class MailActivity(models.Model):
 
     active = fields.Boolean(default=True)
     done = fields.Boolean(default=False)
-    state = fields.Selection(selection_add=[("done", "Done")], compute="_compute_state")
-    date_done = fields.Date(
-        "Completed Date",
-        index=True,
-        readonly=True,
+    state = fields.Selection(
+        selection_add=[("done", "Done")],
+        compute="_compute_state",
+        search="_search_state",
     )
+    date_done = fields.Date("Completed Date", index=True, readonly=True)
 
     @api.depends("date_deadline", "done")
     def _compute_state(self):
-        super(MailActivity, self)._compute_state()
+        super()._compute_state()
         for record in self.filtered(lambda activity: activity.done):
             record.state = "done"
+
+    def _search_state(self, operator, operand):
+        if not operand:
+            # checking for is (not) set
+            if operator == "=":
+                # is not set - never happens actually so we create impossible domain
+                return [("id", "=", False)]
+            else:
+                # is set - always - return empty domain
+                return []
+        else:
+            # checking for value
+            if operand == "done":
+                if operator == "=":
+                    return ["&", ("done", operator, True), ("active", "=", False)]
+                else:
+                    return ["&", ("done", operator, False), ("active", "=", True)]
+            else:
+                if operator == "=":
+                    return [
+                        "&",
+                        (
+                            "date_deadline",
+                            {"today": "=", "overdue": "<", "planned": ">"}[operand],
+                            fields.Date.today(),
+                        ),
+                        ("done", "=", False),
+                    ]
+                else:
+                    return [
+                        "|",
+                        (
+                            "date_deadline",
+                            {"today": "!=", "overdue": ">=", "planned": "<="}[operand],
+                            fields.Date.today(),
+                        ),
+                        ("done", "=", True),
+                    ]
 
 
 class MailActivityMixin(models.AbstractModel):
