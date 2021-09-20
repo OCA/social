917c03bfb430336da6706312b40686f999033e00
Ivàn Todorovich <ivan.todorovich@gmail.com>
2021-09-20 12:09:44 -0300

    [MIG] mass_mailing_unique: Migration to 14.0.

--- mass_mailing_unique/models/__init__.py
+++ mass_mailing_unique/models/__init__.py
@@ -1,8 +1,2 @@
-# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
-# Copyright 2016 Tecnativa - Vicent Cubells
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import mail_mass_mailing_contact
-from . import mail_mass_mailing_list
-from . import mail_mass_mailing_list_contact_rel
-from . import mass_mailing
+from . import mailing_contact
+from . import mailing_list
--- mass_mailing_unique/readme/DESCRIPTION.rst
+++ mass_mailing_unique/readme/DESCRIPTION.rst
@@ -1,5 +1,5 @@
 This module extends the functionality of mass mailing lists to disable
-duplicate entries in list names and contact emails per list.
+duplicate entries in list names and contact emails.
 
 This way you will avoid conflicts when importing contacts to a list that has a
 duplicated name.
