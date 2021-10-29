3eb22a4bf45948eab4196e18917a28c793add429
Ernesto Tejeda <ernesto.tejeda87@gmail.com>
2021-10-29 19:35:47 -0400

    [MIG] mail_full_expand: Migration to 14.0

--- mail_full_expand/static/src/js/mail_full_expand.js
+++ mail_full_expand/static/src/js/mail_full_expand.js
@@ -1,33 +1,26 @@
 /* Copyright 2014-2015 Grupo ESOC <http://www.grupoesoc.es>
  * License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
  */
-odoo.define("mail_full_expand.expand", function (require) {
-    "use strict";
+odoo.define(
+    "mail_full_expand/static/src/components/message/mail_full_expand.js",
+    function (require) {
+        "use strict";
 
-    var ThreadWidget = require("mail.widget.Thread");
+        const components = {
+            Message: require("mail/static/src/components/message/message.js"),
+        };
+        const {patch} = require("web.utils");
 
-    ThreadWidget.include({
-        events: _.defaults(
-            {
-                "click .o_full_expand": "_onClickMessageFullExpand",
+        patch(components.Message, "mail_full_expand.main", {
+            async _onClickMessageFullExpand(ev) {
+                ev.stopPropagation();
+                const action = await this.rpc({
+                    route: "/web/action/load",
+                    params: {action_id: "mail_full_expand.mail_message_action"},
+                });
+                action.res_id = this.message.id;
+                this.env.bus.trigger("do-action", {action: action});
             },
-            ThreadWidget.prototype.events
-        ),
-
-        _onClickMessageFullExpand: function (event) {
-            // Get the action data and execute it to open the full view
-            var do_action = this.do_action,
-                msg_id = $(event.currentTarget).data("message-id");
-
-            this._rpc({
-                route: "/web/action/load",
-                params: {
-                    action_id: "mail_full_expand.mail_message_action",
-                },
-            }).then(function (action) {
-                action.res_id = msg_id;
-                do_action(action);
-            });
-        },
-    });
-});
+        });
+    }
+);
