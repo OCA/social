d4ac5e5cd94fd71c951557e2b03efdc5fabc719e
houssine <houssine.bakkali@gmail.com>
2021-11-08 12:48:44 +0100

    [PEP8] pre-commit

--- mail_tracking/static/src/js/failed_message/mail_failed_box.js
+++ mail_tracking/static/src/js/failed_message/mail_failed_box.js
@@ -1,40 +1,30 @@
-odoo.define('mail_tracking/static/src/js/failed_message/mail_failed_box.js', function(require) {
-    'use strict';
+odoo.define("mail_tracking/static/src/js/failed_message/mail_failed_box.js", function (
+    require
+) {
+    "use strict";
 
-    const components = {
-        Message: require('mail/static/src/components/message/message.js'),
-    };
+    const chatter = require("mail/static/src/components/chatter/chatter.js");
+    const useStore = require("mail/static/src/component_hooks/use_store/use_store.js");
 
-    const useShouldUpdateBasedOnProps = require('mail/static/src/component_hooks/use_should_update_based_on_props/use_should_update_based_on_props.js');
-    const useStore = require('mail/static/src/component_hooks/use_store/use_store.js');
-
-    const chatter = require('mail/static/src/components/chatter/chatter.js');
-
-    const {
-        Component,
-        useState
-    } = owl;
-
+    const {Component} = owl;
 
     class MessageFailedBox extends Component {
-
         constructor(...args) {
             super(...args);
-            useStore(props => {
-                const chatter = this.env.models['mail.chatter'].get(props.chatterLocalId);
+            useStore((props) => {
+                const chatter = this.env.models["mail.chatter"].get(
+                    props.chatterLocalId
+                );
                 const thread = chatter && chatter.thread;
                 return {
                     chatter: chatter ? chatter.__state : undefined,
                     thread: thread && thread.__state,
                 };
-
             });
-
-
         }
 
         get chatter() {
-            return this.env.models['mail.chatter'].get(this.props.chatterLocalId);
+            return this.env.models["mail.chatter"].get(this.props.chatterLocalId);
         }
 
         _onClickTitle() {
@@ -44,9 +34,7 @@ odoo.define('mail_tracking/static/src/js/failed_message/mail_failed_box.js', fun
             return this.env.services.rpc({
                 model: "mail.message",
                 method: "set_need_action_done",
-                args: [
-                    [id]
-                ],
+                args: [[id]],
             });
         }
         _onRetryFailedMessage(event) {
@@ -54,14 +42,14 @@ odoo.define('mail_tracking/static/src/js/failed_message/mail_failed_box.js', fun
             var messageID = $(event.currentTarget).data("message-id");
             const thread = this.chatter.thread;
             var self = this;
-            this.env.bus.trigger('do-action', {
-                action: 'mail.mail_resend_message_action',
+            this.env.bus.trigger("do-action", {
+                action: "mail.mail_resend_message_action",
                 options: {
                     additional_context: {
                         mail_message_to_resend: messageID,
                     },
                     on_close: () => {
-                        self.trigger('reload', { keepChanges: true });
+                        self.trigger("reload", {keepChanges: true});
                         thread.refresh();
                     },
                 },
@@ -70,26 +58,18 @@ odoo.define('mail_tracking/static/src/js/failed_message/mail_failed_box.js', fun
         _onMarkFailedMessageReviewed(event) {
             event.preventDefault();
             var messageID = $(event.currentTarget).data("message-id");
-            this._markFailedMessageReviewed(messageID)
-            this.trigger('reload', { keepChanges: true });
+            this._markFailedMessageReviewed(messageID);
+            this.trigger("reload", {keepChanges: true});
             this.chatter.thread.refreshMessagefailed();
             this.chatter.thread.refresh();
         }
-
-
-
-
     }
     MessageFailedBox.template = "mail_tracking.MessageFailedBox";
     MessageFailedBox.props = {
         chatterLocalId: String,
     };
     chatter.components = Object.assign({}, chatter.components, {
-        MessageFailedBox
+        MessageFailedBox,
     });
     return MessageFailedBox;
-
-
-
-
 });
