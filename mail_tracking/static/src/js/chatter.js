2c35e9beded04e8bcc658aa771839ed50187e601
houssine <houssine.bakkali@gmail.com>
2021-11-08 12:48:44 +0100

    [PEP8] pre-commit

--- mail_tracking/static/src/js/chatter.js
+++ mail_tracking/static/src/js/chatter.js
@@ -1,30 +1,38 @@
-odoo.define('mail_tracking/static/src/js/chatter.js', function (require) {
-'use strict';
+odoo.define("mail_tracking/static/src/js/chatter.js", function (require) {
+    "use strict";
 
-const ChatterContainer = require('mail/static/src/components/chatter_container/chatter_container.js');
-const { attr } = require('mail/static/src/model/model_field.js');
-const { registerInstancePatchModel,registerFieldPatchModel,    registerClassPatchModel,
- } = require('mail/static/src/model/model_core.js');
+    const {attr} = require("mail/static/src/model/model_field.js");
+    const {
+        registerInstancePatchModel,
+        registerFieldPatchModel,
+    } = require("mail/static/src/model/model_core.js");
 
-registerInstancePatchModel('mail.chatter', 'mail/static/src/models/chatter/chatter.js', {
-    async refresh() {
-        this._super(...arguments);
-        this.thread.refreshMessagefailed()
-        },
-    toggleMessageFailedBoxVisibility() {
-        this.update({ isMessageFailedBoxVisible: !this.isMessageFailedBoxVisible })
-    },
-    _onThreadIdOrThreadModelChanged() {
-        this._super(...arguments);
-        this.thread.refreshMessagefailed()
-    }
-
-});
-registerFieldPatchModel('mail.chatter', 'mail/static/src/models/chatter/chatter.js', {
-    isMessageFailedBoxVisible: attr({
-            default: true,
-    }),
-});
-
-
+    registerInstancePatchModel(
+        "mail.chatter",
+        "mail/static/src/models/chatter/chatter.js",
+        {
+            async refresh() {
+                this._super(...arguments);
+                this.thread.refreshMessagefailed();
+            },
+            toggleMessageFailedBoxVisibility() {
+                this.update({
+                    isMessageFailedBoxVisible: !this.isMessageFailedBoxVisible,
+                });
+            },
+            _onThreadIdOrThreadModelChanged() {
+                this._super(...arguments);
+                this.thread.refreshMessagefailed();
+            },
+        }
+    );
+    registerFieldPatchModel(
+        "mail.chatter",
+        "mail/static/src/models/chatter/chatter.js",
+        {
+            isMessageFailedBoxVisible: attr({
+                default: true,
+            }),
+        }
+    );
 });
--- mail_tracking/static/src/js/failed_message/thread.js
+++ mail_tracking/static/src/js/failed_message/thread.js
@@ -1,60 +1,50 @@
-odoo.define('mail_tracking/static/src/js/failed_message/thread.js', function(require) {
-    'use strict';
-
-    const components = {
-        Message: require('mail/static/src/components/message/message.js'),
-    };
-
-    const useShouldUpdateBasedOnProps = require('mail/static/src/component_hooks/use_should_update_based_on_props/use_should_update_based_on_props.js');
-    const useStore = require('mail/static/src/component_hooks/use_store/use_store.js');
-
-    const chatter = require('mail/static/src/components/chatter/chatter.js');
-
-    const {
-        Component,
-        useState
-    } = owl;
+odoo.define("mail_tracking/static/src/js/failed_message/thread.js", function (require) {
+    "use strict";
 
     const {
         registerInstancePatchModel,
         registerFieldPatchModel,
-    } = require('mail/static/src/model/model_core.js');
-    const {
-        one2many
-    } = require('mail/static/src/model/model_field.js');
+    } = require("mail/static/src/model/model_core.js");
+    const {one2many} = require("mail/static/src/model/model_field.js");
 
-
-
-
-    registerInstancePatchModel('mail.thread', 'mail_tracking/static/src/js/failed_message/thread.js', {
-
-        async refreshMessagefailed() {
-            var id = this.__values.id
-            var model = this.__values.model
-            const messagefailedData = await this.async(() => this.env.services.rpc({
-                model: 'mail.message',
-                method: 'get_failed_messsage_info',
-                args: [id, model],
-            }, {
-                shadow: true
-            }));
-            const messagefailed = this.env.models['mail.message.failed'].insert(messagefailedData.map(
-                messageData => this.env.models['mail.message.failed'].convertData(messageData)
-            ));
-            this.update({
-                messagefailed: [
-                    ['replace', messagefailed]
-                ]
-            });
+    registerInstancePatchModel(
+        "mail.thread",
+        "mail_tracking/static/src/js/failed_message/thread.js",
+        {
+            async refreshMessagefailed() {
+                var id = this.__values.id;
+                var model = this.__values.model;
+                const messagefailedData = await this.async(() =>
+                    this.env.services.rpc(
+                        {
+                            model: "mail.message",
+                            method: "get_failed_messsage_info",
+                            args: [id, model],
+                        },
+                        {
+                            shadow: true,
+                        }
+                    )
+                );
+                const messagefailed = this.env.models["mail.message.failed"].insert(
+                    messagefailedData.map((messageData) =>
+                        this.env.models["mail.message.failed"].convertData(messageData)
+                    )
+                );
+                this.update({
+                    messagefailed: [["replace", messagefailed]],
+                });
+            },
         }
+    );
 
-    });
-
-    registerFieldPatchModel('mail.thread', 'mail_tracking/static/src/js/failed_message/thread.js', {
-        messagefailed: one2many('mail.message.failed', {
-            inverse: 'thread',
-        }),
-
-    });
-
+    registerFieldPatchModel(
+        "mail.thread",
+        "mail_tracking/static/src/js/failed_message/thread.js",
+        {
+            messagefailed: one2many("mail.message.failed", {
+                inverse: "thread",
+            }),
+        }
+    );
 });
