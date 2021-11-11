fae2f77eab8c8216c17e113922193e0b949ffcb2
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-11 11:53:59 +0100

    [IMP] mail_attach_existing_attachment_account: black, isort, prettier

--- mail_attach_existing_attachment_account/__manifest__.py
+++ mail_attach_existing_attachment_account/__manifest__.py
@@ -2,21 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': "Mail Attach Existing Attachment (Account)",
-    'summary': "Module to use attach existing attachment for account module",
-    'author': "Thore Baden, "
-              "Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/social",
-    'category': 'Social Network',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
-        'mail_attach_existing_attachment',
-    ],
-    'data': [
-        'wizard/account_invoice_send_view.xml',
-    ],
-    'installable': True,
-    'auto_install': True,
+    "name": "Mail Attach Existing Attachment (Account)",
+    "summary": "Module to use attach existing attachment for account module",
+    "author": "Thore Baden, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "category": "Social Network",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account", "mail_attach_existing_attachment",],
+    "data": ["wizard/account_invoice_send_view.xml",],
+    "installable": True,
+    "auto_install": True,
 }
