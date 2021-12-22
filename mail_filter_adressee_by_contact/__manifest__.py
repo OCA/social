fed0cb41ab85950c8e90055edf926d6f0b4ad5ce
Kev-Roche <kevin.roche@akretion.com>
2021-12-22 11:30:53 +0100

    [ADD] mail_filter_adressee_by_contacts

---
+++ mail_filter_adressee_by_contact/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail filter adressee by partner contacts and users",
+    "summary": "Adresses filter by partner contacts and users",
+    "version": "14.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+}
