2176411260fcc8da51c2ef8b4fc7d46796859f9b
Olga Marco <olga.marco@creublanca.es>
2021-12-30 13:33:52 +0100

    [MIG] mail_restrict_follower_selection: Migration to 14.0

--- mail_restrict_follower_selection/__manifest__.py
+++ mail_restrict_follower_selection/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Restrict follower selection",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
     "author": "Therp BV,Creu Blanca,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Social Network",
