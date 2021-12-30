e17b6d224b166cd4f892c22a9894de5b45751a81
Olga Marco <olga.marco@creublanca.es>
2021-12-30 13:10:45 +0100

    [IMP] mail_drop_target: black, isort, prettier

--- mail_drop_target/static/src/js/mail_drop_target.js
+++ mail_drop_target/static/src/js/mail_drop_target.js
@@ -2,7 +2,7 @@
 // Copyright 2018 Therp BV <https://therp.nl>
 // License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-odoo.define("mail_drop_target", function(require) {
+odoo.define("mail_drop_target", function (require) {
     "use strict";
     var Chatter = require("mail.Chatter");
     var web_drop_target = require("web_drop_target");
@@ -11,17 +11,17 @@ odoo.define("mail_drop_target", function(require) {
 
     Chatter.include({
         _drop_allowed_types: ["message/rfc822"],
-        _get_record_id: function() {
+        _get_record_id: function () {
             return this.record.res_id;
         },
 
-        _handle_drop_items: function(drop_items) {
+        _handle_drop_items: function (drop_items) {
             var self = this;
-            _.each(drop_items, function(item, e) {
+            _.each(drop_items, function (item, e) {
                 return self._handle_file_drop_proxy(item, e);
             });
         },
-        _handle_file_drop_proxy: function(item, e) {
+        _handle_file_drop_proxy: function (item, e) {
             var self = this;
             var file = item;
             if (!file || !(file instanceof Blob)) {
@@ -34,7 +34,7 @@ odoo.define("mail_drop_target", function(require) {
             reader.onerror = self.proxy("_file_reader_error_handler");
             reader.readAsArrayBuffer(file);
         },
-        _handle_file_drop: function(drop_file, reader) {
+        _handle_file_drop: function (drop_file, reader) {
             var self = this,
                 mail_processor = "",
                 data = "";
@@ -58,7 +58,7 @@ odoo.define("mail_drop_target", function(require) {
                 kwargs: {
                     thread_id: this.record.data.id,
                 },
-            }).then(function() {
+            }).then(function () {
                 self.trigger_up("reload", {});
             });
         },
