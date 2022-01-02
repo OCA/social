94ec9308401faf3beb7b233f5d126d9600c68112
Sébastien BEAU <sebastien.beau@akretion.com>
2022-01-02 22:54:32 +0100

    [FIX] clean code

--- mail_unique_layout/wizards/mail_compose_message.py
+++ mail_unique_layout/wizards/mail_compose_message.py
@@ -9,13 +9,5 @@ class MailComposeMessage(models.TransientModel):
     _inherit = "mail.compose.message"
 
     def send_mail(self, auto_commit=False):
-        if self.env.context.get("unique_layout_ctx_added"):
-            return super().send_mail(auto_commit)
-        return (
-            super()
-            .with_context(
-                unique_layout_ctx_added=True,
-                custom_layout="mail_unique_layout.test_layout",
-            )
-            .send_mail(auto_commit)
-        )
+        self = self.with_context(custom_layout="mail_unique_layout.test_layout")
+        return super().send_mail(auto_commit=auto_commit)
