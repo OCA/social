cb6478d30f9025969bf5aef8b1693409669922f1
Christopher Ormaza <chris.ormaza@forgeflow.com>
2022-01-11 09:26:06 -0500

    [14.0][ADD] mail_server_by_user

---
+++ mail_server_by_user/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 ForgeFlow S.L. (https://forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Email Server By User",
+    "summary": "Email Server By User",
+    "version": "14.0.1.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social",
+    "author": ("ForgeFlow, " "Odoo Community Association (OCA)"),
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["base", "mail"],
+    "development_status": "Alpha",
+    "maintainers": ["ChrisOForgeFlow"],
+    "data": [],
+    "qweb": [],
+    "demo": [],
+}
