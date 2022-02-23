831bb19e6685022c3e1b2e6ab315f8c5fd6362e0
simonev <simone.vanin@agilebg.com>
2022-02-23 17:49:55 +0100

    [IMP] mail_notify_bounce: black, isort, prettier

--- mail_notify_bounce/models/__init__.py
+++ mail_notify_bounce/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
--- mail_notify_bounce/models/fetchmail.py
+++ mail_notify_bounce/models/fetchmail.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class FetchmailServer(models.Model):
-    _inherit = 'fetchmail.server'
+    _inherit = "fetchmail.server"
     bounce_notify_partner_ids = fields.Many2many(
-        'res.partner', string='Notify bounce emails to')
+        "res.partner", string="Notify bounce emails to"
+    )
