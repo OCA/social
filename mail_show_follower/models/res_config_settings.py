d5191581debec0740855c33f751eafa98d29c123
Eduardo De Miguel <edu@moduon.team>
2022-03-11 12:11:21 +0100

    [IMP] mail_show_follower: black, isort, prettier

--- mail_show_follower/models/res_config_settings.py
+++ mail_show_follower/models/res_config_settings.py
@@ -2,10 +2,10 @@ from odoo import fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     show_internal_users_cc = fields.Boolean(
-        string='Show Internal Users CC',
-        related='company_id.show_internal_users_cc',
-        readonly=False
+        string="Show Internal Users CC",
+        related="company_id.show_internal_users_cc",
+        readonly=False,
     )
