3f839a6523f830a9696ef71db2fc8d5a09df9f3c
Akim Juillerat <akim.juillerat@camptocamp.com>
2022-03-22 14:59:14 +0100

    Create module mass_mailing_contact_active

---
+++ mass_mailing_contact_active/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Mass Mailing Contact Active",
+    "summary": "Adds active feature on mailing list contact and subscriptions",
+    "version": "13.0.1.0.0",
+    "category": "Marketing/Email Marketing",
+    "website": "https://github.com/OCA/social",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["mass_mailing"],
+}
