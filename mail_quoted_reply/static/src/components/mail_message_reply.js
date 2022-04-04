2ad0022fdc4b6f5fc64f78f4f4f77621b45ee05c
jb <J.Buchholz@cibex.net>
2022-04-04 12:54:11 +0200

    [MIG] mail_quoted_reply: Migration to 14.0

---
+++ mail_quoted_reply/static/src/components/mail_message_reply.js
@@ -0,0 +1,21 @@
+odoo.define("mail_quoted_reply/static/src/components/mail_message_reply.js", function(
+    require
+) {
+    "use strict";
+
+    const components = {
+        Message: require("mail/static/src/components/message/message.js"),
+    };
+    const {patch} = require("web.utils");
+
+    patch(
+        components.Message,
+        "mail_quoted_reply/static/src/models/mail_message_reply.js",
+        {
+            _onClickMailMessageReply(ev) {
+                this.message._onClickMailMessageReply();
+            },
+
+        }
+    );
+});
