6d8f95e8e7f7452437a3db0e0df88b5a876bd255
J.Buchholz@cibex.net <J.Buchholz@cibex.net>
2022-04-04 12:54:11 +0200

    [MIG] mail_quoted_reply: Migration to 14.0

--- mail_quoted_reply/__manifest__.py
+++ mail_quoted_reply/__manifest__.py
@@ -5,11 +5,14 @@
     "name": "Mail Message Reply",
     "summary": """
         Make a reply using a message""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "depends": ["mail"],
     "qweb": ["static/src/xml/mail_message_reply.xml"],
-    "data": ["templates/assets.xml", "data/reply_template.xml"],
+    "data": [
+        "templates/assets.xml",
+        "data/reply_template.xml",
+    ],
 }
