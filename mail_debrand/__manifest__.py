37f03941bd734fe573a1c045f9792f487df4cca2
Miquel Raïch <miquel.raich@forgeflow.com>
2022-04-06 12:09:56 +0200

    [UPD] Eficent -> ForgeFlow

--- mail_debrand/__manifest__.py
+++ mail_debrand/__manifest__.py
@@ -1,7 +1,6 @@
 # Copyright 2016 Tecnativa - Jairo Llopis
 # Copyright 2017 Tecnativa - Pedro M. Baeza
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
-#   - Lois Rilo <lois.rilo@eficent.com>
+# Copyright 2019 ForgeFlow S.L. - Lois Rilo <lois.rilo@forgeflow.com>
 # 2020 NextERP Romania
 # Copyright 2021 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -13,10 +12,10 @@
     ( for powerd by) form all the templates
     removes any 'odoo' that are in tempalte texts > 20characters
     """,
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
-    "author": """Tecnativa, Eficent, Onestein, Sodexis, Nexterp Romania,
+    "author": """Tecnativa, ForgeFlow, Onestein, Sodexis, Nexterp Romania,
              Odoo Community Association (OCA)""",
     "license": "AGPL-3",
     "installable": True,
