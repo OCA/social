6d4721afe8e351f245e95bfd80228c6946d2c2f2
Akim Juillerat <akim.juillerat@camptocamp.com>
2022-05-10 15:27:24 +0200

    mail_tracking_mass_mailing: Set traces in error according to tracking
    If an exception is caught by ir.mail_server.send_email in mail_tracking module,
    the mail.tracking.email record will appear in error but the related mailing
    trace would still appear as sent because mail.mail._postprocess_sent_message is
    called without any failure_type in mail.mail._send in the mail module (as Exception
    is not raised after being caught in mail_tracking module).
    
    Since _postprocess_sent_message method not only sets the mailing.trace state in
    mass_mailing module but can also delete the mail.mail records in mail module,
    we need to ensure the mailing.trace is written accordingly to the tracking here,
    and avoid having the mass_mailing module set a 'sent' status if we had an exception,
    hence the usage of a context key to ignore possible writes.

--- mail_tracking_mass_mailing/__manifest__.py
+++ mail_tracking_mass_mailing/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Mail tracking for mass mailing",
     "summary": "Improve mass mailing email tracking",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, Odoo Community Association (OCA)",
