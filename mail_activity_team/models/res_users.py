93adf4379fd27e38f9abd10c78f852179c2d7808
Enric Tobella <etobella@creublanca.es>
2022-05-26 21:14:04 +0200

    [IMP] mail_activity_team: Improve test using standard methods
    test

--- mail_activity_team/models/res_users.py
+++ mail_activity_team/models/res_users.py
@@ -1,4 +1,4 @@
-# Copyright 2018 ForgeFlow S.L.
+# Copyright 2018 ForgeFlow, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models, modules
 
@@ -17,35 +17,34 @@ class ResUsers(models.Model):
         if not self._context.get("team_activities", False):
             return super().systray_get_activities()
         query = """SELECT m.id, count(*), act.res_model as model,
-                    CASE
-                        WHEN %(today)s::date -
-                        act.date_deadline::date = 0 Then 'today'
-                        WHEN %(today)s::date -
-                        act.date_deadline::date > 0 Then 'overdue'
-                        WHEN %(today)s::date -
-                        act.date_deadline::date < 0 Then 'planned'
-                    END AS states, act.user_id as user_id
-                    FROM mail_activity AS act
-                    JOIN ir_model AS m ON act.res_model_id = m.id
-                    WHERE team_id in (
-                        SELECT mail_activity_team_id
-                        FROM mail_activity_team_users_rel
-                        WHERE res_users_id = %(user_id)s
-                    )
-                    GROUP BY m.id, states, act.res_model, act.user_id;"""
+                                CASE
+                                    WHEN %(today)s::date -
+                                    act.date_deadline::date = 0 Then 'today'
+                                    WHEN %(today)s::date -
+                                    act.date_deadline::date > 0 Then 'overdue'
+                                    WHEN %(today)s::date -
+                                    act.date_deadline::date < 0 Then 'planned'
+                                END AS states, act.user_id as user_id
+                            FROM mail_activity AS act
+                            JOIN ir_model AS m ON act.res_model_id = m.id
+                            WHERE team_id in (
+                                SELECT mail_activity_team_id
+                                FROM mail_activity_team_users_rel
+                                WHERE res_users_id =
+                            %(user_id)s
+                            )
+                            GROUP BY m.id, states, act.res_model, act.user_id;
+                            """
         user = self.env.uid
         self.env.cr.execute(
-            query,
-            {
-                "today": fields.Date.context_today(self),
-                "user_id": user,
-            },
+            query, {"today": fields.Date.context_today(self), "user_id": user}
         )
         activity_data = self.env.cr.dictfetchall()
         model_ids = [a["id"] for a in activity_data]
         model_names = {
             n[0]: n[1] for n in self.env["ir.model"].browse(model_ids).name_get()
         }
+
         user_activities = {}
         for activity in activity_data:
             if not user_activities.get(activity["model"]):
@@ -64,11 +63,9 @@ class ResUsers(models.Model):
             user_activities[activity["model"]][
                 "%s_count" % activity["states"]
             ] += activity["count"]
-            if activity["states"] in ("today", "overdue"):
-                user_activities[activity["model"]]["total_count"] += activity["count"]
-            if activity["user_id"] == user and activity["states"] in (
-                "today",
-                "overdue",
+            if (
+                activity["states"] in ("today", "overdue")
+                and activity["user_id"] != user
             ):
-                user_activities[activity["model"]]["total_count"] -= activity["count"]
+                user_activities[activity["model"]]["total_count"] += activity["count"]
         return list(user_activities.values())
