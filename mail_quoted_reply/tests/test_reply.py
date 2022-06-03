1919902772367d01b4fca35a633fd02f042ecf3a
Lois Rilo <lois.rilo@forgeflow.com>
2022-06-03 20:10:54 +0200

    [IMP] black, isort, prettier

--- mail_quoted_reply/tests/test_reply.py
+++ mail_quoted_reply/tests/test_reply.py
@@ -5,34 +5,28 @@ from odoo.tests import TransactionCase
 
 class TestMessageReply(TransactionCase):
     def test_reply(self):
-        partner = self.env["res.partner"].create({
-            "name": "demo partner"
-        })
+        partner = self.env["res.partner"].create({"name": "demo partner"})
         self.assertFalse(
-            partner.message_ids.filtered(
-                lambda r: r.message_type != 'notification'
-            )
+            partner.message_ids.filtered(lambda r: r.message_type != "notification")
         )
-        message = partner.message_post(
-            body="demo message",
-            message_type="email"
-        )
+        message = partner.message_post(body="demo message", message_type="email")
         partner.refresh()
         self.assertIn(
             message,
-            partner.message_ids.filtered(
-                lambda r: r.message_type != 'notification')
+            partner.message_ids.filtered(lambda r: r.message_type != "notification"),
         )
-        self.assertFalse(partner.message_ids.filtered(
-            lambda r: r.message_type != 'notification' and r != message
-        ))
+        self.assertFalse(
+            partner.message_ids.filtered(
+                lambda r: r.message_type != "notification" and r != message
+            )
+        )
         action = message.reply_message()
-        wizard = self.env[action["res_model"]].with_context(
-            action["context"]
-        ).create({})
+        wizard = (
+            self.env[action["res_model"]].with_context(action["context"]).create({})
+        )
         wizard.action_send_mail()
         new_message = partner.message_ids.filtered(
-            lambda r: r.message_type != 'notification' and r != message
+            lambda r: r.message_type != "notification" and r != message
         )
         self.assertTrue(new_message)
         self.assertEqual(1, len(new_message))
