4e6c7d8c1bbe5151569d9ef16a2b75fd21eb9563
Olga Marco <olga.marco@creublanca.es>
2022-06-09 11:19:19 +0200

    [IMP] mail_broker

---
+++ mail_gateway/controllers/mail_broker.py
@@ -0,0 +1,10 @@
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo.addons.base_rest.controllers import main
+
+
+class MailBrokerController(main.RestController):
+    _root_path = "/broker/"
+    _collection_name = "mail.broker"
+    _default_auth = "none"
