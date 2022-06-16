3146b8d9e8be32f757506bced0ca4f8ade9adf5a
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2022-06-16 07:13:32 +0200

    Add module mail_notification_with_history
    Followers of a discussion in the chatter of Odoo can be informed by email
    of a new message being created.
    This module adds in that email the history of the chatter discussion to help
    the recipient with more context.

---
+++ mail_notification_with_history/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Mail Notification With History",
+    "summary": """Add the previous chatter discussion into new email notifications.""",
+    "version": "14.0.1.0.0",
+    "category": "Social",
+    "website": "https://github.com/OCA/social",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["mail"],
+    "data": ["data/mail_data.xml"],
+    "development_status": "Beta",
+    "maintainers": ["TDu"],
+}
