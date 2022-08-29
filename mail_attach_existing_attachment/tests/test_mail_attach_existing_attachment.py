c4bd2b43a70c6c6b9054510494ae5c60a19d4c7c
Benoit Aimont <benoit.aimont@acsone.eu>
2022-08-29 14:15:22 +0200

    [MIG] mail_attach_existing_attachment: Migration to 16.0

--- mail_attach_existing_attachment/tests/test_mail_attach_existing_attachment.py
+++ mail_attach_existing_attachment/tests/test_mail_attach_existing_attachment.py
@@ -6,7 +6,7 @@ from odoo.tests import common
 
 class TestAttachExistingAttachment(common.TransactionCase):
     def setUp(self):
-        super(TestAttachExistingAttachment, self).setUp()
+        super().setUp()
         self.partner_obj = self.env["res.partner"]
         self.partner_01 = self.env["res.partner"].create(
             {
