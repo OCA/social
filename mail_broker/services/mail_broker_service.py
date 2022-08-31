3eef3c6f1967e0e8e760c8c314d16b9642ef5bd1
Enric Tobella <etobella@creublanca.es>
2022-08-31 09:14:29 +0200

    [FIX] mail_broker: Fix Swagger API Definition

--- mail_broker/services/mail_broker_service.py
+++ mail_broker/services/mail_broker_service.py
@@ -11,20 +11,27 @@ class BrokerMethodParams(restapi.RestMethodParam):
     def to_response(self, service, result):
         return result
 
+    def to_openapi_requestbody(self, service):
+        return {"content": {}}
+
+    def to_openapi_responses(self, service):
+        return {"200": {"content": {}}}
+
 
 class MailBrokerService(Component):
     _inherit = "base.rest.service"
     _name = "mail.broker.base.service"
     _usage = "broker"
     _collection = "mail.broker.services"
+    _description = "Mail Broker services"
 
     @restapi.method(
         [(["/<string:bot_key>/update"], "GET")],
         input_param=BrokerMethodParams(),
-        output_param=BrokerMethodParams(),
         auth="none",
     )
     def get_update(self, token, **kwargs):
+        """Verification of the service from an external service"""
         bot_id = self.env["mail.broker"]._get_broker_id(token, **kwargs)
         if not bot_id:
             return None
@@ -32,11 +39,12 @@ class MailBrokerService(Component):
 
     @restapi.method(
         [(["/<string:bot_key>/update"], "POST")],
-        output_param=BrokerMethodParams(),
+        # output_param=BrokerMethodParams(),
         input_param=BrokerMethodParams(),
         auth="none",
     )
     def post_update(self, token, **kwargs):
+        """Post an update from an external service using GET"""
         bot_id = self.env["mail.broker"]._get_broker_id(token, **kwargs)
         if not bot_id:
             return {}
@@ -44,4 +52,4 @@ class MailBrokerService(Component):
         broker.with_user(broker.webhook_user_id.id).with_context(
             notify_broker=True
         )._receive_update(kwargs)
-        return {}
+        return False
