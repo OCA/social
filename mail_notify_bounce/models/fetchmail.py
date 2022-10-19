63bf24aa422ead9cd6491e74a2cd79328d8bb04d
tafaRU <alex.comba@agilebg.com>
2022-10-19 10:30:42 +0200

    [FIX] mail_notify_bounce: get module installable alongside https://github.com/OCA/l10n-italy/tree/14.0/l10n_it_fatturapa_pec Otherwise installing both we get the following error:
    TypeError: Many2many fields fetchmail.server.e_inv_notify_partner_ids and fetchmail.server.bounce_notify_partner_ids use the same table and columns

--- mail_notify_bounce/models/fetchmail.py
+++ mail_notify_bounce/models/fetchmail.py
@@ -1,5 +1,6 @@
 # Copyright 2017 Lorenzo Battistini - Agile Business Group
 # Copyright 2022 Simone Vanin - Agile Business Group
+# Copyright 2022 Alex Comba - Agile Business Group
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models
@@ -7,6 +8,11 @@ from odoo import fields, models
 
 class FetchmailServer(models.Model):
     _inherit = "fetchmail.server"
+
     bounce_notify_partner_ids = fields.Many2many(
-        "res.partner", string="Notify bounce emails to"
+        comodel_name="res.partner",
+        relation="mnb_fetchmail_server_res_partner_rel",
+        column1="fetchmail_server_id",
+        column2="res_partner_id",
+        string="Notify bounce emails to",
     )
--- mail_notify_bounce/readme/CONTRIBUTORS.rst
+++ mail_notify_bounce/readme/CONTRIBUTORS.rst
@@ -1,2 +1,3 @@
 * Lorenzo Battistini <lorenzo.battistini@agilebg.com>
 * Simone Vanin <simone.vanin@agilebg.com>
+* Alex Comba <alex.comba@agilebg.com>
