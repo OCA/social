4127a2cfdaf6f9802b675b2dddb008a62abf5a10
Enric Tobella <etobella@creublanca.es>
2022-10-21 20:41:03 +0200

    [IMP] mail_broker: Add place for checking origin

--- mail_broker/static/src/js/message.js
+++ mail_broker/static/src/js/message.js
@@ -8,6 +8,9 @@ odoo.define("mail_broker.model.Message", function(require) {
             this._super.apply(this, arguments);
             this.broker_channel_id = data.broker_channel_id || false;
             this.broker_unread = data.broker_unread || false;
+            this.broker_type = data.broker_type || false;
+            this.customer_status = data.customer_status || false;
+            console.log(this);
         },
         isNeedaction: function() {
             return this._super.apply(this, arguments) || this.broker_unread;
