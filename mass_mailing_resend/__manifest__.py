31050c41df71e8fe702e68781b2bfb7f483ee380
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-03 15:12:03 +0100

    [MIG] mass_mailing_resend: Migration to 15.0
    TT40296

--- mass_mailing_resend/__manifest__.py
+++ mass_mailing_resend/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Resend mass mailings",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Marketing",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, Odoo Community Association (OCA)",
