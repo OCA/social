980e7fbf85a70599852689bb2009aa6990f89ae3
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-11-08 12:59:34 +0100

    fixup! [12.0][MIG] mail_edit

--- mail_edit/__manifest__.py
+++ mail_edit/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": "Edit, Delete or Move messages to any model",
     "version": "12.0.1.0.0",
     "category": "Social Network",
-    "website": "https://sunflowerweb.nl",
+    "website": "https://github.com/OCA/social",
     "author": "Sunflower IT, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
