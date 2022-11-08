f0621f544714bfeaa16d065a7495c04146f8b961
Tom Blauwendraat <tom@sunflowerweb.nl>
2022-11-08 09:21:49 +0100

    [UPD] dotfiles

--- setup/mail_drop_target/setup.py
+++ setup/mail_drop_target/setup.py
@@ -2,7 +2,13 @@ import setuptools
 
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
-    odoo_addon=True,
+    odoo_addon={
+        'external_dependencies_override': {
+            'python': {
+                'extract_msg': 'extract_msg<0.30',
+            },
+        },
+    },
     install_requires=[
         'extract_msg<0.30'
     ],
