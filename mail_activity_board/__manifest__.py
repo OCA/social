da60ac125658091f06e84bd5b581255dee87feee
augusto-weiss <awe@adhoc.com.ar>
2022-11-11 15:22:39 -0300

    [MIG] mail_activity_board: Migration to 16.0

--- mail_activity_board/__manifest__.py
+++ mail_activity_board/__manifest__.py
@@ -4,20 +4,18 @@
 {
     "name": "Mail Activity Board",
     "summary": "Add Activity Boards",
-    "version": "15.0.1.1.1",
+    "version": "16.0.1.0.0",
     "development_status": "Beta",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "SDi, David Juaneda, Sodexis, ACSONE SA/NV, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["calendar", "board"],
+    "depends": ["calendar", "spreadsheet_dashboard"],
     "data": ["views/mail_activity_view.xml"],
     "assets": {
         "web.assets_backend": [
             "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.esm.js",
-        ],
-        "web.assets_qweb": [
             "mail_activity_board/static/src/components/chatter_topbar/chatter_topbar.xml",
         ],
     },
