c348a6343a0b4788f4f9c630327513117b62d7ed
Kevin Luna <kevin.luna@creublanca.es>
2022-11-18 13:37:02 +0100

    [IMP] mail_activity_type: Remove deprecated usage of onchange
    Replaced for web_domain_field module

--- mail_activity_team/models/mail_activity.py
+++ mail_activity_team/models/mail_activity.py
@@ -1,5 +1,7 @@
 # Copyright 2018-22 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import json
+
 from odoo import SUPERUSER_ID, _, api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -20,42 +22,40 @@ class MailActivity(models.Model):
         return self.env["mail.activity.team"].search(domain, limit=1)
 
     user_id = fields.Many2one(required=False)
+    team_user_id = fields.Many2one(related="user_id", readonly=False)
 
     team_id = fields.Many2one(
         comodel_name="mail.activity.team", default=lambda s: s._get_default_team_id()
     )
+    user_id_domain = fields.Char(compute="_compute_user_id_domain")
+
+    @api.depends("team_id")
+    def _compute_user_id_domain(self):
+        for record in self:
+            domain = []
+            if record.team_id:
+                domain.append(("id", "in", record.team_id.member_ids.ids))
+            record.user_id_domain = json.dumps(domain)
 
     @api.onchange("user_id")
     def _onchange_user_id(self):
-        res = {"domain": {"team_id": []}}
-        if not self.user_id:
-            return res
-        res["domain"]["team_id"] = [
-            "|",
-            ("res_model_ids", "=", False),
-            ("res_model_ids", "in", self.res_model_id.ids),
-        ]
-        if self.team_id and self.user_id in self.team_id.member_ids:
-            return res
+        if not self.user_id or (
+            self.team_id and self.user_id in self.team_id.member_ids
+        ):
+            return
         self.team_id = self.with_context(
             default_res_model=self.sudo().res_model_id.model
         )._get_default_team_id(user_id=self.user_id.id)
-        return res
 
     @api.onchange("team_id")
     def _onchange_team_id(self):
-        res = {"domain": {"user_id": []}}
-        if not self.team_id:
-            return res
-        res["domain"]["user_id"] = [("id", "in", self.team_id.member_ids.ids)]
-        if self.user_id not in self.team_id.member_ids:
+        if self.team_id and self.user_id not in self.team_id.member_ids:
             if self.team_id.user_id:
                 self.user_id = self.team_id.user_id
             elif len(self.team_id.member_ids) == 1:
                 self.user_id = self.team_id.member_ids
             else:
                 self.user_id = self.env["res.users"]
-        return res
 
     @api.constrains("team_id", "user_id")
     def _check_team_and_user(self):
