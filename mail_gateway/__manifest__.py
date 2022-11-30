a430baa0f218a31ea545469356a1e4e0993e4d38
Enric Tobella <etobella@creublanca.es>
2022-11-30 11:35:53 +0100

    [MIG] mail_broker: Migration to 14.0

--- mail_gateway/__manifest__.py
+++ mail_gateway/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Mail Broker",
     "summary": """
         Set a broker""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
