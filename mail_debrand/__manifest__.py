c656b28ec662239e807b85b82db9f19d90715777
Logeshkumar <logeshkumar@sodexis.com>
2022-12-05 10:53:08 +0530

    [16.0][MIG] mail_debrand

--- mail_debrand/__manifest__.py
+++ mail_debrand/__manifest__.py
@@ -12,7 +12,7 @@
     ( for powerd by) form all the templates
     removes any 'odoo' that are in tempalte texts > 20characters
     """,
-    "version": "15.0.1.2.2",
+    "version": "16.0.1.0.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": """Tecnativa, ForgeFlow, Onestein, Sodexis, Nexterp Romania,
