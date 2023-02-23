5ddc2bbfaad1687b15b448af1a3e8337c21ea089
Enric Tobella <etobella@creublanca.es>
2023-02-23 14:38:10 +0100

    [ADD] mail_activity_unlink_log

---
+++ mail_activity_unlink_log/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 CreuBlanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Mail Activities: log on unlink",
+    "summary": """
+        Leave a message when an activity is unlinked""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "CreuBlanca,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "depends": ["mail"],
+    "data": ["data/unlink_message.xml"],
+}
