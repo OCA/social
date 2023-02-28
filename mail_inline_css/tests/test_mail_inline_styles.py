1e11add2fffbedf691e9825523bc0db630483d8c
Ruchir Shukla <ruchir@bizzappdev.com>
2023-02-28 13:04:38 +0530

    [MIG][15.0]mail_inline_css:Migration to 15.0

--- mail_inline_css/tests/test_mail_inline_styles.py
+++ mail_inline_css/tests/test_mail_inline_styles.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from lxml import html
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class TestMailInlineStyles(SavepointCase):
+class TestMailInlineStyles(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
