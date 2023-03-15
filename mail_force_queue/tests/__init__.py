80cc5b0c8ad2a8356ba62e3aa33f79dc625ecbf6
Giovanni Francesco Capalbo <giovanni@therp.nl>
2023-03-15 13:52:16 +0100

    [ADD] backport mail_force_queue from version 10

---
+++ mail_force_queue/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Lorenzo Battistini - Agile Business Group
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+
+from . import test_mail_notification
