10b60f22f8591bc689e009ac6c716ac373c1209d
Karthik, Sodexis <karthik@sodexis.com>
2023-03-31 12:43:41 +0530

    [IMP] mail_template_substitute: black, isort, prettier

--- mail_template_substitute/__manifest__.py
+++ mail_template_substitute/__manifest__.py
@@ -8,8 +8,7 @@
         """,
     "version": "12.0.1.0.1",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV,"
-              "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "depends": ["base", "mail", "report_substitute"],
     "data": [
--- mail_template_substitute/tests/test_mail_template_substitute.py
+++ mail_template_substitute/tests/test_mail_template_substitute.py
@@ -7,53 +7,51 @@ from odoo.tests.common import TransactionCase
 class TestMailTemplateSubstitute(TransactionCase):
     def setUp(self):
         super(TestMailTemplateSubstitute, self).setUp()
-        self.smt2 = self.env['mail.template'].create(
+        self.smt2 = self.env["mail.template"].create(
             {
-                'name': 'substitute_template_2',
-                'model_id': self.env.ref('base.model_res_partner').id,
+                "name": "substitute_template_2",
+                "model_id": self.env.ref("base.model_res_partner").id,
             }
         )
-        self.smt1 = self.env['mail.template'].create(
+        self.smt1 = self.env["mail.template"].create(
             {
-                'name': 'substitute_template_1',
-                'model_id': self.env.ref('base.model_res_partner').id,
-                'mail_template_substitution_rule_ids': [
+                "name": "substitute_template_1",
+                "model_id": self.env.ref("base.model_res_partner").id,
+                "mail_template_substitution_rule_ids": [
                     (
                         0,
                         0,
                         {
-                            'substitution_mail_template_id': self.smt2.id,
-                            'domain': "[('id', '=', False)]",
+                            "substitution_mail_template_id": self.smt2.id,
+                            "domain": "[('id', '=', False)]",
                         },
                     )
                 ],
             }
         )
-        self.mt = self.env['mail.template'].create(
+        self.mt = self.env["mail.template"].create(
             {
-                'name': 'base_template',
-                'model_id': self.env.ref('base.model_res_partner').id,
-                'mail_template_substitution_rule_ids': [
-                    (0, 0, {'substitution_mail_template_id': self.smt1.id})
+                "name": "base_template",
+                "model_id": self.env.ref("base.model_res_partner").id,
+                "mail_template_substitution_rule_ids": [
+                    (0, 0, {"substitution_mail_template_id": self.smt1.id})
                 ],
             }
         )
-        self.mail_compose = self.env['mail.compose.message'].create(
-            {'template_id': self.mt.id, 'composition_mode': 'mass_mail'}
+        self.mail_compose = self.env["mail.compose.message"].create(
+            {"template_id": self.mt.id, "composition_mode": "mass_mail"}
         )
-        self.partners = self.env['res.partner'].search([])
+        self.partners = self.env["res.partner"].search([])
 
     def test_get_email_template(self):
         self.assertEqual(
             self.mt._get_substitution_template(
-                self.env.ref('base.model_res_partner'), self.partners.ids
+                self.env.ref("base.model_res_partner"), self.partners.ids
             ),
             self.smt1,
         )
         self.assertEqual(
-            self.mt.get_email_template(self.partners.ids).get(
-                self.partners.ids[0]
-            ),
+            self.mt.get_email_template(self.partners.ids).get(self.partners.ids[0]),
             self.smt1,
         )
 
@@ -61,13 +59,13 @@ class TestMailTemplateSubstitute(TransactionCase):
         self.assertEqual(
             self.mail_compose.with_context(
                 active_ids=self.partners.ids
-            )._get_substitution_template('mass_mail', self.mt, None),
+            )._get_substitution_template("mass_mail", self.mt, None),
             self.smt1,
         )
 
     def test_onchange_template_id_wrapper(self):
         self.assertEqual(self.mail_compose.template_id, self.mt)
-        self.smt1.mail_template_substitution_rule_ids.domain = '[]'
+        self.smt1.mail_template_substitution_rule_ids.domain = "[]"
         self.mail_compose.with_context(
             active_ids=self.partners.ids
         ).onchange_template_id_wrapper()
