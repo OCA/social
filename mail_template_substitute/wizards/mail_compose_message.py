39bcabc807225ec8f10067b8e948c9e55bd4ff1e
Karthik, Sodexis <karthik@sodexis.com>
2023-03-31 12:43:41 +0530

    [IMP] mail_template_substitute: black, isort, prettier

--- mail_template_substitute/wizards/mail_compose_message.py
+++ mail_template_substitute/wizards/mail_compose_message.py
@@ -6,15 +6,13 @@ from odoo import api, models
 
 class MailComposeMessage(models.TransientModel):
 
-    _inherit = 'mail.compose.message'
+    _inherit = "mail.compose.message"
 
     @api.model
     def _get_substitution_template(self, composition_mode, template, res_ids):
         if template:
-            if composition_mode == 'mass_mail' and self.env.context.get(
-                'active_ids'
-            ):
-                res_ids = self.env.context.get('active_ids')
+            if composition_mode == "mass_mail" and self.env.context.get("active_ids"):
+                res_ids = self.env.context.get("active_ids")
             res_ids_to_templates = template.get_email_template(res_ids)
             if res_ids_to_templates:
                 return list(res_ids_to_templates.values())[0]
@@ -24,16 +22,16 @@ class MailComposeMessage(models.TransientModel):
     def default_get(self, fields):
         result = super(MailComposeMessage, self).default_get(fields)
         substitution_template = self._get_substitution_template(
-            result.get('composition_mode'),
-            self.env['mail.template'].browse(result.get('template_id')),
-            [result.get('res_id')],
+            result.get("composition_mode"),
+            self.env["mail.template"].browse(result.get("template_id")),
+            [result.get("res_id")],
         )
         if substitution_template:
-            result['template_id'] = substitution_template.id
+            result["template_id"] = substitution_template.id
         return result
 
     @api.multi
-    @api.onchange('template_id')
+    @api.onchange("template_id")
     def onchange_template_id_wrapper(self):
         substitution_template = self._get_substitution_template(
             self.composition_mode, self.template_id, [self.res_id]
