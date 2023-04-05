132512555aef8d9dc1e2fb1fb9454e33428066a3
Ilyas <irazor147@gmail.com>
2023-04-05 10:59:04 +0200

    [IMP] mail_activity_reply_creator: added copyright

--- mail_activity_reply_creator/__manifest__.py
+++ mail_activity_reply_creator/__manifest__.py
@@ -1,3 +1,5 @@
+# Copyright 2023 Ooops404
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Activity Reply Creator",
     "summary": "Assign new to its creator",
--- mail_activity_reply_creator/models/mail_activity.py
+++ mail_activity_reply_creator/models/mail_activity.py
@@ -1,4 +1,4 @@
-# Copyright 2018 ForgeFlow S.L.
+# Copyright 2023 Ooops404
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, models
 
