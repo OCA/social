dbfd0aabfdeafaf9602c046ee54b94b1ff9ba033
Laurence Lars Labusch <lala@labiso.de>
2023-06-28 16:40:03 +0200

    [MIG] mail_quoted_reply
    Completed the migration to 16.0 incl. the upgrade to the OWL framework to make it more lean and reliable for the future.

---
+++ mail_quoted_reply/static/src/models/message_action_view.esm.js
@@ -0,0 +1,35 @@
+/** @odoo-module **/
+
+import { registerPatch } from "@mail/model/model_core";
+
+registerPatch({
+    name: "MessageActionView",
+    recordMethods: {
+        onClick(ev) {
+            if (this.messageAction.messageActionListOwner === this.messageAction.replyMessageAction) {
+                this.messageAction.messageActionListOwner.message.messageReply();
+            } else {
+                this._super(ev);
+            }
+        },
+    },
+    fields: {
+        classNames: {
+            compute() {
+                let classNames = this._super() || "";
+                if (this.messageAction.messageActionListOwner === this.messageAction.replyMessageAction) {
+                    classNames += " fa fa-lg fa-reply";
+                }
+                return classNames;
+            },
+        },
+        title: {
+            compute() {
+                if (this.messageAction.messageActionListOwner === this.messageAction.replyMessageAction) {
+                    return this.env._t("Reply");
+                }
+                return this._super();
+            },
+        },
+    },
+});
