a501617ff2ce53ef8f5d6bfdec76cfcd5bfb9eb6
Holger Brunn <mail@hunki-enterprises.com>
2023-09-04 16:25:12 +0200

    [FIX] mail_activity_done: patch _action_done in a more reliable way

--- mail_activity_done/models/mail_activity.py
+++ mail_activity_done/models/mail_activity.py
@@ -2,6 +2,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 from odoo import api, fields, models
 
+delete_sentinel = object()
+
 
 class MailActivity(models.Model):
 
@@ -60,6 +62,19 @@ class MailActivity(models.Model):
                         ),
                         ("done", "=", True),
                     ]
+
+    def unlink(self):
+        """Don't unlink if we're asked not to"""
+        if self.env.context.get("mail_activity_done") != delete_sentinel:
+            return super().unlink()
+
+    def _action_done(self, feedback=False, attachment_ids=None):
+        """Ask super not to delete the activity and set it to done"""
+        self.write({"done": True, "active": False, "date_done": fields.Date.today()})
+        return super(
+            MailActivity,
+            self.with_context(mail_activity_done=delete_sentinel),
+        )._action_done(feedback=feedback, attachment_ids=attachment_ids)
 
 
 class MailActivityMixin(models.AbstractModel):
