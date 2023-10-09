0119e7c9a9c79b3eba5c3010b3831f709706036d
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-10-09 08:10:29 -0300

    [MIG] mass_mailing_event_registration_exclude: Migration to 16.0

--- mass_mailing_event_registration_exclude/models/event_registration.py
+++ mass_mailing_event_registration_exclude/models/event_registration.py
@@ -10,8 +10,8 @@ class EventRegistration(models.Model):
     _inherit = "event.registration"
 
     @api.model
-    def search_count(self, domain):
-        res = super().search_count(domain)
+    def search_count(self, domain, limit=None):
+        res = super().search_count(domain, limit)
         mass_mailing_id = self.env.context.get("exclude_mass_mailing", False)
         if mass_mailing_id:
             res_ids = event_filtered_ids(self, mass_mailing_id, domain, field="email")
