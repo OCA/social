e71d9d1ae65f2f9bcd7b9003f47d90b755596a50
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-11-17 10:51:52 -0300

    [MIG] mass_mailing_event_registration_exclude: Continue Migration to 16.0

--- mass_mailing_event_registration_exclude/models/mailing.py
+++ mass_mailing_event_registration_exclude/models/mailing.py
@@ -6,33 +6,32 @@ import copy
 from odoo import fields, models
 
 
-def event_filtered_ids(model, mailing_mailing_id, domain, field="email"):
-    field = field or "email"
-    domain = domain or []
-    exclude_emails = []
-    mailing_mailing = model.env["mailing.mailing"].browse(mailing_mailing_id)
-    if mailing_mailing.event_id:
-        exclude = mailing_mailing.exclude_event_state_ids.mapped("code")
-        reg_domain = False
-        registrations = model.env["event.registration"]
-        if exclude:
-            reg_domain = [
-                ("event_id", "=", mailing_mailing.event_id.id),
-                ("state", "in", exclude),
-            ]
-        if reg_domain:
-            registrations = registrations.search(reg_domain)
-        if registrations:
-            exclude_emails = registrations.mapped("email")
-    apply_domain = copy.deepcopy(domain)
-    if exclude_emails:
-        apply_domain.append((field, "not in", exclude_emails))
-    rows = model.search(apply_domain)
-    return rows.ids
-
-
 class MassMailing(models.Model):
     _inherit = "mailing.mailing"
+
+    def event_filtered_ids(self, model, mailing_mailing_id, domain, field="email"):
+        field = field or "email"
+        domain = domain or []
+        exclude_emails = []
+        mailing_mailing = model.env["mailing.mailing"].browse(mailing_mailing_id)
+        if mailing_mailing.event_id:
+            exclude = mailing_mailing.exclude_event_state_ids.mapped("code")
+            reg_domain = False
+            registrations = model.env["event.registration"]
+            if exclude:
+                reg_domain = [
+                    ("event_id", "=", mailing_mailing.event_id.id),
+                    ("state", "in", exclude),
+                ]
+            if reg_domain:
+                registrations = registrations.search(reg_domain)
+            if registrations:
+                exclude_emails = registrations.mapped("email")
+        apply_domain = copy.deepcopy(domain)
+        if exclude_emails:
+            apply_domain.append((field, "not in", exclude_emails))
+        rows = model.search(apply_domain)
+        return rows.ids
 
     def _default_exclude_event_state_ids(self):
         return self.env["event.registration.state"].search([])
@@ -48,7 +47,7 @@ class MassMailing(models.Model):
         res_ids = super()._get_recipients()
         if res_ids:
             domain = [("id", "in", res_ids)]
-            res_ids = event_filtered_ids(
+            res_ids = self.event_filtered_ids(
                 self.env[self.mailing_model_real], self.id, domain, field="email"
             )
         return res_ids
--- mass_mailing_event_registration_exclude/models/mailing_contact.py
+++ mass_mailing_event_registration_exclude/models/mailing_contact.py
@@ -4,17 +4,17 @@
 
 from odoo import api, models
 
-from .mailing import event_filtered_ids
-
 
 class MassMailingContact(models.Model):
     _inherit = "mailing.contact"
 
     @api.model
     def search_count(self, domain, limit=None):
-        res = super().search_count(domain)
+        res = super().search_count(domain, limit=limit)
         mass_mailing_id = self.env.context.get("exclude_mass_mailing", False)
         if mass_mailing_id:
-            res_ids = event_filtered_ids(self, mass_mailing_id, domain, field="email")
+            res_ids = self.env["mailing.mailing"].event_filtered_ids(
+                self, mass_mailing_id, domain, field="email"
+            )
             res = len(res_ids) if res_ids else 0
         return res
