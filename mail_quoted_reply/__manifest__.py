80c3790ac2af6ff3da9b2f6680fde4bc9018485d
Marc Durepos <marc@bemade.org>
2023-12-11 08:11:00 -0500

    [REL] mail_quoted_reply: Migrate JavaScript to 16.0
    Rewrote JavaScript to match major changes in 15.0->16.0 transition.
    Prior code relied on methods that no longer exist. New code patches the
    multiple models in @mail/models to mimic the code present for the
    "reply to" MessageAction.
    
    This version also fixes a bug in which the quoted reply button appeared
    on notes as well as emails and sometimes duplicated itself repeatedly
    after mouseover events.

--- mail_quoted_reply/__manifest__.py
+++ mail_quoted_reply/__manifest__.py
@@ -5,19 +5,15 @@
     "name": "Mail Message Reply",
     "summary": """
         Make a reply using a message""",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
     "depends": ["mail"],
     "data": [],
     "assets": {
-        "web.assets_qweb": [
-            "/mail_quoted_reply/static/src/xml/mail_message_reply.xml",
-        ],
         "web.assets_backend": [
-            "/mail_quoted_reply/static/src/models/mail_message_reply.esm.js",
-            "/mail_quoted_reply/static/src/components/mail_message_reply.esm.js",
+            "/mail_quoted_reply/static/src/models/mail_message_reply.js",
         ],
     },
 }
