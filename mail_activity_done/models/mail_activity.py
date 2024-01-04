3d742cde2bfd0f768625198ee8b3f2979e2eb8bd
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2024-01-04 16:04:49 -0300

    [16.0][TMP] mail_activity_done

--- mail_activity_done/models/mail_activity.py
+++ mail_activity_done/models/mail_activity.py
@@ -1,5 +1,6 @@
 # Copyright 2018-22 ForgeFlow <http://www.forgeflow.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
 from odoo import api, fields, models
 
 delete_sentinel = object()
