853f335e85449710e4921320d7ed0f60820197f5
andrea <ast@360erp.com>
2024-02-08 08:52:09 +0100

    [17.0][MIG] mail_send_confirmation: migration to 17.0

--- mail_send_confirmation/__manifest__.py
+++ mail_send_confirmation/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Mail Send Confirmation",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Mail",
     "website": "https://github.com/OCA/social",
     "depends": ["mail"],
     "assets": {
-        "mail.assets_messaging": [
+        "web.assets_backend": [
             "mail_send_confirmation/static/src/models/composer_view.esm.js",
         ],
     },
--- mail_send_confirmation/readme/CONTRIBUTORS.md
+++ mail_send_confirmation/readme/CONTRIBUTORS.md
@@ -1,2 +1,4 @@
 - [Quartile](https://www.quartile.co):
   - Aung Ko Ko Lin
+- [360ERP](https://www.360erp.com):
+  - Andrea Stirpe
