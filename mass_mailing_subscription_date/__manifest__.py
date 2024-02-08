cb9a3a33cf4888caeea08f7370525312559c6b6b
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2024-02-08 17:51:52 +0000

    [MIG] mass_mailing_subscription_date: Migration to 16.0

--- mass_mailing_subscription_date/__manifest__.py
+++ mass_mailing_subscription_date/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Mass Mailing Subscription Date",
     "summary": "Track contact's subscription date to mailing lists",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/social",
