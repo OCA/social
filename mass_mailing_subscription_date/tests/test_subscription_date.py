52975279cc1eb3b827aed83d9580d217e5a3389e
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2024-02-08 17:32:49 +0000

    [MIG] mass_mailing_subscription_date: Migration to 15.0

--- mass_mailing_subscription_date/tests/test_subscription_date.py
+++ mass_mailing_subscription_date/tests/test_subscription_date.py
@@ -2,10 +2,10 @@
 # @author Iván Todorovich <ivan.todorovich@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestSubscriptionDate(SavepointCase):
+class TestSubscriptionDate(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
