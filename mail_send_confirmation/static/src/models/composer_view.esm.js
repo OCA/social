f20582c8bc51a7174564ea0e43ecf78fb379f7c9
andrea <ast@360erp.com>
2024-02-08 08:52:09 +0100

    [17.0][MIG] mail_send_confirmation: migration to 17.0

--- mail_send_confirmation/static/src/models/composer_view.esm.js
+++ mail_send_confirmation/static/src/models/composer_view.esm.js
@@ -1,45 +1,34 @@
-/** @odoo-module **/
+/* @odoo-module */
 
-import {registerPatch} from "@mail/model/model_core";
-import "@mail/models/composer_view";
-import Dialog from "web.Dialog";
-import core from "web.core";
-const _t = core._t;
+import {Composer} from "@mail/core/common/composer";
+import {ConfirmationDialog} from "@web/core/confirmation_dialog/confirmation_dialog";
+import {_t} from "@web/core/l10n/translation";
+import {patch} from "@web/core/utils/patch";
+import {useService} from "@web/core/utils/hooks";
 
-registerPatch({
-    name: "ComposerView",
-    recordMethods: {
-        /**
-         * @override
-         */
-        onClickSend() {
-            const composerModel = this;
-            if (this.composer.isLog) {
-                this._super();
-            } else {
-                this.dialog = new Dialog(this, {
-                    title: _t("Confirmation"),
-                    $content: $("<div/>", {
-                        text: _.str.sprintf(
-                            _t(
-                                "This message will be sent to external partners as well. Are you sure you would like to send this message?"
-                            )
-                        ),
-                    }),
-                    buttons: [
-                        {
-                            text: _t("Confirm"),
-                            classes: "btn-primary",
-                            close: true,
-                            click: function () {
-                                composerModel._super();
-                            },
-                        },
-                        {text: _t("Discard"), close: true},
-                    ],
-                });
-                this.dialog.open();
-            }
-        },
+patch(Composer.prototype, {
+    /**
+     * @override
+     */
+    setup() {
+        super.setup();
+        this.dialogService = useService("dialog");
+    },
+    async sendMessage() {
+        if (this.props.type === "message") {
+            this.dialogService.add(ConfirmationDialog, {
+                body: _t(
+                    "This message will be sent to external partners as well. Are you sure you would like to send this message?"
+                ),
+                confirm: async () => {
+                    super.sendMessage();
+                },
+                cancel: () => {
+                    /* We need this empty function to display the cancel button */
+                },
+            });
+        } else {
+            super.sendMessage();
+        }
     },
 });
