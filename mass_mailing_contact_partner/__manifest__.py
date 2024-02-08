09bc7597b6626d38223af778b07577e9db68267e
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2024-02-08 16:57:23 +0000

    [MIG] mass_mailing_contact_partner: Migration to 15.0

--- mass_mailing_contact_partner/__manifest__.py
+++ mass_mailing_contact_partner/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Mass Mailing Contact Partner",
     "summary": "Links mailing.contacts with res.partners.",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "maintainers": ["ivantodorovich"],
     "website": "https://github.com/OCA/social",
