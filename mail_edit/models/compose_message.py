943660ec932145e4cc0518511dc2f1094c00efd4
Tom Blauwendraat <tom@sunflowerweb.nl>
2024-02-17 05:13:53 +0100

    [FIX] pre-commit

--- mail_edit/models/compose_message.py
+++ mail_edit/models/compose_message.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # © 2016 Sunflower IT (http://sunflowerweb.nl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -12,8 +11,8 @@ class MailMessage(models.Model):
     def _get_model_selection(self):
         """Get allowed models and their names."""
         model_objs = self.env["res.request.link"].search(
-            [("mail_edit", "=", True)],
-            order="name")
+            [("mail_edit", "=", True)], order="name"
+        )
         return [(m.object, m.name) for m in model_objs]
 
     @api.one
@@ -25,8 +24,7 @@ class MailMessage(models.Model):
             self.model = self.destination_object_id._name
             self.res_id = self.destination_object_id.id
 
-            model_name = self.env["ir.model"].search([
-                ("model", "=", self.model)]).name
+            model_name = self.env["ir.model"].search([("model", "=", self.model)]).name
             display_name = self.destination_object_id.display_name
             if model_name:
                 display_name = "%s %s" % (model_name, display_name)
@@ -38,14 +36,16 @@ class MailMessage(models.Model):
     destination_object_id = fields.Reference(
         _get_model_selection,
         "Destination object",
-        help="Object where the message will be moved to")
+        help="Object where the message will be moved to",
+    )
 
     @api.model
     def _message_read_dict_postprocess(self, messages, message_tree):
         res = super(MailMessage, self)._message_read_dict_postprocess(
-            messages, message_tree)
+            messages, message_tree
+        )
         for message_dict in messages:
             # Check if current user is a superuser
-            if self.env.user.has_group('mail_edit.group_mail_edit_superuser'):
-                message_dict['is_superuser'] = True
+            if self.env.user.has_group("mail_edit.group_mail_edit_superuser"):
+                message_dict["is_superuser"] = True
         return res
