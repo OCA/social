ed5b0bd8fcc527c70767a131daaf578401b7a66a
chien <chien@trobz.com>
2024-02-19 10:26:40 +0700

    [MIG] mail_drop_target: Migration to 16.0

--- mail_drop_target/__init__.py
+++ mail_drop_target/__init__.py
@@ -1,3 +1,4 @@
 # Copyright 2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import controllers
 from . import models
--- mail_drop_target/__manifest__.py
+++ mail_drop_target/__manifest__.py
@@ -2,23 +2,18 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Drag & drop emails to Odoo",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Discuss",
     "website": "https://github.com/OCA/social",
     "summary": "Attach emails to Odoo by dragging them from your desktop",
     "depends": ["mail"],
-    "external_dependencies": {"python": ["extract_msg"]},
+    "external_dependencies": {"python": ["extract_msg", "cryptography<37"]},
     "data": ["views/res_config_settings_views.xml"],
     "assets": {
         "web.assets_backend": [
-            "mail_drop_target/static/lib/base64js.min.js",
-            "mail_drop_target/static/src/js/mail_drop_target.esm.js",
-            "mail_drop_target/static/src/css/mail_drop_target.css",
-        ],
-        "web.assets_qweb": [
-            "mail_drop_target/static/src/xml/*.xml",
+            "mail_drop_target/static/src/js/file_uploader.esm.js",
         ],
     },
 }
