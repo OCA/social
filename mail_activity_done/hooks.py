484f24d56e4e79f31a957d36a6aff2fdb27ffb4e
Carlos Lopez <carlos.lopez@dynapps.eu>
2024-03-06 15:20:20 +0100

    [MIG] mail_activity_done: Migration to 17.0

--- mail_activity_done/hooks.py
+++ mail_activity_done/hooks.py
@@ -3,24 +3,24 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     """The objective of this hook is to default to false all values of field
     'done' of mail.activity
     """
-    cr.execute(
+    env.cr.execute(
         """SELECT column_name
     FROM information_schema.columns
     WHERE table_name='mail_activity' AND
     column_name='done'"""
     )
-    if not cr.fetchone():
-        cr.execute(
+    if not env.cr.fetchone():
+        env.cr.execute(
             """
             ALTER TABLE mail_activity ADD COLUMN done boolean;
             """
         )
 
-    cr.execute(
+    env.cr.execute(
         """
         UPDATE mail_activity
         SET done = False
@@ -28,11 +28,11 @@ def pre_init_hook(cr):
     )
 
 
-def uninstall_hook(cr, registry):
+def uninstall_hook(env, registry):
     """The objective of this hook is to remove all activities that are done
     upon module uninstall
     """
-    cr.execute(
+    env.env.cr.execute(
         """
         DELETE FROM mail_activity
         WHERE done=True
