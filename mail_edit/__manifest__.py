6423a8545618b82094ea3e9b2754d92f3ad8c0ba
ntsirintanis <ntsirintanis@therp.nl>
2024-03-08 09:55:23 +0100

    [MIG] mail_edit: Migration to 16.0

--- mail_edit/__manifest__.py
+++ mail_edit/__manifest__.py
@@ -1,10 +1,10 @@
-# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# © 2016-24 Sunflower IT (http://sunflowerweb.nl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Message Edit",
     "summary": "Edit, Delete or Move messages to any model",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "Sunflower IT, Odoo Community Association (OCA)",
@@ -17,11 +17,16 @@
     ],
     "data": [
         "security/mail_edit_security.xml",
-        "views/assets.xml",
         "views/compose_message.xml",
     ],
     "demo": ["demo/data.xml"],
-    "qweb": [
-        "static/src/components/message/mail_edit.xml",
-    ],
+    "assets": {
+        "web.assets_backend": [
+            "mail_edit/static/src/components/message/mail_edit.js",
+            "mail_edit/static/src/js/tour.js",
+        ],
+        "web.assets_qweb": [
+            "mail_edit/static/src/components/message/mail_edit.xml",
+        ],
+    },
 }
