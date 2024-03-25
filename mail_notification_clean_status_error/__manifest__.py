7aebb578aa2ee8dee1559a7530b61ea7eed638c5
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-03-25 16:35:57 +0100

    mail_notification_clean_status_error: remove notifications in error

---
+++ mail_notification_clean_status_error/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Mail Notification - Clean up notifications in error",
+    "summary": "Extend Odoo scheduled action to also delete notifications in error.",
+    "version": "14.0.1.0.0",
+    "category": "Mail",
+    "website": "https://github.com/OCA/social",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["mail"],
+    "development_status": "Production/Stable",
+    "maintainers": ["sebalix"],
+}
