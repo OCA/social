c3621d225a2214622022cc791c82091ebe90a980
Ronald Portier <ronald@therp.nl>
2024-05-11 12:09:48 +0200

    [RNM] Rename module as now proposed for social repo

--- mail_hide_chatter_send_message/__manifest__.py
+++ mail_hide_chatter_send_message/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2024 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Web Hide Chatter Send Message",
+    "name": "Mail Hide Chatter Send Message",
     "version": "16.0.1.0.0",
     "author": "Therp BV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
@@ -11,7 +11,7 @@
     "depends": ["mail", "web"],
     "assets": {
         "web.assets_backend": [
-            "web_hide_chatter_send_message/static/src/"
+            "mail_hide_chatter_send_message/static/src/"
             "components/chatter_topbar/chatter_topbar.xml",
         ],
     },
