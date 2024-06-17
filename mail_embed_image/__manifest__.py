ff2514d93b438c7319d4a8cca7af966c0679c5f6
ilo <dev@nomadiplus.com>
2024-06-17 10:43:22 -0300

    [16.0][MIG] migrating to 16.0

--- mail_embed_image/__manifest__.py
+++ mail_embed_image/__manifest__.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail Embed Image",
-    "version": "10.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Social",
     "summary": "Replace img.src's which start with http with inline cids",
-    'website': 'https://github.com/OCA/social',
+    "website": "https://github.com/OCA/social",
     "depends": [
-        'web',
+        "web",
     ],
     "installable": True,
     "application": False,
