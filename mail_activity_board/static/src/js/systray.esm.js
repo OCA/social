7a61268aa34000d9b566a28827a66c32e15d0a6f
Hans Henrik Gabelgaard <hhg@gabelgaard.org>
2024-07-04 17:49:47 +0200

    [15.0][IMP]mail_activity_board: Adapt Open All to v15

--- mail_activity_board/static/src/js/systray.esm.js
+++ mail_activity_board/static/src/js/systray.esm.js
@@ -1,24 +1,19 @@
-odoo.define('mail_activity_board.systray.ActivityMenu', function (require) {
-    "use strict";
+/** @odoo-module **/
+import ActivityMenu from "@mail/js/systray/systray_activity_menu";
 
-    var ActivityMenu = require('mail.systray.ActivityMenu');
-    var session = require("web.session");
+ActivityMenu.include({
+    events: _.extend({}, ActivityMenu.prototype.events, {
+        "click .o_all_activities_button": "_onClickOpenAllActivities",
+    }),
 
-    ActivityMenu.include({
-        events: _.extend({}, ActivityMenu.prototype.events, {
-            'click .o_all_activities_button': '_onClickOpenAllActivities',
-        }),
+    _open_boards_activities_domain: function () {
+        return {additional_context: {search_default_activities_my: 1}};
+    },
 
-        _open_boards_activities_domain: function () {
-            return {additional_context: {'search_default_activities_my':  1}};
-        },
-
-        _onClickOpenAllActivities: function () {
-            this.do_action(
-                "mail_activity_board.open_boards_activities",
-                this._open_boards_activities_domain()
-            )
-        },
-    });
-
+    _onClickOpenAllActivities: function () {
+        this.do_action(
+            "mail_activity_board.open_boards_activities",
+            this._open_boards_activities_domain()
+        );
+    },
 });
