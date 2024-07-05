f501d3709f046ffd8d8c715fc2a153ecf510740e
Ariel Barreiros <arielbarreiros96@gmail.com>
2024-07-05 10:10:28 +0100

    [14.0][ADD] mail_activity_multiple_users module

---
+++ mail_activity_multiple_users/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Binhex (<https://binhex.cloud>)
+# Copyright 2024 Binhex Ariel Barreiros
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Mail Activity Multiple Users",
+    "summary": """This module allows to create several
+    activities for several users at once, if needded""",
+    "version": "14.0.1.0.0",
+    "category": "Social Network",
+    "author": "Binhex, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": ["views/mail_activity_view.xml"],
+    "installable": True,
+}
