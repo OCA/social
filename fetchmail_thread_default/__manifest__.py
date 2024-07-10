dbcf2cf03ba3ec1a8c1ee690cba2af1b7648d80b
Christopher Ormaza <chris.ormaza@forgeflow.com>
2024-07-10 08:58:00 -0500

    [16.0][FIX] fetchmail_thread_default, add sudo call to models without explicit access

--- fetchmail_thread_default/__manifest__.py
+++ fetchmail_thread_default/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Default Thread For Unbounded Emails",
     "summary": "Post unkonwn messages to an existing thread",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "category": "Discuss",
     "website": "https://github.com/OCA/social",
     "author": "Tecnativa, Therp BV,  Odoo Community Association (OCA)",
