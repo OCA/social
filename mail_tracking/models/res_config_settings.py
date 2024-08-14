48da964e29f5f55cd4adab3b7ac4b343bc4233b5
Henry Backman <henry.backman@camptocamp.com>
2024-08-14 12:53:52 +0200

    mail_tracking: add garbage collection
    Add autovacuum to mail_tracking_email that removes old records based on new configuration variable mail_tracking_email_max_age_days.
    Due to possibly a large number of records to be deleted on first run, set a default limit of 5000 per run.

--- mail_tracking/models/res_config_settings.py
+++ mail_tracking/models/res_config_settings.py
@@ -8,3 +8,9 @@ class ResConfigSettings(models.TransientModel):
         related="company_id.mail_tracking_show_aliases",
         readonly=False,
     )
+    mail_tracking_email_max_age_days = fields.Integer(
+        "Max age in days of mail tracking email records",
+        config_parameter="mail_tracking.mail_tracking_email_max_age_days",
+        help="If set as positive integer enables the deletion of "
+        "old mail tracking records to reduce the database size.",
+    )
