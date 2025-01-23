7d57840af43c8290dd83e6f88cf5752ef7fb728c
Aungkokolin1997 <lin@quartile.co>
2025-01-23 10:09:53 +0000

    [ADD] mail_reply_stage

---
+++ mail_reply_stage/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Reply Stage",
+    "category": "Mail",
+    "version": "15.0.1.0.0",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mail_reply_config_views.xml",
+    ],
+    "installable": True,
+}
