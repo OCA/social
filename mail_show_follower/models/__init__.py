8fa2a2aaff6d7c89dbb2d7cc14edb3e2ec3abc15
Thomas Pot <thomas@open2bizz.nl>
2025-01-27 18:38:49 +0100

    [NEW] WIP migrate mail_show_follower

---
+++ mail_show_follower/models/__init__.py
@@ -0,0 +1,4 @@
+from . import mail_mail
+from . import res_company
+from . import res_config_settings
+from . import res_users
---
+++ mail_show_follower/readme/CONTRIBUTORS.rst
@@ -0,0 +1,3 @@
+* Valentin Vinagre <valentin.vinagre@sygel.es>
+* Lorenzo Battistini
+* Eduardo de Miguel <edu@moduon.team>
