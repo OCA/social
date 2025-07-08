9eebcf72a9a8d4de2fa691fef6ebea855764d29d
Ronald Portier <ronald@therp.nl>
2025-07-08 17:07:29 +0200

    [IMP] ..company_aware..r: test server functions

--- mail_company_aware_sender/tests/test_company_aware_sender.py
+++ mail_company_aware_sender/tests/test_company_aware_sender.py
@@ -1,65 +1,9 @@
 # Copyright 2025 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import Command
-from odoo.tests import TransactionCase
+from .common import CompanyAwareSenderCase
 
 
-class TestCompanyAwareSender(TransactionCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        # Create two companies and a partner/user.
-        cls.Company = cls.env["res.company"]
-        cls.Partner = cls.env["res.partner"]
-        cls.User = cls.env["res.users"]
-        cls.IrMailServer = cls.env["ir.mail_server"]
-        cls.mail_server = cls.IrMailServer.create(
-            {
-                "name": "localhost",
-                "smtp_host": "localhost",
-                "domain_whitelist": "therp.nl",
-            }
-        )
-        cls.company_kingdom = cls.Company.create(
-            {
-                "name": "The kingdom of France",
-                "email": "info@kingdom.fr",
-            }
-        )
-        cls.company_imperium = cls.Company.create(
-            {
-                "name": "Imperium Romanum",
-                "email": "chancellery@imperiumromanum.org",
-                "use_email_domain": True,
-                "format_email": False,
-            }
-        )
-        cls.partner_charles = cls.Partner.create(
-            {
-                "name": "Charles Le Magne",
-                "email": "charlemagne@therp.nl",
-            }
-        )
-        cls.user_charles = cls.User.with_context(no_reset_password=True).create(
-            {
-                "partner_id": cls.partner_charles.id,
-                "login": "charlemagne",
-                "email": "charlemagne@therp.nl",
-                "company_id": cls.company_kingdom.id,
-                "company_ids": [
-                    Command.set([cls.company_kingdom.id, cls.company_imperium.id]),
-                ],
-            }
-        )
-        cls.partner_himiltrude = cls.Partner.create(
-            {
-                "name": "Himiltrude",
-                "email": "himiltrude@therp.nl",
-                "user_id": cls.user_charles.id,
-                "company_id": cls.company_imperium.id,
-            }
-        )
-
+class TestCompanyAwareSender(CompanyAwareSenderCase):
     def test_nothing_changed(self):
         # Check with default user and author (current user).
         mail_thread = (
@@ -117,7 +61,3 @@ class TestCompanyAwareSender(TransactionCase):
         # Make sure user and company from object used.
         email_from = self.env.user.sudo().get_company_aware_email(himiltrude)
         self.assertEqual(email_from, "charlemagne@imperiumromanum.org")
-
-    def test_disable_encapsulation(self):
-        email_from = self.mail_server._get_default_from_address()
-        self.assertEqual(email_from, None)
---
+++ mail_company_aware_sender/tests/test_mail_server.py
@@ -0,0 +1,45 @@
+# Copyright 2025 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo.exceptions import ValidationError
+
+from .common import CompanyAwareSenderCase
+
+
+class TestMailServer(CompanyAwareSenderCase):
+    def test_test_email_adresses(self):
+        # Whitelist domain, and enable from address.
+        self.mail_server.write(
+            {
+                "from_filter": "info@therp.nl",
+                "domain_whitelist": "therp.nl,kingdom.fr,imperiumromanum.org",
+            }
+        )
+        email_from, email_to = self.mail_server._get_test_email_addresses()
+        self.assertEqual(email_from, "info@therp.nl")
+        self.assertEqual(email_to, "noreply@odoo.com")
+        # Disable from_filter and remove therp.nl from whitelist.
+        self.mail_server.write(
+            {
+                "from_filter": "info@therp.nl",
+                "domain_whitelist": "kingdom.fr,imperiumromanum.org",
+            }
+        )
+        self.mail_server.write({"from_filter": False})
+        # Partner charles in imperium should use company aware from.
+        email_from, email_to = (
+            self.mail_server.with_user(self.user_charles)
+            .with_company(self.company_imperium)
+            ._get_test_email_addresses()
+        )
+        self.assertEqual(email_from, "charlemagne@imperiumromanum.org")
+        self.assertEqual(email_to, "noreply@odoo.com")
+        # There should be an exception when using an invalid email domain.
+        self.company_imperium.write({"email": "court@aachen.de"})
+        with self.assertRaises(ValidationError):
+            self.mail_server.with_user(self.user_charles).with_company(
+                self.company_imperium
+            )._get_test_email_addresses()
+
+    def test_disable_encapsulation(self):
+        email_from = self.mail_server._get_default_from_address()
+        self.assertEqual(email_from, None)
