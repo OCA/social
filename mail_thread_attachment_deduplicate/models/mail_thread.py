ca08d7f3298a4bdae93c107ac3ba39b86ea58eb0
len <len@lambdao.dev>
2025-08-30 23:52:59 +0200

    [IMP] mail_thread_attachment_deduplicate: update body references to use deduplicated ids

--- mail_thread_attachment_deduplicate/models/mail_thread.py
+++ mail_thread_attachment_deduplicate/models/mail_thread.py
@@ -1,43 +1,193 @@
 # Copyright 2025 Lambdao
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+import re
+
+import lxml.html
+from markupsafe import Markup
+
 from odoo import api, models
 
 
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
-    def _message_deduplicate_attachments(self, attachments):
-        if attachments:  # this is an enumerable of _Attachment named tuples
-            domain = [("res_id", "in", self.ids), ("res_model", "=", self._name)]
-            Attachments = self.env["ir.attachment"]
-            checksums = [
-                r["checksum"]
-                for r in Attachments.search_read(domain, fields=["checksum"])
-            ]
-            checksum = Attachments._compute_checksum
-            # we use a[1] instead of a.content because some modules might use
-            # simple tuples instead of named tuples...
-            attachments = [a for a in attachments if checksum(a[1]) not in checksums]
-        return attachments  # if it's None there's nothing to do
+    def _message_deduplicate_attachments(self, attachments, body=None):
+        """
+        Deduplicate attachments and update body CID references.
 
-    def _message_deduplicate_attachment_ids(self, attachment_ids):
-        # we let the mail_attachment_upload function create a dup with a res_id 0
-        # for the pending message. Here we simply reuse an existing attachment,
-        # so the dup will be GCed like any attachment for an unsent message
+        Instead of dropping duplicate attachments, this method:
+        1. Identifies duplicate attachments by checksum
+        2. For duplicates, finds the existing attachment
+        3. Updates CID references in the body to point to existing attachments
+        4. Returns deduplicated attachment list and updated body
+        """
+        if not attachments:
+            return attachments, body
+
+        domain = [("res_id", "in", self.ids), ("res_model", "=", self._name)]
+        Attachments = self.env["ir.attachment"]
+        existing_attachments = Attachments.search_read(
+            domain, fields=["checksum", "id", "access_token"]
+        )
+        checksum_to_attachment = {att["checksum"]: att for att in existing_attachments}
+
+        checksum = Attachments._compute_checksum
+        filtered_attachments = []
+        cid_mapping = {}
+
+        for attachment in attachments:
+            if len(attachment) >= 3:
+                _name, content, info = attachment[0], attachment[1], attachment[2] or {}
+            elif len(attachment) == 2:
+                _name, content, info = attachment[0], attachment[1], {}
+            else:
+                continue
+
+            attachment_checksum = checksum(content)
+
+            if attachment_checksum in checksum_to_attachment:
+                existing_att = checksum_to_attachment[attachment_checksum]
+                cid = info.get("cid")
+                if cid:
+                    access_token = existing_att.get("access_token")
+                    if not access_token:
+                        existing_record = Attachments.browse(existing_att["id"])
+                        access_token = existing_record._generate_access_token()
+                        existing_record.write({"access_token": access_token})
+
+                    cid_mapping[cid] = {"id": existing_att["id"], "token": access_token}
+            else:
+                filtered_attachments.append(attachment)
+
+        if cid_mapping and body:
+            body = self._update_body_cid_references(body, cid_mapping)
+        return filtered_attachments, body
+
+    def _update_body_cid_references(self, body, cid_mapping):
+        """Update CID references in body to point to existing attachments."""
+        if not body or not cid_mapping:
+            return body
+        try:
+            root = lxml.html.fromstring(body)
+            postprocessed = False
+            for node in root.iter("img"):
+                if node.get("src", "").startswith("cid:"):
+                    cid = node.get("src").split("cid:")[1]
+                    if cid in cid_mapping:
+                        att_info = cid_mapping[cid]
+                        node.set(
+                            "src",
+                            f"/web/image/{att_info['id']}?access_token={att_info['token']}",
+                        )
+                        postprocessed = True
+            if postprocessed:
+                return Markup(  # noqa: S704
+                    lxml.html.tostring(root, pretty_print=False, encoding="unicode")
+                )
+        except (ValueError, lxml.etree.XMLSyntaxError):  # pylint: disable=except-pass
+            pass  # If parsing fails, return original body
+        return body
+
+    def _message_deduplicate_attachment_ids(self, attachment_ids, body=None):
+        """
+        Deduplicate attachment IDs by replacing duplicates with existing ones.
+        Also updates body references to point to existing attachments.
+        """
+        if not attachment_ids:
+            return attachment_ids, body
+
+        domain = [("res_id", "in", self.ids), ("res_model", "=", self._name)]
+        Attachments = self.env["ir.attachment"]
+        existing_attachments = Attachments.search_read(
+            domain, fields=["checksum", "id", "access_token"]
+        )
+        id_by_checksum = {r["checksum"]: r["id"] for r in existing_attachments}
+        checksum_to_attachment = {att["checksum"]: att for att in existing_attachments}
+
         res = []
-        if attachment_ids:
-            domain = [("res_id", "in", self.ids), ("res_model", "=", self._name)]
-            Attachments = self.env["ir.attachment"]
-            id_checksum_list = Attachments.search_read(domain, fields=["checksum"])
-            id_by_checksum = {r["checksum"]: r["id"] for r in id_checksum_list}
-            for attachment_id in attachment_ids:
-                checksum = Attachments.browse(attachment_id).checksum
-                if checksum in id_by_checksum:
-                    res.append(id_by_checksum[checksum])
-                else:
-                    res.append(attachment_id)
-        return res
+        attachment_id_mapping = {}  # Maps old attachment ID to new attachment info
+
+        for attachment_id in attachment_ids:
+            attachment = Attachments.browse(attachment_id)
+            checksum = attachment.checksum
+            if checksum in id_by_checksum:
+                # Use existing attachment instead of duplicate
+                existing_att_id = id_by_checksum[checksum]
+                res.append(existing_att_id)
+
+                # Store mapping for body update
+                existing_att = checksum_to_attachment[checksum]
+                access_token = existing_att.get("access_token")
+                if not access_token:
+                    existing_record = Attachments.browse(existing_att_id)
+                    access_token = existing_record._generate_access_token()
+                    existing_record.write({"access_token": access_token})
+
+                attachment_id_mapping[attachment_id] = {
+                    "id": existing_att_id,
+                    "token": access_token,
+                }
+            else:
+                res.append(attachment_id)
+
+        # Update body to replace attachment references if any mappings exist
+        updated_body = body
+        if attachment_id_mapping and body:
+            updated_body = self._update_body_attachment_references(
+                body, attachment_id_mapping
+            )
+
+        return res, updated_body
+
+    def _update_body_attachment_references(self, body, attachment_id_mapping):  # noqa: C901
+        """Update attachment references in body to point to existing attachments."""
+        if not body or not attachment_id_mapping:
+            return body
+        try:
+            root = lxml.html.fromstring(body)
+            postprocessed = False
+
+            for node in root.iter("img"):
+                data_attachment_id = node.get("data-attachment-id")
+                if data_attachment_id:
+                    try:
+                        old_attachment_id = int(data_attachment_id)
+                        if old_attachment_id in attachment_id_mapping:
+                            att_info = attachment_id_mapping[old_attachment_id]
+                            node.set("data-attachment-id", str(att_info["id"]))
+                            postprocessed = True
+                    except (ValueError, TypeError):  # pylint: disable=except-pass
+                        pass
+
+                # Check for /web/image/ URLs in src attribute
+                src = node.get("src", "")
+                if "/web/image/" in src:
+                    for old_id, att_info in attachment_id_mapping.items():
+                        # Match patterns like
+                        # /web/image/1066-eb8c51b1/filename.jpg?access_token=...
+                        # or /web/image/1066?access_token=...
+                        if f"/web/image/{old_id}" in src:
+                            new_src = src.replace(
+                                f"/web/image/{old_id}", f"/web/image/{att_info['id']}"
+                            )
+                            if "access_token=" in new_src:
+                                new_src = re.sub(
+                                    r"access_token=[^&]*",
+                                    f"access_token={att_info['token']}",
+                                    new_src,
+                                )
+                            node.set("src", new_src)
+                            postprocessed = True
+                            break
+
+            if postprocessed:
+                return Markup(  # noqa: S704
+                    lxml.html.tostring(root, pretty_print=False, encoding="unicode")
+                )
+        except (ValueError, lxml.etree.XMLSyntaxError):  # pylint: disable=except-pass
+            pass  # If parsing fails, return original body
+        return body
 
     @api.returns("mail.message", lambda value: value.id)
     def message_post(
@@ -54,12 +204,17 @@ class MailThread(models.AbstractModel):
         partner_ids=None,
         attachments=None,
         attachment_ids=None,
-        **kwargs
+        **kwargs,
     ):
-        attachments = self._message_deduplicate_attachments(attachments)
-        attachment_ids = self._message_deduplicate_attachment_ids(attachment_ids)
+        attachments, updated_body = self._message_deduplicate_attachments(
+            attachments, body
+        )
+        attachment_ids, updated_body = self._message_deduplicate_attachment_ids(
+            attachment_ids, updated_body
+        )
+
         return super().message_post(
-            body=body,
+            body=updated_body,
             subject=subject,
             message_type=message_type,
             email_from=email_from,
@@ -70,5 +225,5 @@ class MailThread(models.AbstractModel):
             partner_ids=partner_ids,
             attachments=attachments,
             attachment_ids=attachment_ids,
-            **kwargs
+            **kwargs,
         )
--- mail_thread_attachment_deduplicate/tests/test_mail_thread.py
+++ mail_thread_attachment_deduplicate/tests/test_mail_thread.py
@@ -1,8 +1,10 @@
 # Copyright 2025 Lambdao
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.modules.module import get_module_resource
+from os.path import join
+
 from odoo.tests import TransactionCase
+from odoo.tools.misc import file_path
 
 
 class TestMailDeduplicate(TransactionCase):
@@ -18,12 +20,9 @@ class TestMailDeduplicate(TransactionCase):
         self.customer.message_ids.message_id = (
             "<175290809903340.1751622377.487310171127319-openerp-9-res.partner@lambdao>"
         )
-        with open(
-            get_module_resource(
-                "mail_thread_attachment_deduplicate", "tests", "sent.eml"
-            ),
-            "rb",
-        ) as request_file:
+        path = join("mail_thread_attachment_deduplicate", "tests", "sent.eml")
+        full_path = file_path(path)
+        with open(full_path, "rb") as request_file:
             request_message = request_file.read()
 
         message_1 = request_message.replace(b"XXXID", b"7ecf70df-9e29-4619-984b")
@@ -83,3 +82,92 @@ class TestMailDeduplicate(TransactionCase):
         self.customer._compute_message_attachment_count()
         self.assertEqual(self.customer.message_attachment_count, 1)
         self.assertEqual(msg_2.attachment_ids, attachment)
+
+    def test_deduplicate_with_body_references(self):
+        vals_attachment = {
+            "name": "test_image.jpg",
+            "raw": b"Test image binary data",
+            "res_id": self.customer.id,
+            "res_model": "res.partner",
+            "mimetype": "image/jpeg",
+        }
+        existing_attachment = self.env["ir.attachment"].create(vals_attachment)
+
+        # Create a duplicate attachment (same content, different ID)
+        duplicate_attachment = self.env["ir.attachment"].create(
+            {
+                **vals_attachment,
+                "res_id": 0,
+                "res_model": "mail.compose.message",
+            }
+        )
+        body_with_attachment_ref = f"""
+        <div>Test message with inline image</div>
+        <div>
+            <img src="/web/image/{duplicate_attachment.id}?access_token=old-token"
+                 data-attachment-id="{duplicate_attachment.id}"
+                 alt="Test Image"
+                 class="img img-fluid"/>
+        </div>
+        """
+        msg = self.customer.message_post(
+            body=body_with_attachment_ref,
+            attachment_ids=[duplicate_attachment.id],
+            message_type="comment",
+        )
+        # The duplicate attachment was not used
+        self.assertEqual(msg.attachment_ids, existing_attachment)
+        self.assertNotEqual(msg.attachment_ids.id, duplicate_attachment.id)
+
+        message_body = msg.body
+        self.assertIn(f"/web/image/{existing_attachment.id}", message_body)
+        self.assertIn(f"access_token={existing_attachment.access_token}", message_body)
+
+        # Old references are gone
+        self.assertNotIn(f"/web/image/{duplicate_attachment.id}", message_body)
+        self.assertNotIn(
+            f'data-attachment-id="{duplicate_attachment.id}"', message_body
+        )
+        self.assertNotIn("access_token=old-token", message_body)
+
+        self.customer._compute_message_attachment_count()
+        self.assertEqual(self.customer.message_attachment_count, 1)
+
+    def test_deduplicate_with_cid_references(self):
+        vals_attachment = {
+            "name": "signature.png",
+            "raw": b"Signature image binary data",
+            "res_id": self.customer.id,
+            "res_model": "res.partner",
+            "mimetype": "image/png",
+        }
+        existing_attachment = self.env["ir.attachment"].create(vals_attachment)
+        existing_attachment.access_token = existing_attachment._generate_access_token()
+
+        # Create attachments list with CID reference (simulates email processing)
+        duplicate_content = b"Signature image binary data"  # Same content as existing
+        attachments_with_cid = [
+            ("signature.png", duplicate_content, {"cid": "signature123@example.com"})
+        ]
+
+        # Create message body with CID reference
+        body_with_cid = """
+        <div>Email message with embedded signature</div>
+        <div>
+            <img src="cid:signature123@example.com" alt="Signature"/>
+        </div>
+        <p>Best regards</p>
+        """
+        msg = self.customer.message_post(
+            body=body_with_cid,
+            attachments=attachments_with_cid,
+            message_type="comment",
+        )
+
+        message_body = msg.body
+        self.assertNotIn("cid:signature123@example.com", message_body)
+        self.assertIn(f"/web/image/{existing_attachment.id}", message_body)
+        self.assertIn(f"access_token={existing_attachment.access_token}", message_body)
+
+        self.customer._compute_message_attachment_count()
+        self.assertEqual(self.customer.message_attachment_count, 1)
