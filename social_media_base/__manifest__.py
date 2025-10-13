f644c463e4c41b3902c63af1b526fa8ba54033cb
Tran Thi Minh Ha <ha@trobz.com>
2025-10-13 12:58:05 +0700

    Backport PR#1622 to 18.0 (manifest version bump)

--- social_media_base/__manifest__.py
+++ social_media_base/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Social Media Base",
     "summary": """Basic module for social media management.""",
-    "version": "17.0.1.0.0",
+    "version": "18.0".1.0.0",
     "license": "AGPL-3",
     "author": "Binhex <https://www.binhex.cloud>,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/social",
