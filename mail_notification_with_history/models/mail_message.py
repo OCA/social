6052f316087cf7350a7bb4271403464323a923ed
Michael Köck <mkoeck@elektroshopkoeck.com>
2025-11-11 17:58:23 +0100

    [IMP] mail_notification_with_history: add deprecation warning

--- mail_notification_with_history/models/mail_message.py
+++ mail_notification_with_history/models/mail_message.py
@@ -2,7 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import models
+from logging import getLogger
 
+_logger = getLogger(__name__)
 
 class Message(models.Model):
     _inherit = "mail.message"
@@ -16,6 +18,10 @@ class Message(models.Model):
 
         if not ir_model.include_mail_history:
             return self.browse()
+
+        if hasattr(self.env[self.model], '_mail_notification_include_history'):
+            _logger.warning('The model %s uses the deprecated attribute _mail_notification_include_history. Please use the field include_mail_history on ir.model instead.', self.model)
+
         domain = self._get_notification_message_history_domain()
         messages = self.env["mail.message"].search(domain, order="date desc")
         return messages - self
