888820d62f4565c5bb22396a614e7b51723d5c7d
Michael Köck <mkoeck@elektroshopkoeck.com>
2025-11-11 17:58:06 +0100

    [IMP] mail_notification_with_history: add migration script

--- mail_notification_with_history/__manifest__.py
+++ mail_notification_with_history/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mail Notification With History",
     "summary": """Add the previous chatter discussion into new email notifications.""",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
     "author": "Camptocamp, Odoo Community Association (OCA)",
