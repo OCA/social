041bf1284a117d27a89acace07572e76eead4cd2
Andreu Orensanz <andreu.orensanz@forgeflow.com>
2026-01-14 16:18:02 +0100

    [IMP] mail_history_mark_unread: enable marking notifications as unread from search panel in Discuss app

--- mail_history_mark_unread/__manifest__.py
+++ mail_history_mark_unread/__manifest__.py
@@ -1,7 +1,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mail History Mark as Unread",
-    "summary": "Add 'Mark as Unread' action to messages in History mailbox",
+    "summary": """Add 'Mark as Unread' action to messages in History mailbox
+    and Search results""",
     "version": "17.0.1.0.0",
     "category": "Social Network",
     "website": "https://github.com/OCA/social",
@@ -14,6 +15,7 @@
             "mail_history_mark_unread/static/src/message_actions_patch.esm.js",
             "mail_history_mark_unread/static/src/message_service_patch.esm.js",
             "mail_history_mark_unread/static/src/mail_core_web_service_patch.esm.js",
+            "mail_history_mark_unread/static/src/message_card_list.xml",
         ],
     },
 }
