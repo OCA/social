88ed793b02dd463d99870e94fb2662318c1116fc
Ronald Portier <ronald@therp.nl>
2026-02-24 12:40:11 +0100

    [ADD] Add module fetchmail_outlook_multi_client

---
+++ fetchmail_outlook_multi_client/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Fetchmail Outlook Multi Client",
+    "version": "14.0.1.0.0",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "website": "https://github.com/OCA/social",
+    "depends": [
+        "fetchmail_outlook",
+        "microsoft_outlook_multi_client",
+    ],
+    "data": [
+        "views/fetchmail_server_views.xml",
+    ],
+    "installable": True,
+}
