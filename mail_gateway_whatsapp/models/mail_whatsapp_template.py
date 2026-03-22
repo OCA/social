e6c1163f7a15425ac46c172bbdd1b24d1429723b
Julio Santa Cruz <bartacruz@gmail.com>
2026-03-21 23:21:44 -0300

    [FIX] mail_gateway_whatsapp: apply OCA formatting

--- mail_gateway_whatsapp/models/mail_whatsapp_template.py
+++ mail_gateway_whatsapp/models/mail_whatsapp_template.py
@@ -327,7 +327,7 @@ class MailWhatsAppTemplate(models.Model):
             "gateway_id": gateway.id,
         }
         is_supported = True
-        template = self.search([("template_uid","=", json_data.get("id"))])
+        template = self.search([("template_uid", "=", json_data.get("id"))])
         for component in json_data.get("components", []):
             if component["type"] == "HEADER" and component["format"] == "TEXT":
                 vals["header"] = component["text"]
