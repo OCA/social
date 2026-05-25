6317741e57bb6cbd29c19ff0cd0862026fdc04cb
Fernando La Chica <flachica@sdi.es>
2026-05-25 11:13:40 +0000

    [19.0][MIG] mail_tracking: Migration to version 19.0

--- mail_tracking/models/mail_alias.py
+++ mail_tracking/models/mail_alias.py
@@ -15,7 +15,7 @@ class MailAlias(models.Model):
             x["display_name"]
             for x in self.search_read([("alias_name", "!=", False)], ["display_name"])
         }
-        mail_alias_domain = self.env["mail.alias.domain"].search([])
+        mail_alias_domain = self.env["mail.alias.domain"].search([])  # noqa W8613
         catchall_emails = set(mail_alias_domain.mapped("catchall_email"))
         default_from_emails = set(
             mail_alias_domain.filtered("default_from").mapped("default_from_email")
