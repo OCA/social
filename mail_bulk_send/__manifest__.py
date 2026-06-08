2afd0fd44a3bf61f016ce5d770ae70d4fec15faf
nobuQuartile <nobu@quartile.co>
2026-06-08 05:48:35 +0000

    fixup!

--- mail_bulk_send/__manifest__.py
+++ mail_bulk_send/__manifest__.py
@@ -2,9 +2,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Mail Bulk Send",
+    "summary": "Send emails in bulk using a mail template",
     "category": "Email",
     "version": "15.0.1.0.0",
     "author": "Quartile, Odoo Community Association (OCA)",
+    "maintainers": ["nobuQuartile"],
     "website": "https://github.com/OCA/social",
     "license": "AGPL-3",
     "depends": ["mail"],
